d822ea7d48478fcb14858651b22942ac7db9f197
David E <3david@gmail.com>
2011-10-15 23:36:23 +0200

    updates for file sharing

--- setup.sh
+++ setup.sh
@@ -3,11 +3,8 @@
 sudo apt-get install vim-gnome mercurial \
   ctags keepass2 gimp htop \
   samba libpam-smbpass \
+  pysdm \
   gnome-shell compizconfig-settings-manager
-
-# Storage Device Manager
-sudo apt-get install pysdm
-# use the following settings to mount: nls=iso8859-8,ro,umask=027,utf8,gid=users,uid=david
 
 sudo apt-get install git 
 
