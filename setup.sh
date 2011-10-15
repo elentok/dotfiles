ca7efc1550d309b08a2410904370361a6a2ec6ce
David E <3david@gmail.com>
2011-10-15 23:19:41 +0200

    added pysdm

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 sudo apt-get install vim-gnome mercurial \
-  ctags keepass2 gimp htop
+  ctags keepass2 gimp htop \
+  samba libpam-smbpass \
+  gnome-shell compizconfig-settings-manager
 
-sudo apt-get install gnome-shell compizconfig-settings-manager
+# Storage Device Manager
+sudo apt-get install pysdm
+# use the following settings to mount: nls=iso8859-8,ro,umask=027,utf8,gid=users,uid=david
+
 sudo apt-get install git 
 
 #sudo apt-get install xfce4-xkb-plugin xfce4-cpufreq-plugin \
