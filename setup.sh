ae82c7e4802934910573f935246014629b8e5c49
David E <3david@gmail.com>
2011-11-05 20:00:48 +0200

    added google talk plugin repository + handbrake

--- setup.sh
+++ setup.sh
@@ -39,8 +39,16 @@ sudo apt-get install jdownloader
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo sh -c 'echo "deb http://dl.google.com/linux/talkplugin/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
 sudo apt-get install google-talkplugin google-chrome-stable
 sudo apt-get -f install
 sudo apt-get install google-talkplugin google-chrome-stable
+
+# Handbrake (Video converter)
+
+sudo add-apt-repository ppa:stebbins/handbrake-snapshots 
+sudo apt-get update
+sudo apt-get install handbrake
+
 
