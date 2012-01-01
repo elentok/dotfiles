c87a24f9e797d7412b351ae4f1240904c1913719
David E <devnull@localhost>
2012-01-02 00:20:39 +0200

    battery_logger now tests every 5 seconds

--- setup.sh
+++ setup.sh
@@ -3,82 +3,15 @@
 mkdir temp
 cd temp
 
-sudo apt-get install vim-gnome mercurial \
+apt-get install vim-gnome mercurial \
   ctags keepass2 gimp htop \
   samba libpam-smbpass \
-  pysdm unrar regexxer
+  pysdm unrar regexxer zsh screen \
+  xfce4-terminal network-manager-gnome
 
-# Music Player Daemon
-sudo apt-get install mpd mpc ncmpcpp pms ncmpc-lyrics
-
-# KDE Mpd Plasma Client
-hg clone http://bitbucket.org/memnek/mpd-plasma-client/
-plasmapkg -i mpd-plasma-client
-
-# Kid3 - ID3 tag editor
-sudo apt-get install kid3
-
-# Git
-sudo apt-get install git 
-git config --global --add color.ui true
-
-# Ruby
-sudo apt-get install build-essential libopenssl-ruby libfcgi-dev \
-  ruby irb rubygems ruby1.8-dev \
-  sqlite3 libsqlite3-dev \
-  libmysql-ruby libmysqlclient-dev
-
-sudo gem install rubygems-update --no-ri --no-rdoc
-sudo gem install rails --include-dependencies --no-ri --no-rdoc
-
-# LAMP
-sudo apt-get install tasksel
-sudo tasksel install lamp-server
-sudo apt-get install php5-sqlite
-sudo service apache2 restart
-
-# JDownloader
-sudo apt-add-repository ppa:jd-team/jdownloader
-sudo apt-get update
-sudo apt-get install jdownloader
-
-# Chrome + Google talk plugin
-
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo sh -c 'echo "deb http://dl.google.com/linux/talkplugin/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get update
-sudo apt-get install google-talkplugin google-chrome-stable
-sudo apt-get -f install
-sudo apt-get install google-talkplugin google-chrome-stable
-
-# Handbrake (Video converter)
-
-sudo add-apt-repository ppa:stebbins/handbrake-snapshots 
-sudo apt-get update
-sudo apt-get install handbrake
-
-# Java
-sudo add-apt-repository ppa:ferramroberto/java
-sudo apt-get update
-sudo apt-get install sun-java6-jdk sun-java6-plugin
-
-# Unity-LIke Launcher for KDE
-
-sudo add-apt-repository ppa:gnumdk/ppa
-sudo apt-get update
-sudo apt-get install plasma-widget-icon-tasks
-
-# Sublime Text
-
-sudo add-apt-repository ppa:webupd8team/sublime-text-2
-sudo apt-get update
-sudo apt-get install sublime-text-2
-# Coffeescript for Sublime Text
-wget https://github.com/HenriMorlaye/Coffeescript-package-for-Sublime-Text/zipball/master
-mv master coffeescript.zip
-mkdir -p ~/.config/sublime-text-2/Packages/CoffeeScript
-unzip -j coffeescript.zip -d ~/.config/sublime-text-2/Packages/CoffeeScript
+# oh-my-zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+usermod -s /bin/zsh david
 
 # i3
 echo 'deb http://debian.sur5r.net/i3/ oneiric universe' >> /etc/apt/sources.list
@@ -87,7 +20,74 @@ apt-get --allow-unauthenticated install sur5r-keyring
 apt-get update
 apt-get install i3
 
-apt-get install xfce4-terminal network-manager-gnome \
-  lxappearance xscreensaver \
-  thunar-volman thunar
 
+# Music Player Daemon
+apt-get install mpd mpc ncmpcpp ncmpc-lyrics
+
+# KDE Mpd Plasma Client
+#hg clone http://bitbucket.org/memnek/mpd-plasma-client/
+#plasmapkg -i mpd-plasma-client
+
+# Kid3 - ID3 tag editor
+#sudo apt-get install kid3
+
+# Git
+apt-get install git 
+git config --global --add color.ui true
+
+# Ruby
+#sudo apt-get install build-essential libopenssl-ruby libfcgi-dev \
+#  ruby irb rubygems ruby1.8-dev \
+#  sqlite3 libsqlite3-dev \
+#  libmysql-ruby libmysqlclient-dev
+
+#sudo gem install rubygems-update --no-ri --no-rdoc
+#sudo gem install rails --include-dependencies --no-ri --no-rdoc
+
+# LAMP
+apt-get install tasksel
+tasksel install lamp-server
+apt-get install php5-sqlite
+service apache2 restart
+
+# JDownloader
+apt-add-repository ppa:jd-team/jdownloader
+apt-get update
+apt-get install jdownloader
+
+# Chrome + Google talk plugin
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sh -c 'echo "deb http://dl.google.com/linux/talkplugin/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+apt-get update
+apt-get install google-talkplugin google-chrome-stable
+apt-get -f install
+
+# Handbrake (Video converter)
+
+add-apt-repository ppa:stebbins/handbrake-snapshots 
+apt-get update
+apt-get install handbrake
+
+# Java
+add-apt-repository ppa:ferramroberto/java
+apt-get update
+apt-get install sun-java6-jdk sun-java6-plugin
+
+# Unity-LIke Launcher for KDE
+
+#sudo add-apt-repository ppa:gnumdk/ppa
+#sudo apt-get update
+#sudo apt-get install plasma-widget-icon-tasks
+
+# Sublime Text
+
+#sudo add-apt-repository ppa:webupd8team/sublime-text-2
+#sudo apt-get update
+#sudo apt-get install sublime-text-2
+## Coffeescript for Sublime Text
+#wget https://github.com/HenriMorlaye/Coffeescript-package-for-Sublime-Text/zipball/master
+#mv master coffeescript.zip
+#mkdir -p ~/.config/sublime-text-2/Packages/CoffeeScript
+#unzip -j coffeescript.zip -d ~/.config/sublime-text-2/Packages/CoffeeScript
