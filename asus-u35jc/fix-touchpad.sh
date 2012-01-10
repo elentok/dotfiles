628808c1b2bfe08db1b62c041f8803c8aaef0185
David E <3david@gmail.com>
2012-01-10 20:37:45 +0200

    more cleanup

--- asus-u35jc/fix-touchpad.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#to reenable the touchpad after it has stopped run:
-synclient TouchpadOff=0
--- showkeys.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-xev | grep -A2 --line-buffered '^KeyRelease' | sed -n '/keycode /s/^.*keycode \([0-9]*\).* (.*, \(.*\)).*$/\1 \2/p'
