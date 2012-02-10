c8d54a4d0f33c43e2de9cdee084d713b43fe2f90
David Elentok <3david@gmail.com>
2012-02-10 15:23:02 +0200

    ruby: add wirble + irbrc to load wirble

--- modules/ruby/install.sh
+++ modules/ruby/install.sh
@@ -44,3 +44,17 @@ echo "Installing Extra Gems"
 echo "========================================"
 gem install clipboard
 gem install rspec
+# wirble improves irb (syntax highlighting, autocomplete, ...)
+gem install wirble 
+gem install cucumber
+gem install capybara
+
+echo "========================================"
+echo "Creating symbolic links"
+echo "========================================"
+
+DIR=$(dirname "${BASH_SOURCE[0]}")
+DIR=$(cd -P $DIR && pwd)
+
+ln -sf "$DIR/irbrc" ~/.irbrc
+
