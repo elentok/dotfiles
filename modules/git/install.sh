7237a26274455ae006a3aef84727886f082aa52e
David Elentok <3david@gmail.com>
2012-04-03 16:13:40 +0300

    git: install git-subtree plugin

--- modules/git/install.sh
+++ modules/git/install.sh
@@ -10,3 +10,9 @@ DIR=$(dirname "${BASH_SOURCE[0]}")
 DIR=$(cd -P $DIR && pwd)
 
 ln -sf "$DIR/gitconfig" ~/.gitconfig
+
+
+cd /tmp
+git clone https://github.com/apenwarr/git-subtree/
+cd git-subtree
+sudo make install
