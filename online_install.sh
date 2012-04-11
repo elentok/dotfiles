6da7503884e62799b3bdabde18c634ac3e8602e0
David Elentok <3david@gmail.com>
2012-04-11 11:08:14 +0300

    changed online install script to support users who don't have ssh access to my repositories

--- online_install.sh
+++ online_install.sh
@@ -4,23 +4,28 @@
 # curl -L https://bitbucket.org/3david/dotfiles/raw/master/online_install.sh | bash
 #
 # Prerequisits:
-# sudo apt-get install git curl
+# sudo apt-get install curl
 
 (
   mkdir -p ~/.config
   cd ~/.config
 
-  sudo apt-get install git curl
+  sudo apt-get install git
+
+  bitbucket_root="git@bitbucket.org:3david"
+  if [ "`whoami`" != "david" ]; then
+    bitbucket_root="https://bitbucket.org/3david"
+  fi
 
   echo ""
   echo "========================================"
   echo "Cloning dotfiles"
-  git clone git@bitbucket.org:3david/dotfiles
+  git clone $bitbucket_root/dotfiles
 
   echo ""
   echo "========================================"
   echo "Cloning dotvim"
-  git clone git@bitbucket.org:3david/dotvim
+  git clone $bitbucket_root/dotvim
 
   echo "========================================"
   echo "setting up vim"
