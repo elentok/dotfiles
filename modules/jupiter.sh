2f40894c80e2454819980c32ccaaf37914ece92b
David Elentok <3david@gmail.com>
2012-04-30 08:38:02 +0300

    added jupiter module

---
+++ modules/jupiter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo ""
+echo "========================================"
+echo "Installing Jupiter applet"
+echo "========================================"
+
+sudo add-apt-repository ppa:webupd8team/jupiter
+sudo apt-get update
+sudo apt-get install jupiter
