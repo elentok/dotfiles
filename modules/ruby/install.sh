798b2076799218cd909491cd717ebcb3485df756
David Elentok <3david@gmail.com>
2012-05-22 10:19:45 +0300

    added more gems

--- modules/ruby/install.sh
+++ modules/ruby/install.sh
@@ -5,10 +5,10 @@ echo "========================================"
 echo "Installing rvm requirements"
 echo "========================================"
 # run "rvm requirements" and you get this list of packages:
-sudo apt-get install -y build-essential openssl libreadline6 libreadline6-dev curl \
-  git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev \
-  sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool \
-  bison subversion
+sudo apt-get install -y build-essential openssl libreadline6 libreadline6-dev \
+  curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 \
+  libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev \
+  automake libtool bison subversion
 
 #sudo apt-get install -y libreadline-gplv2-dev libreadline-dev
 sudo apt-get install -y libreadline-dev
@@ -48,6 +48,11 @@ gem install rspec
 gem install wirble 
 gem install cucumber
 gem install capybara
+gem install debugger pry haml-rails guard-livereload guard-spork spork devise cancan paperclip guard
+gem install rspec-rails jquery-rails jquery-ui-rails factory_girl_rails turn thin capistrano capistrano_colors 
+gem install compass-rails simple_form simple_enum rb-fsevent bootstrap-sass bourbon jasmine jasminerice guard-jasmine
+gem install uglifier sass-rails coffee-rails modernizer_rails backbone-on-rails
+
 
 echo "========================================"
 echo "Creating symbolic links"
