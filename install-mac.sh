c9d9b366de15af6dd5fcff82903e32a83e99c106
David Elentok <3david@gmail.com>
2012-08-14 10:04:23 +0300

    added nodejs mac support

--- install-mac.sh
+++ install-mac.sh
@@ -4,3 +4,4 @@
 ./modules/zsh/install.sh
 ./modules/ack-grep/install.sh
 ./modules/ruby/post-install.sh
+./modules/nodejs.sh
--- modules/nodejs.sh
+++ modules/nodejs.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-sudo apt-get install -y python-software-properties
-sudo add-apt-repository ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install -y nodejs nodejs-dev npm
+if [ "`uname -s`" == "Darwin" ]; then
+  brew install nodejs
+  curl https://npmjs.org/install.sh | sh
+else
+  sudo apt-get install -y python-software-properties
+  sudo add-apt-repository ppa:chris-lea/node.js
+  sudo apt-get update
+  sudo apt-get install -y nodejs nodejs-dev npm
+fi
+
+
 
 sudo npm install -g coffee-script
 sudo npm install -g supervisor
