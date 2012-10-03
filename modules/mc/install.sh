d6f31560043959b28c3b2252c5af609c78cebc94
David Elentok <3david@gmail.com>
2012-10-03 13:50:14 +0200

    midnight commander mac support

--- modules/mc/install.sh
+++ modules/mc/install.sh
@@ -4,10 +4,17 @@ echo ""
 echo "========================================"
 echo "Installing Midnight Commander"
 echo "========================================"
-sudo apt-get install -y mc
 
 DIR=$(dirname "${BASH_SOURCE[0]}")
 DIR=$(cd -P $DIR && pwd)
 
-mkdir -p ~/.mc
-ln -sf "$DIR/ini" ~/.mc/ini
+if [ "`uname -s`" == "Darwin" ]; then
+  brew install mc
+  mkdir -p ~/.config/mc
+  ln -sf "$DIR/ini" ~/.config/mc/ini
+else
+  sudo apt-get install -y mc
+  mkdir -p ~/.mc
+  ln -sf "$DIR/ini" ~/.mc/ini
+fi
+
