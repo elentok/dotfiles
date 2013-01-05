0caf6adc4430f7c039c9a546db2fa03e5872b75d
David Elentok <3david@gmail.com>
2013-01-05 18:51:03 +0200

    added 'reattach-to-namespace' for tmux

--- modules/tmux/install.sh
+++ modules/tmux/install.sh
@@ -7,6 +7,10 @@ echo "========================================"
 
 if [ "`uname -s`" == "Darwin" ]; then
   brew install tmux
+
+  brew install reattach-to-user-namespace
+  # for more info, see:
+  #   http://robots.thoughtbot.com/post/19398560514/how-to-copy-and-paste-with-tmux-on-mac-os-x
 else
   sudo apt-get install -y tmux
 fi
