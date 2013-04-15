d325146e93cd126ad676333b55072a7556764c33
David Elentok <3david@gmail.com>
2013-04-15 14:23:04 +0300

    cleaned up vim/install.sh (added 'symlinks' options to just install symlinks)

--- vim/install.sh
+++ vim/install.sh
@@ -1,57 +1,65 @@
 #!/bin/bash
 
-DIR=$(dirname $0)
-DIR=$(cd -P $DIR && pwd)
+source `dirname $0`/../config.sh
 
-if [ "`uname -s`" == "Darwin" ]; then
-  echo "=============================="
-  echo "Installing MacVim"
-  echo "=============================="
+install_on_mac() {
+  header "Installing MacVim"
   brew install macvim --with-cscope --override-system-vim --with-lua
   cd /usr/bin
   sudo mv vim vim-builtin
   latest_vim=`/bin/ls -1 -d /usr/local/Cellar/macvim/7.* | tail -1`
   sudo ln -s $latest_vim/bin/vim vim
-  echo "=============================="
-  echo "Installing ctags"
-  echo "=============================="
+
+  header "Installing ctags"
   brew install ctags
+}
+
+install_on_linux() {
+  header "Installing Powerline fonts"
+  if [ "`uname -s`" == "Linux" ]; then
+    bash "$DOTF/vim/powerline-fonts/install.sh"
+  fi
+}
+
+install_symlinks() {
+  header "Installing ~/.vim and ~/.vimrc symlinks"
+  mv -f ~/.vimrc ~/.vimrc.backup
+  mv -f ~/.vim ~/.vim.backup
+  mv -f ~/.pylintrc ~/.pylintrc.backup
+  ln -sf "$DOTF/vim/vimrc" ~/.vimrc
+  ln -sf "$DOTF/vim" ~/.vim
+  ln -sf "$DOTF/vim/pylintrc" ~/.pylintrc
+}
+
+install_vundle() {
+  header "Installing Vundle"
+  cd ~/.vim
+  git clone http://github.com/gmarik/vundle.git bundle/vundle
+  vim +BundleInstall +qall
+}
+
+install_utils() {
+  header "Installing Utils (Linters, Silver Searcher, ...)"
+
+  npm install -g coffeelint
+  npm install -g jshint
+  npm install -g jsonlint
+  npm install -g marked
+  brew install tidy
+  brew install ag # the silver searcher
+  sudo easy_install pylint
+}
+
+if [ "$1" == "symlinks" ]; then
+  install_symlinks
+else
+  if [ "`uname -s`" == "Darwin" ]; then
+    install_on_mac
+  else
+    install_in_linux
+  fi
+
+  install_symlinks
+  install_vundle
+  install_utils
 fi
-
-echo "=============================="
-echo "Installing ~/.vim and ~/.vimrc symlinks"
-echo "=============================="
-ln -sf "$DIR/vimrc" ~/.vimrc
-ln -sf "$DIR" ~/.vim
-
-echo "=============================="
-echo "Installing Vundle"
-echo "=============================="
-cd ~/.vim
-git clone http://github.com/gmarik/vundle.git bundle/vundle
-
-echo "=============================="
-echo "Installing Powerline fonts"
-echo "=============================="
-if [ "`uname -s`" == "Linux" ]; then
-  bash "$DIR/vimfiles/powerline-fonts/install.sh"
-fi
-
-echo "=============================="
-echo "Installing Vundle Bundles"
-echo "=============================="
-vim +BundleInstall +qall
-
-echo "=============================="
-echo "Installing Utils (Linters, Silver Searcher, ...)"
-echo "=============================="
-
-npm install -g coffeelint
-npm install -g jshint
-npm install -g jsonlint
-npm install -g marked
-sudo easy_install pylint
-ln -sf "$DIR/pylintrc" ~/.pylintrc
-brew install tidy
-# the silver searcher
-brew install ag
