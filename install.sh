117bf21db897ffdfc708ed7804ed70cf577b108a
David Elentok <3david@gmail.com>
2013-04-19 21:16:58 +0300

    cleaned up install.sh

--- install.sh
+++ install.sh
@@ -3,43 +3,33 @@
 
 source `dirname $0`/config.sh
 
+./nodejs/install.sh $*
+./git/install.sh $*
+./zsh/install.sh $*
+./tmux/install.sh $*
+./vim/install.sh $*
+./ack/install.sh $*
+./ruby/install.sh $*
+./todo/install.sh $*
+
 if [ "$OS" == "mac" ]; then
-
-  ./nodejs/install.sh $*
-  ./git/install.sh $*
-  ./zsh/install.sh $*
-  ./tmux/install.sh $*
-  ./vim/install.sh $*
-  ./ack/install.sh $*
-  ./ruby/install.sh $*
+  echo .
   #./modules/mac-smb-performance-fix/install.sh $*
-
-  # required for todo.sh colorSymbols plugin
-  brew_install gawk
-
 else
 
   header "Base packages"
-  sudo apt-get install -y vim-gnome mercurial \
-    ctags keepassx gimp htop \
+  sudo apt-get install -y mercurial \
+    keepassx gimp htop \
     samba libpam-smbpass \
     pysdm unrar regexxer \
     krusader vlc
 
-  header "Modules"
-  ./git/install.sh $*
-  ./zsh/install.sh $*
-  ./tmux/install.sh $*
-  ./vim/install.sh $*
- $*
-  ./modules/ack-grep/install.sh $*
-  ./modules/mc/install.sh $*
-  ./modules/mpd/install.sh $*
-  ./modules/ruby/install.sh $*
-  ./modules/nodejs.sh $*
-  sudo ./modules/google.sh $*
-  ./modules/movgrab.sh $*
-  ./modules/jdownloader.sh $*
-  ./modules/handbrake.sh $*
-  ./modules/lamp.sh $*
+  #header "Modules"
+  #./modules/mc/install.sh $*
+  #./modules/mpd/install.sh $*
+  #sudo ./modules/google.sh $*
+  #./modules/movgrab.sh $*
+  #./modules/jdownloader.sh $*
+  #./modules/handbrake.sh $*
+  #./modules/lamp.sh $*
 fi
--- todo/install.sh
+++ todo/install.sh
@@ -5,6 +5,9 @@ source `dirname $0`/../config.sh
 install_todo() {
   if [ "$OS" == "mac" ]; then
     brew_install todo-txt
+
+    # required for the colorSymbols plugin
+    brew_install gawk
   else
     error "TODO: IMPLEMENT THIS"
   fi
