10d8fef030cfa50644241a2207e6f28515601d42
David Elentok <3david@gmail.com>
2013-06-24 17:36:09 +0300

    cleaned up fish git bindings

--- fish/completions/gco.fish
+++
@@ -1,3 +0,0 @@
-complete -c gco -a '(__fish_git_branches)' --description 'Branch'
-complete -c gco -a '(__fish_git_tags)' --description 'Tag'
-complete -c gco -s b -d 'Create a new branch'
--- fish/completions/gp.fish
+++
@@ -1,2 +0,0 @@
-complete --no-files -c gp -a '(__fish_git_remotes)' --description 'Remote'
-complete --no-files -c gp -a '(__fish_git_branches)' --description 'Branch'
