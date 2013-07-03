649b1862a45201accba4cd0833fff61b5b240857
David Elentok <3david@gmail.com>
2013-07-03 18:58:45 +0300

    added goreplace

--- modules/go.sh
+++ modules/go.sh
@@ -6,8 +6,20 @@ install_on_mac() {
   brew_install hg
   brew_install go
   make_dir $HOME/.go
+
+  mv /usr/local/Cellar/go/1.0.3/bin/* /usr/local/Cellar/go/1.1/bin/
+}
+
+install_goreplace() {
+  cd ~/.go
+  mkdir -p bin
+  cd bin
+  go get github.com/piranha/goreplace
+  go build github.com/piranha/goreplace
 }
 
 if [ "$OS" == "mac" ]; then
   install_on_mac
 fi
+
+install_goreplace
