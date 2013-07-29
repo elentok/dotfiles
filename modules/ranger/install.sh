3b0a41252e133408a7b6f7cfc7d6259cd0240771
David Elentok <3david@gmail.com>
2013-07-29 11:27:17 +0300

    added scripts

---
+++ modules/ranger/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+brew install ranger
+
+brew install atool # archive tool (for als)
+brew install w3c
+
+# these require XQuarts to be installed
+brew install xpdf
+brew install imlib2 # for libcaca
+brew install libcaca --with-imlib2 # for img2txt
+
