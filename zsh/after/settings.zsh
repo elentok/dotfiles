d633dc72a05a4ad592c5cd9f0ccaa6616e6d675d
David Elentok <3david@gmail.com>
2013-09-12 14:50:40 +0300

    zsh keymappings

--- zsh/after/settings.zsh
+++ zsh/after/settings.zsh
@@ -2,7 +2,8 @@
 bindkey -v
 
 bindkey '^R' history-incremental-search-backward
-bindkey '^J' down-line-or-search
-bindkey '^K' up-line-or-search
+bindkey '^N' down-line-or-search
+bindkey '^K' kill-line
+bindkey '^P' up-line-or-search
 bindkey '^A' beginning-of-line
 bindkey '^E' end-of-line
