5d81d6004620fb574a3e9f98eddf2690eb7ac6c8
David Elentok <elentok@google.com>
2014-01-09 11:55:05 +0200

    Misc

--- zsh/before/0-settings.zsh
+++ zsh/before/0-settings.zsh
@@ -3,6 +3,7 @@ if [[ "$TERM" != "screen-256color" ]]; then
   export TERM=xterm-256color
 fi
 
+fpath=($BREW_HOME/lib/node_modules/tailr/completions $fpath)
 fpath=(/usr/local/share/npm/lib/node_modules/tailr/completions $fpath)
 fpath=(/usr/local/share/npm/lib/node_modules/dns-switcher/completions $fpath)
 fpath=($HOME/.rbenv/versions/2.0.0-p247/lib/ruby/gems/2.0.0/gems/shaft-0.8.8/completions $fpath)
