b69d9c2a738b9a60c48e50f7fe99915d2b37d051
David Elentok <elentok@google.com>
2014-01-15 10:51:38 +0200

    Add nginx unblock script

---
+++ modules/nginx.sh
@@ -0,0 +1,2 @@
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --add $BREW_HOME/Cellar/nginx/1.4.4/bin/nginx
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --unblockapp $BREW_HOME/Cellar/nginx/1.4.4/bin/nginx
