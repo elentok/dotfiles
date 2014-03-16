e1706bed08f655366d5c892a7a73b996eac7e51f
David Elentok <3david@gmail.com>
2014-03-16 16:00:55 +0200

    Update nvim installer

--- nvim/install.sh
+++ nvim/install.sh
@@ -3,21 +3,24 @@
 source `dirname $0`/../config.sh
 
 install_requirements() {
-  brew md5sha1sum libtool automake cmake
+  brew_install md5sha1sum libtool automake cmake
 }
 
 install_neovim() {
   if [ ! -e "$HOME/.neovim-src" ]; then
     cd
-    git clone https://github.com/neovim/neovim .neovim-src
-    cd .neovim-src
+    mkdir .neovim-src
+    git_clone https://github.com/neovim/neovim neovim
+    git_clone https://github.com/neovim/docs docs
+    git_clone https://github.com/neovim/vimscript vimscript
+    cd neovim
     make cmake
     make
   fi
 }
 
 install_symlinks() {
-  symlink "$DOTF/nvim/neovimrc" ~/.neovimrc
+  symlink "$DOTF/nvim/nvimrc" ~/.nvimrc
 }
 
 install_requirements
