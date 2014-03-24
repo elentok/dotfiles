7d3ff3c75dc4626e60af4e44d20b579c06807ebc
David Elentok <3david@gmail.com>
2014-03-24 20:20:31 +0200

    Update neovim installer

--- nvim/install.sh
+++ nvim/install.sh
@@ -2,21 +2,27 @@
 
 source `dirname $0`/../config.sh
 
+NEOROOT=$HOME/.neovim-src
+
 install_requirements() {
   brew_install md5sha1sum libtool automake cmake
 }
 
 install_neovim() {
-  if [ ! -e "$HOME/.neovim-src" ]; then
-    cd
-    mkdir .neovim-src
-    git_clone https://github.com/neovim/neovim neovim
-    git_clone https://github.com/neovim/docs docs
-    git_clone https://github.com/neovim/vimscript vimscript
-    cd neovim
-    make cmake
-    make
+  if [ ! -e "$NEOROOT" ]; then
+    mkdir -p $NEOROOT
   fi
+
+  cd $NEOROOT
+  git_clone https://github.com/neovim/neovim neovim
+  git_clone https://github.com/neovim/docs docs
+  git_clone https://github.com/neovim/vimscript vimscript
+
+  cd neovim
+  make clean
+  #cmake -DCMAKE_INSTALL_PREFIX=$NEOROOT
+  make cmake
+  make
 }
 
 install_symlinks() {
