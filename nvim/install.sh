c42c033d710abb7eee072c27d855e27101bc81ba
David Elentok <3david@gmail.com>
2014-08-26 12:15:57 +0300

    Install neovim using homebrew

--- nvim/install.sh
+++ nvim/install.sh
@@ -2,33 +2,13 @@
 
 source `dirname $0`/../config.sh
 
-NEOROOT=$HOME/.neovim-src
-
-install_requirements() {
-  brew_install md5sha1sum libtool automake cmake
-}
-
 install_neovim() {
-  if [ ! -e "$NEOROOT" ]; then
-    mkdir -p $NEOROOT
-  fi
-
-  cd $NEOROOT
-  git_clone https://github.com/neovim/neovim neovim
-  git_clone https://github.com/neovim/docs docs
-  git_clone https://github.com/neovim/vimscript vimscript
-
-  cd neovim
-  make clean
-  #cmake -DCMAKE_INSTALL_PREFIX=$NEOROOT
-  make cmake
-  make
+  brew install --HEAD https://raw.github.com/neovim/neovim/master/neovim.rb
 }
 
 install_symlinks() {
   symlink "$DOTF/nvim/nvimrc" ~/.nvimrc
 }
 
-install_requirements
 install_neovim
 install_symlinks
