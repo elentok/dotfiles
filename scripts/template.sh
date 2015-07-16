5b7176c0a146b5fb17e355aac1e6b9d48e671d14
David Elentok <3david@gmail.com>
2015-07-16 15:05:20 +0300

    new-script and app-id

---
+++ scripts/template.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+source $DOTF/framework
+
+main() {
+  if [ $# -eq 0 ]; then
+    usage
+  else
+    run "$@"
+  fi
+}
+
+usage() {
+  cat<<EOF
+
+{{script-name}}
+
+Usage:
+  {{script-name}} ship new <name>...
+  {{script-name}} ship <name> move <x> <y> [--speed=<kn>]
+  {{script-name}} ship shoot <x> <y>
+  {{script-name}} mine (set|remove) <x> <y> [--moored|--drifting]
+  {{script-name}} -h | --help
+  {{script-name}} --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+  --speed=<kn>  Speed in knots [default: 10].
+  --moored      Moored (anchored) mine.
+  --drifting    Drifting mine.
+
+EOF
+}
+
+run() {
+  echo 'hello'
+}
+
+main "$@"
