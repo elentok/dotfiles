aaadb5f492aaa117b68d0f0de0b3f776d2c3aeaa
David Elentok <3david@gmail.com>
2015-07-18 17:17:15 +0300

    Remove obsolete tmux aliases

--- zsh/after/aliases.zsh
+++ zsh/after/aliases.zsh
@@ -2,14 +2,8 @@ alias x=exit
 alias c=clear
 
 # Tmux {{{1
-TMUX_CONFIG="$DOTF/plugins/tmux/tmux.conf"
-if [ "$(uname)" = "Darwin" ]; then
-  TMUX_CONFIG="$DOTF/plugins/tmux/tmux-osx.conf"
-fi
-alias tmux="tmux -f $TMUX_CONFIG"
 alias tm='tmux -u'
 alias tma='tm a'
-alias tmt='tm new-session -A  -s test-output' # create or attach to 'test-output' session
 
 # Todo.txt {{{1
 t() { vim "+Todo $*" }
