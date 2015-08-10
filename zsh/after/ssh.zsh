d72a31b4ca55f1c1716bf74045cb171edaf6b45f
David Elentok <3david@gmail.com>
2015-08-10 16:37:09 +0300

    Attempt to fix SSH issues

--- zsh/after/ssh.zsh
+++ zsh/after/ssh.zsh
@@ -1,7 +1,4 @@
-export SSH_AUTH_SOCK="/tmp/ssh-agent-$USER-tmux"
-
-if [ ! -e "$SSH_AUTH_SOCK" ]; then
-  echo "Creating new ssh-agent on $SSH_AUTH_SOCK"
-  mkdir -p $(dirname $SSH_AUTH_SOCK)
-  eval $(ssh-agent -a $SSH_AUTH_SOCK)
+if [ -n "$TMUX" ]; then
+  export SSH_AUTH_SOCK="$HOME/.ssh/default-agent"
+  ssh-switch fix
 fi
