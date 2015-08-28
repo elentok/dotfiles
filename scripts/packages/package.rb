a3291425832388a68a67630b247c1a74612d6301
David Elentok <3david@gmail.com>
2015-08-28 16:48:52 +0300

    Add pkgs script

---
+++ scripts/packages/package.rb
@@ -0,0 +1,97 @@
+class Package
+  attr_accessor :title, :date, :estimated, :tracking, :store
+  def initialize(title, options = {})
+    @title = title
+    @store = options[:store]
+    @date = Date.parse(options[:date])
+    @estimated = DeliveryEstimation.parse(options[:estimated])
+  end
+
+  def to_s
+    @title
+  end
+
+  def order
+    if @estimated.nil?
+      99999
+    elsif @estimated.overdue?
+      -99999
+    else
+      @estimated.days_to_start_expecting
+    end
+  end
+
+  def days_ago
+    @days_ago ||= (Date.today - @date).to_i
+  end
+
+  def time_ago
+    if days_ago == 0
+      'today'
+    elsif days_ago == 1
+      'yesterday'
+    elsif days_ago < 7
+      "#{days_ago} day(s) ago"
+    else
+      weeks = (days_ago / 7).to_i
+      "#{weeks} week(s) ago"
+    end
+  end
+
+  def when_to_expect
+    @estimated.nil? ? 'unknown' : @estimated.when
+  end
+end
+
+class DeliveryEstimation
+  def initialize(from, to = nil)
+    @from = from
+    @to = to
+  end
+
+  def self.parse(raw)
+    return nil if raw.nil?
+
+    dates = raw.split(' - ').map { |date| Date.parse(date) }
+    DeliveryEstimation.new(*dates)
+  end
+
+  def overdue?
+    @to && Date.today > @to
+  end
+
+  def days_to_start_expecting
+    (@from - Date.today).to_i
+  end
+
+  def when_to_start_expecting
+    days = days_to_start_expecting
+    if days >= 7
+      weeks = (days / 7).to_i
+      if weeks == 1
+        'next week'
+      else
+        "in #{weeks} weeks"
+      end
+    else
+      if Date.today.wday < @from.wday
+        'this week'
+      else
+        'next week'
+      end
+    end
+  end
+
+  def when
+    if overdue?
+      :overdue
+    else
+      if Date.today > @from
+        :any_day_now
+      else
+        when_to_start_expecting
+      end
+    end
+  end
+end
+
---
+++ scripts/packages/reader.rb
@@ -0,0 +1,31 @@
+require_relative './package'
+
+class PackageReader
+  def initialize(filename)
+    @filename = filename
+    @store = nil
+    @packages = []
+  end
+
+  def read
+    instance_eval File.read(@filename), @filename
+    @packages
+  end
+
+  def store(name, &block)
+    @store = name
+    yield self
+    @store = nil
+  end
+
+  def pkg(title, options = {})
+    (options ||= {}).merge!(store: @store)
+    @packages << Package.new(title, options)
+  end
+
+  def self.read(filename)
+    filename = File.expand_path(filename)
+    self.new(filename).read
+  end
+end
+
