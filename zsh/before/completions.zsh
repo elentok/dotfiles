c64f60f9c57e38dbde1e8da00c418696fdd15674
David Elentok <3david@gmail.com>
2015-11-07 15:44:31 +0200

    Cleanup zsh config

--- zsh/before/completions.zsh
+++ zsh/before/completions.zsh
@@ -1,14 +1,5 @@
-# so tmux will allow 256 colors:
-if [[ "$TERM" != "screen-256color" ]]; then
-  export TERM=xterm-256color
-fi
-
-export TMUX_TMPDIR=/tmp/$USERNAME
-mkdir -p $TMUX_TMPDIR
-
 fpath=($BREW_HOME/lib/node_modules/tailr/completions $fpath)
 fpath=(/usr/local/share/npm/lib/node_modules/tailr/completions $fpath)
 fpath=(/usr/local/share/npm/lib/node_modules/dns-switcher/completions $fpath)
 fpath=($HOME/.rbenv/versions/2.0.0-p247/lib/ruby/gems/2.0.0/gems/shaft-0.8.8/completions $fpath)
 
-zstyle ':presto:load' pmodule 'git'
--- zsh/before/ruby.zsh
+++
@@ -1,4 +0,0 @@
-# taken care of by ~/.zprezto/modules/ruby/init.zsh
-#if which rbenv > /dev/null; then
-  #eval "$(rbenv init - | grep -v 'rehash 2>')";
-#fi
