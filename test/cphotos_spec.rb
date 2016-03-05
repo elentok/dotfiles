2fd6ef23d7adbd69a00a9191b896ccec0d04b0b0
David Elentok <3david@gmail.com>
2016-03-05 18:54:43 +0200

    Refactor

--- test/cphotos_spec.rb
+++ test/cphotos_spec.rb
@@ -25,25 +25,26 @@ describe '#analyze' do
   end
 
   it 'analyzes' do
-    result = analyze('test/fixtures/photos-source', '/tmp/photos-target')
+    a = Analysis.new('test/fixtures/photos-source', '/tmp/photos-target')
+    a.analyze
 
-    expect(result.added.length).to eq 4
+    expect(a.added.length).to eq 4
 
-    expect(result.added[0].target).to eq \
+    expect(a.added[0].target).to eq \
       '/tmp/photos-target/2016/02-10/1020-p1020304-fz1000.jpg'
 
-    expect(result.added[1].target).to eq \
+    expect(a.added[1].target).to eq \
       '/tmp/photos-target/2016/01-05 description/0102-p1020305-fz1000.jpg'
 
-    expect(result.added[2].target).to eq \
+    expect(a.added[2].target).to eq \
       '/tmp/photos-target/2016/01-06_10 multi day trip/day02/0102-p1020306-fz1000.jpg'
 
-    expect(result.added[3].target).to eq \
+    expect(a.added[3].target).to eq \
       '/tmp/photos-target/2016/03-30_04-12 multi day trip/day04/0102-p1020309-fz1000.jpg'
 
-    expect(result.deleted.length).to eq 1
+    expect(a.deleted.length).to eq 1
 
-    expect(result.deleted[0].target).to eq \
+    expect(a.deleted[0].target).to eq \
       '/tmp/photos-target/2016/10-05/0102-p1020310-fz1000.jpg'
   end
 end
