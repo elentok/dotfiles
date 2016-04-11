5f99afeb127df25905d3282f036454e4230f5ec8
Danny Shternberg <dansht@google.com>
2016-04-11 16:08:08 +0300

    Fix autocomplete cache dir

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -2,7 +2,7 @@ autoload -U compinit && compinit
 
 # Use caching to make completion for cammands such as dpkg and apt usable.
 zstyle ':completion::complete:*' use-cache on
-zstyle ':completion::complete:*' cache-path "~/.local/share/zcompcache"
+zstyle ':completion::complete:*' cache-path "$HOME/.local/share/zcompcache"
 
 # Case-insensitive (all), partial-word, and then substring completion.
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
