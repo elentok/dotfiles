e9f2e65eb821d306ee780c992b1203b6ddc9bc12
David Elentok <3david@gmail.com>
2016-06-28 15:47:40 +0300

    Add <c-x>b fzf git branch completion

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -1,4 +1,4 @@
-# Trigger fzf completion
+# ^f: fzf completion {{{1
 my-fzf-trigger-completion() {
   LBUFFER="${LBUFFER}**"
   zle fzf-completion
@@ -6,5 +6,17 @@ my-fzf-trigger-completion() {
 
 zle -N my-fzf-trigger-completion
 bindkey '^f' my-fzf-trigger-completion
+
+# ^xb: branch completion {{{1
+
+my-branch-completion() {
+  LBUFFER="${LBUFFER}$(git all-branches | fzf)"
+  local ret=$?
+  zle redisplay
+  return $ret
+}
+
+zle -N my-branch-completion
+bindkey '^xb' my-branch-completion
 
 # vim: foldmethod=marker
