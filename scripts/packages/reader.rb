2cf654ae058fc3e80a33fa194266484664f2b367
David Elentok <3david@gmail.com>
2016-08-30 16:43:21 +0300

    Packages: switch to yaml format

--- scripts/packages/reader.rb
+++ scripts/packages/reader.rb
@@ -1,34 +1,17 @@
+require 'yaml'
 require_relative './package'
 
 class PackageReader
-  def initialize(filename)
-    @filename = filename
-    @store = nil
-    @packages = []
-  end
-
-  def read
-    instance_eval File.read(@filename), @filename
-    @packages
-  end
-
-  def store(name, &block)
-    @store = name
-    yield self
-    @store = nil
-  end
-
-  def arrived(&block)
-  end
-
-  def pkg(title, date, estimated = nil, options = {})
-    (options ||= {}).merge!(store: @store)
-    @packages << Package.new(title, date, estimated, options)
-  end
-
   def self.read(filename)
     filename = File.expand_path(filename)
-    self.new(filename).read
+    packages = []
+
+    YAML.load_file(filename).map do |store, store_packages|
+      store_packages.each do |title, options|
+        packages << Package.new(title, store, options)
+      end
+    end
+
+    packages
   end
 end
-
