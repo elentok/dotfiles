e54b5bfc8d5da523d557b43265e9f70ec08ff474
David Elentok <3david@gmail.com>
2016-09-12 15:36:50 +0300

    Add ssh server widget

--- zsh/widgets.zsh
+++ zsh/widgets.zsh
@@ -16,3 +16,13 @@ git-aliases-widget() {
 }
 zle     -N   git-aliases-widget
 bindkey '^G' git-aliases-widget
+
+ssh-server-widget() {
+  LBUFFER="${LBUFFER}$(pick-ssh-server)"
+  local ret=$?
+  zle redisplay
+  typeset -f zle-line-init >/dev/null && zle zle-line-init
+  return $ret
+}
+zle     -N   ssh-server-widget
+bindkey '^Xs' ssh-server-widget
