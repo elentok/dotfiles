24ec3471cdb4e83d839466e93661772ffa80c91e
David Elentok <3david@gmail.com>
2016-10-10 16:31:47 +0300

    Add ports alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -67,6 +67,7 @@ alias vl='vim "+OpenSession! last"'
 alias vz='file="$(edit-zsh-dotfile)" && source $file'
 alias x=exit
 alias tailpow='tail -f $(find ~/Library/Logs/Pow -name "*.log" | fzf)'
+alias ports='sudo echo && (sudo lsof -i -n -P | fzf --header-lines=1)'
 
 alias ew='whichx $EDITOR'
 alias cw='whichx cat'
