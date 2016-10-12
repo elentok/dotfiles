2ccd6dcc53e8feaf524cfa3a6daa2292a3ac6ba7
David Elentok <3david@gmail.com>
2016-10-12 21:50:29 +0300

    Add label maker

---
+++ scripts/labelmaker/labels.js
@@ -0,0 +1,16 @@
+/* global window */
+
+window.labels = [
+  { icon: "usb", title: "USB Cables" },
+  { icon: "usb", title: "USB Misc", subtitle: "(Chargers, Adapters)" },
+  { icon: "pencil", title: "Fountain Pens" },
+  { icon: "headphones", title: "Headphones", subtitle: ("(and Adapters)") },
+  { icon: "volume-up", title: "Audio" },
+  { icon: "plug", title: "Power" },
+  { icon: "sitemap", title: "Network" },
+  { icon: "battery-three-quarters", title: "Batteries" },
+  { icon: "mobile", title: "Mobile", subtitle: "(Watch, MicroSD adapters)" },
+  { icon: "question-circle", title: "Junk 1" },
+  { icon: "question-circle", title: "Junk 2" },
+  { icon: "question-circle", title: "Mini-Junk" },
+]
