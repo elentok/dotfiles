9e9d3ce093aa32840c01a52784c8b3a11f4c2672
David Elentok <3david@gmail.com>
2016-10-18 20:36:07 +0300

    Label Maker updates

--- scripts/labelmaker/labels.js
+++ scripts/labelmaker/labels.js
@@ -1,12 +1,27 @@
 /* global window */
 
 window.labels = [
+  { icon: "cubes", title: "Lego" },
+  { icon: "leaf", title: "Gardening", subtitle: "(Taftafot, adapters)" },
+  { icon: "search", title: "Magnifiers" },
+  { icon: "paint-brush", title: "Colors" },
+
+  { icon: "hdd-o", title: "HDD & DVD Burner" },
+  { icon: "hdd-o", title: "HDD & Flash drives" },
+  { icon: "circle-o", title: "Blank CDs & DVDs", subtitle: ("and cases") },
+  { icon: "plug", title: "Extension Cords", subtitle: ("and splitters") },
+
   { icon: "usb", title: "USB Cables" },
   { icon: "usb", title: "USB Misc", subtitle: "(Chargers, Adapters)" },
   { icon: "pencil", title: "Fountain Pens" },
-  { icon: "headphones", title: "Headphones", subtitle: ("(and Adapters)") },
-  { icon: "volume-up", title: "Audio" },
+  { icon: "headphones", title: "Headphones", subtitle: "(and Adapters)" },
+  { icon: "volume-up", title: "Audio", subtitle: "(Speakers, Mics, Cables)" },
+  { icon: "desktop", title: "Video", subtitle: "(Cables, adapters)" },
   { icon: "plug", title: "Power" },
+  { icon: "bolt", title: "Soldering Iron" },
+  { icon: "power-off", title: "Power Supply" },
+  { icon: "link", title: "Wires and Keychains" },
+  { icon: "medkit", title: "Medication" },
   { icon: "sitemap", title: "Network" },
   { icon: "battery-three-quarters", title: "Batteries" },
   { icon: "mobile", title: "Mobile", subtitle: "(Watch, MicroSD adapters)" },
