fe84926ae92f42d966a8ac536b3b5d92ae472379
David Elentok <3david@gmail.com>
2016-11-13 12:03:59 +0200

    Add calendar generator

---
+++ scripts/calendar/utils.js
@@ -0,0 +1,29 @@
+/* globals document */
+
+function div(className, content = null) {
+  const el = document.createElement("div")
+  el.className = className
+  if (content != null) {
+    add(el, content)
+  }
+  return el
+}
+
+function add(parent, children) {
+  if (children == null) { return }
+  if (children.forEach) {
+    children.forEach(child => addSingle(parent, child))
+  } else {
+    addSingle(parent, children)
+  }
+}
+
+function addSingle(parent, child) {
+  if (child == null) { return }
+
+  if (typeof child === "string") {
+    child = document.createTextNode(child)
+  }
+
+  parent.appendChild(child)
+}
