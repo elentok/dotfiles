549685ff9109e5574ae32e5f2269de7b78072b59
David Elentok <3david@gmail.com>
2017-02-19 14:33:25 +0200

    Zsh: Fix up/down arrows on linux

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -59,6 +59,14 @@ setopt HIST_BEEP                 # Beep when accessing non-existent history.
 autoload -Uz url-quote-magic
 zle -N self-insert url-quote-magic
 
+# Up/Down arrow search {{{1
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
+
 # Functions {{{1
 
 function encrypt() {
