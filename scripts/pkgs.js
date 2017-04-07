0d57555064a8799e918148c43f986f4b8448076f
David Elentok <3david@gmail.com>
2017-04-07 16:00:14 +0300

    Implement basic pkgs.js

--- scripts/pkgs.js
+++ scripts/pkgs.js
@@ -4,14 +4,48 @@ const yaml = require("js-yaml");
 const path = require("path");
 const fs = require("fs");
 const _ = require("underscore");
+const Table = require("easy-table");
 
 class Package {
   constructor(name, store, { when, tracking }) {
     this.name = name;
     this.store = store;
     this.when = when;
-    console.log(when);
     this.tracking = tracking;
+
+    if (when == null) {
+      console.warn(`WARNING: Package "${name}" is missing "when"`);
+    }
+  }
+
+  addToTable(table) {
+    table.cell("Name", this.name);
+    table.cell("Order date", this.getTimeAgo());
+    table.newRow();
+  }
+
+  getTimeAgo() {
+    const msAgo = new Date() - this.when;
+    const daysAgo = Math.floor(msAgo / 1000 / 60 / 60 / 24);
+
+    if (daysAgo === 0) {
+      return "today";
+    } else if (daysAgo === 1) {
+      return "yesterday";
+    } else {
+      const weeks = Math.floor(daysAgo / 7);
+      const days = daysAgo - weeks * 7;
+
+      const values = [];
+      if (weeks > 0) {
+        values.push(`${weeks}w`);
+      }
+      if (days > 0) {
+        values.push(`${days}d`);
+      }
+
+      return values.join(" ") + " ago";
+    }
   }
 }
 
@@ -32,20 +66,19 @@ function loadPackages() {
   return packages;
 }
 
-function showStatus() {
+function listPackages() {
   const packages = _.sortBy(loadPackages(), pkg => pkg.when);
-  // console.log(packages);
+  const table = new Table();
+  packages.forEach(pkg => pkg.addToTable(table));
 
-  const titleWidth = _.max(packages.map(pkg => pkg.name.length));
   console.info(`\nExpecting ${packages.length} packages:\n`);
-
-  _.groupBy(packages, pkg => pkg.whenToExpect());
+  console.info(table.toString());
 }
 
 const command = process.argv[2];
 switch (command) {
-  case "s":
-  case "status":
-    showStatus();
+  case "l":
+  case "list":
+    listPackages();
     break;
 }
