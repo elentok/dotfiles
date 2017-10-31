3ffc9a96e5a033af472e7f8bfd7e8ccfb333029d
David Elentok <3david@gmail.com>
2017-10-31 16:51:24 +0200

    Pkgs: show iparcel history

--- scripts/packages/views/app.js
+++ scripts/packages/views/app.js
@@ -1,5 +1,17 @@
 /* globals document fetch */
 
+function dom(tagName, className, contents) {
+  const el = document.createElement(tagName);
+  if (className != null) {
+    el.className = className;
+  }
+  if (contents != null) {
+    el.innerText = contents;
+  }
+
+  return el;
+}
+
 class TrackingNumber {
   constructor(el) {
     this.el = el;
@@ -11,9 +23,30 @@ class TrackingNumber {
     return fetch(`/track/${this.el.dataset.number}`)
       .then(response => response.json())
       .then(json => {
-        resultEl.innerText = json.text;
+        if (json.history != null) {
+          resultEl.appendChild(this._renderHistoryTable(json.history));
+        } else if (json.text != null) {
+          resultEl.innerText = json.text;
+        }
         return json;
       });
+  }
+
+  _renderHistoryTable(history) {
+    const table = dom("table");
+
+    history.forEach(item => {
+      const tr = dom("tr");
+
+      const date = dom("td", null, item.date);
+      const text = dom("td", null, item.text);
+      tr.appendChild(date);
+      tr.appendChild(text);
+
+      table.appendChild(tr);
+    });
+
+    return table;
   }
 }
 
@@ -29,12 +62,22 @@ class Order {
     Promise.all(
       this.trackingNumbers.map(number => number.track())
     ).then(results => {
+      let finalStatus = null;
+
       results.forEach(result => {
         if (result.status) {
+          if (result.status !== "unknown") finalStatus = result.status;
           const status = result.status.toLowerCase().replace(/ /g, "-");
           this.el.classList.add(`o-order--${status}`);
         }
       });
+
+      if (finalStatus != null) {
+        this.el.parentElement.insertBefore(
+          this.el,
+          this.el.parentElement.childNodes[0]
+        );
+      }
     });
   }
 }
