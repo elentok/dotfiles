e20eed62db7b91846010a34b407fa4299606fa97
David Elentok <3david@gmail.com>
2017-11-01 11:18:25 +0200

    Pkgs: add importer

--- scripts/packages/order-repo.js
+++ scripts/packages/order-repo.js
@@ -4,13 +4,63 @@ const yaml = require("js-yaml");
 const Order = require("./order");
 const _ = require("underscore");
 
+const ORDERS_FILENAME = path.join(process.env.HOME, ".packages");
+
 const OrderRepo = {
-  load() {
-    const orders = yaml
-      .safeLoad(fs.readFileSync(path.join(process.env.HOME, ".packages")))
+  all() {
+    if (this._all == null) this._load();
+    return this._all;
+  },
+
+  reset() {
+    this._load();
+  },
+
+  _load() {
+    this._all = yaml
+      .safeLoad(fs.readFileSync(ORDERS_FILENAME))
       .map(order => new Order(order));
 
-    return _.sortBy(orders, order => order.date);
+    this._byId = {};
+    this._all.forEach(o => (this._byId[o.id] = o));
+
+    this._sort();
+  },
+
+  _sort() {
+    this._all = _.sortBy(this._all, order => order.date);
+    this._all.reverse();
+  },
+
+  findById(id) {
+    if (this._all == null) this._load();
+    return this._byId[id];
+  },
+
+  add(order, { save = false }) {
+    if (!(order instanceof Order)) {
+      order = new Order(order);
+    }
+
+    this.all().push(order);
+    this._byId[order.id] = order;
+    if (save) this.save();
+  },
+
+  save() {
+    if (this._all == null) return;
+    fs.writeFileSync(ORDERS_FILENAME, this._dump());
+  },
+
+  _dump() {
+    this._sort();
+    const raw = this._all.map(o => o.toJSON());
+    try {
+      return yaml.safeDump(raw);
+    } catch (e) {
+      console.error("Error generating YAML:", e);
+      throw e;
+    }
   }
 };
 
