4aceb615f9535f24a081c347716f9f1d7215f768
David Elentok <3david@gmail.com>
2017-11-02 09:07:00 +0200

    Fix basic.zsh

--- zsh/basic.zsh
+++ zsh/basic.zsh
@@ -10,18 +10,4 @@ directory="%F{blue}%~%f"
 export PROMPT="
 %F{red}$(whoami) at $(hostname)%f
 $directory
-o vim: foldmethod=marker
-
-# Prompt {{{1
-success="%F{green}❯%f"
-error="%F{red}❯%f"
-exit_code="%(?.$success.$error) "
-
-directory="%F{blue}%~%f"
-
-export PROMPT="
-%F{blue}__________________________________%f
-%F{red}$(whoami) at $(hostname)%f
-$directory
-$exit_code"
 $exit_code"
