a83e08ab10a89716dbe128e3509697f303e8b20f
David Elentok <3david@gmail.com>
2017-11-04 14:31:44 +0200

    Improve the bookmarklets

--- bookmarklets/aliexpress.js
+++ bookmarklets/aliexpress.js
@@ -1,17 +1,16 @@
-/* eslint es6: false */
-/* global copy document */
+/* global copy document window */
 
 (function() {
-  var STATUS_MAPPING = {
+  const STATUS_MAPPING = {
     "awaiting shipment": "processing",
     "awaiting delivery": "shipped"
   };
 
   function parseItem(el) {
-    var img = el.querySelector(".product-sets .pic img");
-    var a = el.querySelector(".product-title a");
+    const img = el.querySelector(".product-sets .pic img");
+    const a = el.querySelector(".product-title a");
 
-    var amountItems = el.querySelectorAll(".product-amount span");
+    const amountItems = el.querySelectorAll(".product-amount span");
 
     return {
       img: img.src.replace("50x50", "200x200"),
@@ -29,16 +28,16 @@
   }
 
   function parseOrder(el) {
-    var head = el.querySelector(".order-head");
+    const head = el.querySelector(".order-head");
 
-    var orderInfo = head.querySelectorAll(".order-info .info-body");
-    var id = orderInfo[0];
-    var date = orderInfo[1];
+    const orderInfo = head.querySelectorAll(".order-info .info-body");
+    const id = orderInfo[0];
+    const date = orderInfo[1];
 
-    var sellerName = head.querySelector(".store-info .first-row .info-body");
-    var amount = el.querySelector(".order-amount .amount-num");
-    var status = el.querySelector(".order-status .f-left");
-    var items = Array.from(el.querySelectorAll(".order-body")).map(parseItem);
+    const sellerName = head.querySelector(".store-info .first-row .info-body");
+    const amount = el.querySelector(".order-amount .amount-num");
+    const status = el.querySelector(".order-status .f-left");
+    const items = Array.from(el.querySelectorAll(".order-body")).map(parseItem);
 
     return {
       id: id.innerText,
@@ -52,14 +51,14 @@
   }
 
   function showPopup(text) {
-    var el = document.createElement("div");
+    const el = document.createElement("div");
     el.style.position = "fixed";
-    el.style.left = "15%";
-    el.style.right = "15%";
-    el.style.top = "15%";
-    el.style.bottom = "15%";
+    el.style.left = "30%";
+    el.style.right = "30%";
+    el.style.top = "30%";
+    el.style.bottom = "30%";
 
-    var button = document.createElement("button");
+    const button = document.createElement("button");
     button.innerText = "Close";
     button.style.fontSize = "20px";
     button.style.fontWeight = "bold";
@@ -68,18 +67,22 @@
     });
     el.appendChild(button);
 
-    var textarea = document.createElement("textarea");
+    const textarea = document.createElement("textarea");
     textarea.style.width = "100%";
     textarea.style.height = "90%";
     textarea.value = text;
     el.appendChild(textarea);
 
     document.body.appendChild(el);
+
+    textarea.select();
+    document.execCommand("copy");
   }
 
-  var orders = (window.orders = Array.from(
+  const orders = (window.orders = Array.from(
     document.querySelectorAll(".order-item-wraper")
   ).map(parseOrder));
 
-  showPopup(JSON.stringify(orders, null, 2));
+  const json = JSON.stringify(orders, null, 2);
+  showPopup(json);
 })();
