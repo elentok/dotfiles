5155eb0fa155a784234f5ed99bb705bc4ae60e20
David Elentok <3david@gmail.com>
2017-11-04 14:32:15 +0200

    Pkgs: ignore cancelled/finished orders

--- scripts/packages/importer.js
+++ scripts/packages/importer.js
@@ -1,6 +1,8 @@
 const OrderRepo = require("./order-repo");
 const { paste } = require("copy-paste");
 
+const STATUSES_TO_IGNORE = ["closed", "finished"];
+
 const Importer = {
   importFromClipboard() {
     const ordersToImport = JSON.parse(paste());
@@ -10,8 +12,12 @@ const Importer = {
         console.info(`[IMPORT] Updating ${orderToImport.id}`);
         order.update(orderToImport);
       } else {
-        console.info(`[IMPORT] Adding ${orderToImport.id}`);
-        OrderRepo.add(orderToImport, { save: false });
+        if (
+          STATUSES_TO_IGNORE.indexOf(orderToImport.status.toLowerCase()) === -1
+        ) {
+          console.info(`[IMPORT] Adding ${orderToImport.id}`);
+          OrderRepo.add(orderToImport, { save: false });
+        }
       }
     });
     OrderRepo.save();
