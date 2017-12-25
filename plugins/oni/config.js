e7a33defdd5ca272c6e187dc2480b098e52616f9
David Elentok <3david@gmail.com>
2017-12-25 14:54:37 +0200

    Add oni config

---
+++ plugins/oni/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'oni.useDefaultConfig': true,
+  'oni.loadInitVim': true,
+  'editor.fontSize': '16px',
+  'editor.fontFamily': 'mononoki Nerd Font Mono',
+  'tabs.mode': 'tabs'
+}
