69760306831b7145b178e330fca33a439576e22d
David Elentok <3david@gmail.com>
2017-12-28 22:20:15 +0200

    Optimize zsh compinit

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,7 +1,22 @@
 # vim: foldmethod=marker
 
 # Zsh completion {{{1
-autoload -U compinit && compinit
+autoload -Uz compinit
+
+# On slow systems, checking the cached .zcompdump file to see if it must be
+# regenerated adds a noticable delay to zsh startup.  This little hack restricts
+# it to once a day.  It should be pasted into your own completion file.
+#
+# The globbing is a little complicated here:
+# - '#q' is an explicit glob qualifier that makes globbing work within zsh's [[ ]] construct.
+# - 'N' makes the glob pattern evaluate to nothing when it doesn't match (rather than throw a globbing error)
+# - '.' matches "regular files"
+# - 'mh+24' matches files (or directories or whatever) that are older than 24 hours.
+if [[ -n ${ZDOTDIR}/.zcompdump(#qN.mh+24) ]]; then
+  compinit
+else
+  compinit -C
+fi
 
 # Use caching to make completion for cammands such as dpkg and apt usable.
 zstyle ':completion::complete:*' use-cache on
@@ -24,49 +39,3 @@ zstyle ':completion:*:default' list-prompt '%S%M matches%s'
 zstyle ':completion:*' format ' %F{yellow}-- %d --%f'
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*' verbose yes
-
-# FZF Completion: capistrano {{{1
-_fzf_complete_bec() {
-  _fzf_complete "" "$@" < <(
-    if [ "$(echo $@ | wc -w)" = "1" ]; then
-      __cap-complete-stage
-    else
-      __cap-complete-action
-    fi
-  )
-}
-
-__cap-complete-stage() {
-  if [ -e 'config/capistrano/stages' ]; then
-    /bin/ls -1 config/capistrano/stages | sed 's/\.rb//'
-  fi
-}
-
-__cap-complete-action() {
-  if [ ! -e .capistrano-cache ]; then
-    be cap -T | grep '^cap ' | sed 's/^cap //' > .capistrano-cache
-  fi
-
-  cat .capistrano-cache
-}
-
-_fzf_complete_bec_post() {
-  awk '{print $1}'
-}
-
-# FZF Completion: npm {{{1
-alias nr="npm run"
-
-_fzf_complete_nr() {
-  _fzf_complete "" "$@" < <(
-    _npm_list_scripts
-  )
-}
-
-_npm_list_scripts() {
-  node -e 'const s = require("./package.json").scripts; Object.keys(s).forEach(key => { console.log(`${key}: ${s[key]}`) })'
-}
-
-_fzf_complete_nr_post() {
-  IFS=": " awk '{print $1}' | sed 's/:$//'
-}
