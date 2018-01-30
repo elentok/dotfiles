38230d309a53e9b18e0ae9d51a7bc32e5a09221a
David Elentok <3david@gmail.com>
2018-01-30 21:58:59 +0200

    WSL: add 'open' and missing PATH entries

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -99,7 +99,13 @@ function p() {
   fi
 }
 
-if is_linux; then
+if is_wsl; then
+  function open() {
+    filepath=$(echo $@ | sed -E 's#/mnt/([a-z])#\1:#' | tr '/' '\\')
+    echo "Running: powershell.exe Start $filepath"
+    powershell.exe "Start $filepath"
+  }
+elif is_linux; then
   function open() {
     xdg-open "$@"
   }
