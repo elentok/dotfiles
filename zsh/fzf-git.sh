9f7c9f2efc33cf4b0fba06fd67d3dbda6584d19a
David Elentok <3david@gmail.com>
2018-02-04 23:01:00 +0200

    Add fzf-git

---
+++ zsh/fzf-git.sh
@@ -0,0 +1,73 @@
+# See https://gist.github.com/junegunn/8b572b8d4b5eddd8b85e5f4d40f17236
+
+is_in_git_repo() {
+  git rev-parse HEAD > /dev/null 2>&1
+}
+
+gf() {
+  is_in_git_repo || return
+  git -c color.status=always status --short |
+  fzf -m --ansi --nth 2..,.. \
+    --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' |
+  cut -c4- | sed 's/.* -> //'
+}
+
+gb() {
+  is_in_git_repo || return
+  git branch -a --color=always | grep -v '/HEAD\s' | sort |
+  fzf --ansi --multi --tac --preview-window right:70% \
+    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -'$LINES |
+  sed 's/^..//' | cut -d' ' -f1 |
+  sed 's#^remotes/##'
+}
+
+gt() {
+  is_in_git_repo || return
+  git tag --sort -version:refname |
+  fzf --multi --preview-window right:70% \
+    --preview 'git show --color=always {} | head -'$LINES
+}
+
+gh() {
+  is_in_git_repo || return
+  git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
+  fzf --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
+    --header 'Press CTRL-S to toggle sort' \
+    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -'$LINES |
+  grep -o "[a-f0-9]\{7,\}"
+}
+
+gr() {
+  is_in_git_repo || return
+  git remote -v | awk '{print $1 "\t" $2}' | uniq |
+  fzf --tac \
+    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1} | head -200' |
+  cut -d$'\t' -f1
+}
+
+if is_bash; then
+  bind '"\er": redraw-current-line'
+  bind '"\C-g\C-f": "$(gf)\e\C-e\er"'
+  bind '"\C-g\C-b": "$(gb)\e\C-e\er"'
+  bind '"\C-g\C-t": "$(gt)\e\C-e\er"'
+  bind '"\C-g\C-h": "$(gh)\e\C-e\er"'
+  bind '"\C-g\C-r": "$(gr)\e\C-e\er"'
+else
+	join-lines() {
+		local item
+		while read item; do
+			echo -n "${(q)item} "
+		done
+	}
+
+	bind-git-helper() {
+		local char
+		for c in $@; do
+			eval "fzf-g$c-widget() { local result=\$(g$c | join-lines); zle reset-prompt; LBUFFER+=\$result }"
+			eval "zle -N fzf-g$c-widget"
+			eval "bindkey '^g^$c' fzf-g$c-widget"
+		done
+	}
+	bind-git-helper f b t r h
+	unset -f bind-git-helper
+fi
