53cdb943a2f5680bd767eec0f8abfc210087e720
David Elentok <3david@gmail.com>
2018-03-12 20:32:55 +0200

    Dashboard: better UI

--- scripts/dashboard/app.js
+++ scripts/dashboard/app.js
@@ -1,17 +1,42 @@
 /* globals document moment */
+/* eslint-disable no-var */
 
-const ui = {
+var ui = {
   date: document.querySelector('.date'),
-  time: document.querySelector('.time')
+  time: document.querySelector('.time'),
+  calendarBody: document.querySelector('.calendar tbody')
 }
 
 function initialize() {
   setTimeout(update, 200)
+  renderCalendar()
 }
 
 function update() {
   ui.time.innerText = moment().format('HH:mm')
   ui.date.innerText = moment().format('dddd, MMMM DD')
+}
+
+function renderCalendar() {
+  var thisMonth = moment().month()
+  var day = moment().startOf('week')
+  var today = moment()
+
+  ui.calendarBody.innerHTML = ''
+
+  while (day.month() <= thisMonth) {
+    var tr = document.createElement('tr')
+    for (var i = 0; i < 7; i++) {
+      var td = document.createElement('td')
+      if (day.month() === thisMonth) {
+        td.innerText = day.date()
+        if (day.isSame(today, 'day')) td.className = 'is-today'
+      }
+      tr.appendChild(td)
+      day.add(1, 'day')
+    }
+    ui.calendarBody.appendChild(tr)
+  }
 }
 
 initialize()
