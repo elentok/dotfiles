f2fee0c77766311705acf7929b35a8b0762afa3d
David Elentok <3david@gmail.com>
2018-05-22 21:01:03 +0300

    Oni: switch to typescript config

--- plugins/oni/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'oni.useDefaultConfig': true,
-  'oni.loadInitVim': true,
-  'editor.fontSize': '16px',
-  'editor.fontFamily': 'mononoki Nerd Font Mono',
-  'tabs.mode': 'tabs'
-}
