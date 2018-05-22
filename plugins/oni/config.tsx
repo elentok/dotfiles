c4a23fe3d9d64d1b1364409cf2f62e56147fb67d
David Elentok <3david@gmail.com>
2018-05-22 21:01:03 +0300

    Oni: switch to typescript config

---
+++ plugins/oni/config.tsx
@@ -0,0 +1,39 @@
+
+import * as React from "react"
+import * as Oni from "oni-api"
+
+export const activate = (oni: Oni.Plugin.Api) => {
+    console.log("config activated")
+
+    // Input
+    //
+    // Add input bindings here:
+    //
+    oni.input.bind("<c-enter>", () => console.log("Control+Enter was pressed"))
+
+    //
+    // Or remove the default bindings here by uncommenting the below line:
+    //
+    // oni.input.unbind("<c-p>")
+
+}
+
+export const deactivate = (oni: Oni.Plugin.Api) => {
+    console.log("config deactivated")
+}
+
+export const configuration = {
+    //add custom config here, such as
+
+    "ui.colorscheme": "nord",
+
+    //"oni.useDefaultConfig": true,
+    //"oni.bookmarks": ["~/Documents"],
+    //"oni.loadInitVim": false,
+    "editor.fontSize": "14px",
+    //"editor.fontFamily": "Monaco",
+
+    // UI customizations
+    "ui.animations.enabled": true,
+    "ui.fontSmoothing": "auto",
+}
