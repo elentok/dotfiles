b0638e9591b7b23e22eee7e727ae49e334381b8a
David Elentok <3david@gmail.com>
2018-05-26 23:13:03 -0700

    Windows: move git setup to main setup.ps1

--- windows/powershell/setup.ps1
+++ windows/powershell/setup.ps1
@@ -10,7 +10,3 @@ Write-Output 'Installing powershell profile.ps1'
 $script = Join-Path $PSScriptRoot 'profile.ps1'
 $output = Join-Path ([System.Environment]::GetFolderPath('MyDocuments')) 'WindowsPowerShell/profile.ps1'
 Write-Output ". $script" | Out-File -FilePath $output
-
-Write-Output 'Installing gitconfig'
-$gitconfig = Join-Path $DOTF 'plugins/git/gitconfig'
-git config --global include.path $gitconfig
