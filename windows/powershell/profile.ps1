dae66551cc8d1284a871c2e71ed02438db12a83b
David Elentok <3david@gmail.com>
2018-05-28 18:29:13 -0700

    PS: support <c-r> and grep

--- windows/powershell/profile.ps1
+++ windows/powershell/profile.ps1
@@ -1,8 +1,12 @@
 if (($host.Name -eq 'ConsoleHost') -Or ($host.Name -eq 'Visual Studio Code Host')) {
+  Write-Output 'Loading dotfiles profile.ps1'
+
   Import-Module PSReadLine
   Set-PSReadlineOption -EditMode Vi
+  Set-PSReadlineKeyHandler -Chord Ctrl+R -Function ReverseSearchHistory
 
   Set-Alias g git
   Set-Alias chi 'choco install -y'
   Set-Alias chs 'choco search'
+  Set-Alias grep Select-String
 }
