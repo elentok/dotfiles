597a82a11896589aec6218711aa03a9aebc8b321
David Elentok <3david@gmail.com>
2018-06-01 15:50:23 +0100

    Windows: improved setup

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,3 +1,5 @@
+Set-ExecutionPolicy RemoteSigned
+
 if (!(Get-Command choco -errorAction SilentlyContinue)) {
   Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
@@ -5,11 +7,17 @@ if (!(Get-Command choco -errorAction SilentlyContinue)) {
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 
 choco install -y nodejs googlechrome 7zip git vscode keepass telegram doublecmd libreoffice vlc `
-  irfanview irfanviewplugins Everything f.lux
+  irfanview irfanviewplugins Everything
+
+Write-Output 'Installing yarn'
+npm i -g yarn
 
 Write-Output 'Setting up git'
+$DOTF = Resolve-Path (Join-Path $PSScriptRoot '..')
 $gitconfig = Join-Path $DOTF 'plugins/git/gitconfig'
 git config --global include.path $gitconfig
 git config --global user.name 'David Elentok'
 git config --global user.email '3david@gmail.com'
 git config --global github.user 'elentok'
+
+. powershell\setup.ps1
