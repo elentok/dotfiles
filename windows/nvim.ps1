75891b627b2a0cb3be07da522bd30faf4e7a535d
David Elentok <3david@gmail.com>
2018-08-04 21:09:06 +0300

    Windows: neovim fixes

--- windows/nvim.ps1
+++ windows/nvim.ps1
@@ -1,11 +1,5 @@
 $DOTF = Resolve-Path (Join-Path $PSScriptRoot '..')
-$mainInitVim = Join-Path $DOTF 'nvim/init.vim'
 
 $nvimConfigDir = Join-Path $env:LOCALAPPDATA 'nvim'
-if (!(Test-Path -Path $nvimConfigDir)) {
-  mkdir $nvimConfigDir
-}
 
-$localInitVim = Join-Path $nvimConfigDir 'init.vim'
-
-Write-Output "source $mainInitVim" | Out-File -FilePath $localInitVim
+New-Item -Path $nvimConfigDir -ItemType SymbolicLink -Value $DOTF\nvim
--- windows/setup.ps1
+++ windows/setup.ps1
@@ -4,10 +4,16 @@ if (!(Get-Command choco -errorAction SilentlyContinue)) {
   Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
+if (!(Get-Command scoop -errorAction SilentlyContinue)) {
+  Invoke-Expression (new-object net.webclient).downloadstring('https://get.scoop.sh')
+}
+
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 
 choco install -y nodejs googlechrome 7zip git vscode keepass telegram doublecmd libreoffice vlc `
   irfanview irfanviewplugins Everything transgui procexp wsltty pandoc
+
+scoop install neovim fzf ripgrep git
 
 Write-Output 'Enabling SSH Agent'
 Set-Service -name "ssh-agent" -startuptype "automatic"
