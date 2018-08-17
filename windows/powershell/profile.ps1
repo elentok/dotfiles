bc7269468fd8cc962999fa13f98e8af1420de28b
David Elentok <3david@gmail.com>
2018-08-17 15:20:13 +0300

    Windows: powershell fixes

--- windows/powershell/profile.ps1
+++ windows/powershell/profile.ps1
@@ -2,10 +2,23 @@ if (($host.Name -eq 'ConsoleHost') -Or ($host.Name -eq 'Visual Studio Code Host'
   Write-Output 'Loading dotfiles profile.ps1'
 
   Import-Module PSReadLine
-  Set-PSReadlineOption -EditMode Vi
-  Set-PSReadlineKeyHandler -Chord Ctrl+R -Function ReverseSearchHistory
+  # Set-PSReadlineOption -EditMode Vi
+  # Set-PSReadlineKeyHandler -Chord Ctrl+R -Function ReverseSearchHistory
+  # Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+  # Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+  # Set-PSReadLineKeyHandler -Key Tab -Function Complete
+  #
+  function QuitReplacement{
+    Invoke-command -ScriptBlock {exit}
+  }
 
+  function TigStatus{
+    Invoke-command -ScriptBlock {tig status}
+  }
+
+  Set-Alias x QuitReplacement
   Set-Alias g git
+  Set-Alias ts TigStatus
   Set-Alias chi 'choco install -y'
   Set-Alias chs 'choco search'
   Set-Alias grep Select-String
