fea47408d06d61aff20a9b13cf1fd4d4e3358c53
David Elentok <3david@gmail.com>
2018-12-20 22:18:28 +0200

    Oni: make it work

--- plugins/oni/config.tsx
+++ plugins/oni/config.tsx
@@ -1,38 +1,43 @@
-import * as Oni from 'oni-api'
-import * as path from 'path'
-import * as React from 'react'
+import * as Oni from "oni-api";
+import * as path from "path";
+import * as React from "react";
 
-const HOME: string = process.env.HOME
+// const HOME: string = process.env.HOME;
 
 export const activate = (oni: Oni.Plugin.Api) => {
-  console.info('Oni config activated')
+  console.info("Oni config activated");
 
-  oni.input.bind('<c-enter>', () => console.info('Control+Enter was pressed'))
-  oni.input.bind(['<enter>', '<tab>'], 'contextMenu.select')
+  oni.input.bind("<c-enter>", () => console.info("Control+Enter was pressed"));
+  oni.input.bind(["<enter>", "<tab>"], "contextMenu.select");
 
   // Remove default bindings:
   // oni.input.unbind("<c-p>")
-}
+};
 
 export const deactivate = (oni: Oni.Plugin.Api) => {
-  console.info('Oni config deactivated')
-}
+  console.info("Oni config deactivated");
+};
 
 export const configuration = {
-  'editor.fontFamily': 'mononokiNerdFontCM-Regular',
-  'editor.fontSize': '15px',
-  'editor.renderer': 'webgl',
-  'environment.additionalPaths': [
-    path.resolve(HOME, '.dotfiles', 'scripts'),
-    path.resolve(HOME, '.dotlocal', 'bin'),
-    path.resolve(HOME, '.yarn', 'bin'),
-    path.resolve(HOME, '.n', 'bin'),
-    path.resolve(HOME, 'bin'),
-    '/usr/local/bin',
-    '/usr/bin'
+  "editor.fontFamily": "mononokiNerdFontCM-Regular",
+  "editor.fontSize": "15px",
+  "editor.renderer": "webgl",
+  "environment.additionalPaths": [
+    "$HOME/.dotfiles/scripts",
+    "$HOME/.dotlocal/bin",
+    "$HOME/.n/bin",
+    "$HOME/bin",
+    // path.resolve(HOME, ".dotfiles", "scripts"),
+    // path.resolve(HOME, ".dotlocal", "bin"),
+    // path.resolve(HOME, ".yarn", "bin"),
+    // path.resolve(HOME, ".n", "bin"),
+    // path.resolve(HOME, "bin"),
+    "/usr/local/bin",
+    "/usr/bin"
   ],
-  'oni.useDefaultConfig': false, // the default configs clash with my own
-  'sidebar.default.open': false,
-  'ui.colorscheme': 'nord',
-  'ui.fontSize': '15px'
-}
+  "oni.useDefaultConfig": false, // the default configs clash with my own
+  "sidebar.default.open": false,
+  "ui.colorscheme": "nord",
+  "ui.fontSize": "15px",
+  "tabs.mode": "spaces"
+};
