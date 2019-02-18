4bf2e23e682c3b702b2661b92cccf0a1ac216a52
David Elentok <elentok@google.com>
2019-02-18 16:12:22 +0200

    Git: start migrating code to ts

---
+++ lib/ts/git/branch.ts
@@ -0,0 +1,69 @@
+import { IBranch, IRepo } from './types'
+
+abstract class Branch implements IBranch {
+  public gitName: string
+  private cachedHash?: string
+
+  constructor(protected repo: IRepo, public name: string) {
+    this.gitName = name
+  }
+
+  public hash(): string {
+    if (this.cachedHash == null) {
+      this.cachedHash = this.repo.git(`log -1 --pretty=%h ${this.gitName}`, { silent: true })
+    }
+    return this.cachedHash
+  }
+
+  public resetHash(): void {
+    this.cachedHash = null
+  }
+
+  public toString(): string {
+    return this.gitName
+  }
+
+  public isProtected(): boolean {
+    return ['master', 'develop', 'protected'].indexOf(this.name) !== null
+  }
+}
+
+export class LocalBranch extends Branch {
+  private remoteBranches: IBranch[] = []
+
+  public hasRemotes(): boolean {
+    return this.remoteBranches.length > 0
+  }
+
+  public destroy({ force = false } = {}): string {
+    const arg = force ? '-D' : '-d'
+    return this.repo.git(`branch ${arg} ${this.name}`)
+  }
+}
+
+class RemoteBranch extends Branch {
+  public remoteName: string
+
+  constructor(repo: IRepo, name: string) {
+    super(repo, name)
+
+    name = name.replace(/ ->.*/g, '')
+    const parts = name.split('/', 3)
+    this.remoteName = parts[1]
+    this.name = parts[2]
+    this.gitName = `${this.remoteName}/${this.name}`
+  }
+
+  public destroy(): string {
+    return this.repo.git(`push --delete ${this.remoteName} ${this.name}`)
+  }
+}
+
+export function parseBranchLine(line: string, repo: IRepo): IBranch {
+  line = line.replace(/^\*/g, '').trim()
+  if (line.match(/^remotes\//)) {
+    return new RemoteBranch(repo, line)
+  } else {
+    return new LocalBranch(repo, line)
+  }
+}
