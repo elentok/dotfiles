5eed53a04bd6430619512b7451c231cd93e36384
David Elentok <3david@gmail.com>
2019-04-16 13:34:53 +0300

    fix(scripts): finalize ts version of rename

--- lib/ts/rename-cli.ts
+++ lib/ts/rename-cli.ts
@@ -1,72 +1,29 @@
 import * as program from 'commander'
-import * as path from 'path'
-import * as fs from 'fs'
-import chalk from 'chalk'
-import { execSync } from 'child_process'
+import { findRenameables } from './renameable'
+import { confirm } from './utils'
 
 function main() {
   program
     .arguments('<pattern> <replacement> <file...>')
     .option('-y, --yes', "Don't ask for confirmation")
-    .action((pattern: string, replacement: string, files: string[], args: IArgs) => {
-      const matches = findMatches(pattern, replacement, files)
-      if (matches.length === 0) {
-        console.info('No matches')
+    .action(async (pattern: string, replacement: string, files: string[], args: IArgs) => {
+      const renameables = findRenameables(pattern, replacement, files)
+      if (renameables.length === 0) {
+        console.info('No files to rename')
         return
+      }
+
+      renameables.forEach(r => r.print('   '))
+
+      if (args.yes || (await confirm('Rename?'))) {
+        renameables.forEach(r => {
+          r.print('Renaming ')
+          r.rename()
+        })
       }
     })
 
   program.parse(process.argv)
-}
-
-function findMatches(pattern: string, replacement: string, files: string[]): Match[] {
-  const re = new RegExp(pattern, 'ig')
-  const matches: Match[] = []
-
-  files.forEach(filename => {
-    const basename = path.basename(filename)
-    const newBasename = basename.replace(re, replacement)
-
-    if (basename !== newBasename) matches.push(new Match(filename, newBasename))
-  })
-
-  return matches
-}
-
-class Match {
-  public fromDirname: string
-  public fromBasename: string
-  public toFullpath: string
-
-  constructor(public fromFullpath: string, public toBasename: string) {
-    this.fromDirname = path.dirname(fromFullpath)
-    this.fromBasename = path.basename(fromFullpath)
-    this.toFullpath = path.join(this.fromDirname, toBasename)
-  }
-
-  public print(prefix = ''): void {
-    console.info(`${prefix}${chalk.gray(this.fromDirname)}/${this.fromBasename}`)
-    console.info(`${this.indent(prefix)} => ${chalk.blue(this.toBasename)}`)
-  }
-
-  public rename(): void {
-    if (this.isInGitRepo()) {
-      execSync(`git mv '${this.fromFullpath}' '${this.toFullpath}'`)
-    } else {
-      fs.renameSync(this.fromDirname, this.toFullpath)
-    }
-  }
-
-  private indent(prefix: string): string {
-    let text = ''
-    const indentWidth = Math.max(prefix.length + this.fromDirname.length - 3, 0)
-    while (text.length < indentWidth) text += ' '
-    return text
-  }
-
-  private isInGitRepo(): boolean {
-    return execSync(`git ls-files --error-unmatch ${this.fromFullpath} > /dev/null 2>&1`)
-  }
 }
 
 interface IArgs {
