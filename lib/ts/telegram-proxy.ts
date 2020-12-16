e8b8002797ee137e0a949b3f05112f2867ce8edc
David Elentok <3david@gmail.com>
2020-12-16 10:47:10 +0200

    fix(telegram-proxy): use HTML mode by default

--- lib/ts/telegram-proxy.ts
+++ lib/ts/telegram-proxy.ts
@@ -9,39 +9,54 @@ const MQTT_HOST = getConfig('telegram_mqtt_host') || 'localhost'
 const BOT_TOKEN = getConfigOrDie('telegram_bot_token')
 const CHAT_ID = getConfigOrDie('telegram_chat_id')
 
+enum ParseMode {
+  HTML = 'HTML',
+  MarkdownV2 = 'MarkdownV2'
+}
+
 function main(): void {
   const client = mqtt.connect(`mqtt://${MQTT_HOST}`)
   client.on('connect', () => client.subscribe('telegram:send'))
   client.on('message', (topic: string, message: Buffer) => {
     if (topic === 'telegram:send') {
-      sendToTelegram(message.toString())
+      sendToTelegram(message.toString(), ParseMode.HTML)
     }
   })
 
   const app = express()
   app.use(bodyParser.urlencoded({ extended: true }))
   app.post('/send', (req: express.Request, res: express.Response) => {
-    console.info('Sending message', req.body.message)
-    sendToTelegram(req.body.message).then(() => res.send('OK'))
+    const parseMode = parseParseMode(req.body.parseMode)
+    console.info(`Sending message ${req.body.message} with parse mode ${parseMode}`)
+    sendToTelegram(req.body.message, parseMode).then(() => res.send('OK'))
   })
   app.listen(10000, () => console.info('Telegram proxy listening on port 10000'))
 
-  sendToTelegram('*Telegram Proxy Started*')
+  sendToTelegram('*Telegram Proxy Started*', ParseMode.MarkdownV2)
 }
 
-async function sendToTelegram(message: string): Promise<void | AxiosResponse<any>> {
+async function sendToTelegram(
+  message: string,
+  parseMode = ParseMode.HTML
+): Promise<void | AxiosResponse<any>> {
   const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`
-  // eslint-disable-next-line camelcase
-  const body = new URLSearchParams()
-  body.set('chat_id', CHAT_ID)
-  body.set('text', message)
-  body.set('parse_mode', 'MarkdownV2')
+  const body = {
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    chat_id: CHAT_ID,
+    text: message,
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    parse_mode: parseMode
+  }
 
-  console.info('Making request to', url, 'with', body.toString())
-  return axios.post(url, body.toString()).catch(err => {
+  console.info('Making request to', url, 'with', JSON.stringify(body))
+  return axios.post(url, body).catch(err => {
     console.error('Error sending message:', err.message)
   })
 }
 
-main()
+function parseParseMode(value?: string): ParseMode {
+  if (value == ParseMode.MarkdownV2) return ParseMode.MarkdownV2
+  return ParseMode.HTML
+}
 
+main()
