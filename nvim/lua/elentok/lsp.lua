e52cb2e7e2da95c61d2f61959fb3d8f17de321a8
David Elentok <3david@gmail.com>
2020-12-31 14:20:31 +0200

    fix(nvim): lsp: add shellcheck and shfmt

--- nvim/lua/elentok/lsp.lua
+++ nvim/lua/elentok/lsp.lua
@@ -37,6 +37,46 @@ local function lspConfigSetup ()
       },
     }
   }
+
+  lspconfig.diagnosticls.setup{
+    filetypes = {"sh"},
+    init_options = {
+      filetypes = {
+        sh = "shellcheck",
+      },
+      formatFiletypes = {
+        sh = "shfmt"
+      },
+      linters = {
+        shellcheck = {
+          command = "shellcheck",
+          debounce = 100,
+          args = { "--format", "json", "-" },
+          sourceName = "shellcheck",
+          parseJson = {
+            line = "line",
+            column = "column",
+            endLine = "endLine",
+            endColumn = "endColumn",
+            message = "${message} [${code}]",
+            security = "level"
+          },
+          securities = {
+            error = "error",
+            warning = "warning",
+            info = "info",
+            style = "hint"
+          },
+        },
+      },
+      formatters = {
+        shfmt = {
+          command = "shfmt",
+          args = {"-i", "2", "-bn", "-ci", "-sr"},
+        }
+      }
+    }
+  }
 end
 
 function LspCompletionOnAttach ()
