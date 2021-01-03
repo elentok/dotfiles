e5ec32b3e149f4da3dfcd4e956e0f4bda988a9ba
David Elentok <elentok@google.com>
2021-01-03 12:16:21 +0200

    chore(nvim): lsp improvements

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,23 +1,4 @@
 vim.api.nvim_command('source $DOTF/nvim/init-legacy.vim')
 
-require'nvim-treesitter.configs'.setup{
-  ensure_installed = "maintained",
-  highlight = {
-    enable = true
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "gnn",
-      node_incremental = "grn",
-      scope_incremental = "grc",
-      node_decremental = "grm",
-    },
-  },
-  indent = {
-    enable = true
-  },
-}
-
-vim.api.nvim_set_option('foldmethod', 'expr')
-vim.api.nvim_set_option('foldexpr', 'nvim_treesitter#foldexpr()')
+require'elentok/lsp'
+require'elentok/treesitter'
