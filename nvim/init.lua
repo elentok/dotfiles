2b879df061209b765b3016434194296a74712187
David Elentok <3david@gmail.com>
2021-05-23 09:42:55 +0300

    fix(nvim): add lua/elentok/init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,3 @@
 vim.api.nvim_command('source $DOTF/nvim/init-legacy.vim')
 
-require'elentok/packer-bootstrap'
-require'elentok/plugins'
-require'elentok/lsp'
-require'elentok/treesitter'
-require'elentok/keys'
-
 require('nvim-autopairs').setup()
