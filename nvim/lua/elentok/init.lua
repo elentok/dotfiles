72e8be5a8099c358b9723a92ebb9bedf0cbdd571
David Elentok <elentok@google.com>
2021-07-27 12:10:37 +0300

    feat(nvim): add DotReload command
    Reloads all loaded dotfiles lua files.

--- nvim/lua/elentok/init.lua
+++ nvim/lua/elentok/init.lua
@@ -8,6 +8,7 @@ require'elentok/keys'
 require'elentok/autoformat'
 require'elentok/telescope'
 require'elentok/statusline'
+require'elentok/commands'
 
 local util = require'elentok/util'
 util.safe_require('elentok-local')
