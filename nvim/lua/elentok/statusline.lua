b7f002a2fc2cb964f2a2feb034197002af70668e
David Elentok <elentok@google.com>
2021-07-27 12:10:11 +0300

    feat(nvim): migrate statusline code to lua

---
+++ nvim/lua/elentok/statusline.lua
@@ -0,0 +1,72 @@
+vim.o.statusline = table.concat({
+  -- Path to the file in the buffer, as typed or relative to current directory.
+  '%{Elentok_StatusLineFileName()}',
+  -- Where to truncate line.
+  '%< ',
+  '%{&modified?\' +\':\'\'}',
+  '%{&readonly?\' \':\'\'}',
+  -- Separation point between left and right aligned items.
+  '%= ',
+  -- Filetype.
+  ' [%{\'\'!=#&filetype?&filetype:\'none\'}]',
+  -- Line number + column number.
+  ' %l:%v'
+})
+
+local statusline_shorteners = {}
+local statusline_shorteners_updated = false
+
+local function add_path_shortener(full, short)
+  table.insert(statusline_shorteners, {full, short})
+  statusline_shorteners_updated = true
+end
+
+local function shorten(path)
+  for _, item in ipairs(statusline_shorteners) do
+    local full, short = unpack(item)
+    path = path:gsub(full .. '/', short .. '/')
+    path = path:gsub(full .. '$', short)
+  end
+  return path
+end
+
+local function filename()
+  if vim.b.vaffle ~= nil then
+    local short_path = shorten(vim.b.vaffle['dir'])
+    return 'DIR: ' .. short_path
+  end
+
+  -- Temporary solution
+  if statusline_shorteners_updated then
+    statusline_shorteners_updated = false
+    if vim.b.short_path ~= nil then
+      vim.b.short_path = nil
+    end
+  end
+
+  local name = vim.fn.expand('%:t')
+
+  if vim.b.short_path == nil then
+    vim.b.short_path = shorten(vim.fn.expand('%:p:h'))
+  end
+
+  return string.format('%s (%s)', name, vim.b.short_path)
+end
+
+add_path_shortener(vim.env.HOME, '~')
+
+vim.cmd([[
+  function! Elentok_AddPathShortener(full, short)
+    lua require('elentok/statusline').add_path_shortener(a:full, a:short)
+  endfunction
+
+  function! Elentok_StatusLineFileName()
+    return luaeval("require('elentok/statusline').filename()")
+  endfunction
+]])
+
+return {
+  add_path_shortener = add_path_shortener,
+  filename = filename,
+  shorten = shorten,
+}
