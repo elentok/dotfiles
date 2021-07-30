5a0af641bd867c52c08f11e6431974328c9c66ea
David Elentok <3david@gmail.com>
2021-07-30 11:42:20 +0300

    fix(nvim): better formatting

---
+++ nvim/lua/elentok/format.lua
@@ -0,0 +1,46 @@
+local api = vim.api
+local util = require('elentok/util')
+
+local formatters = {
+  css = "prettier",
+  javascript = "clang",
+  typescript = "clang",
+  typescriptreact = "clang",
+  html = "prettier",
+}
+
+local function prettier()
+  cursor = api.nvim_win_get_cursor(0)
+  api.nvim_exec('%!prettier --stdin-filepath %', true)
+  util.restore_cursor(0, cursor)
+end
+
+local function clang()
+  cursor = api.nvim_win_get_cursor(0)
+  api.nvim_exec('%!clang-format --style=Google --assume-filename %', true)
+  util.restore_cursor(0, cursor)
+end
+
+local function format()
+  local formatter = formatters[util.buf_get_filetype()]
+
+  if formatter == 'clang' then
+    clang()
+  elseif formatter == 'prettier' then
+    prettier()
+  else
+    vim.lsp.buf.formatting_seq_sync()
+  end
+end
+
+vim.cmd([[
+  command! Prettier lua require('elentok/format').prettier()
+  command! ClangFormat lua require('elentok/format').clang()
+  command! Format lua require('elentok/format').format()
+]])
+
+return {
+  clang = clang,
+  format = format,
+  prettier = prettier,
+}
