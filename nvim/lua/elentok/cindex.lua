936fbbe44edb446c8d843217c115e199656520cc
David Elentok <3david@gmail.com>
2021-08-03 21:11:58 +0300

    fix(nvim): telescope cindex cleanup

--- nvim/lua/elentok/cindex.lua
+++ nvim/lua/elentok/cindex.lua
@@ -1,38 +1,18 @@
-local utils = require "telescope.utils"
-local finders = require "telescope.finders"
-local pickers = require "telescope.pickers"
-local conf = require("telescope.config").values
+local telescope = require('elentok/telescope')
 
 local function goto_symbol(opts)
-  local results = utils.get_os_command_output({'cindex', 'list'})
-
-  pickers.new(opts, {
-    prompt_title = "Go To Symbol",
-    sorter = conf.file_sorter(opts),
-    finder = finders.new_table {
-      results = results,
-      entry_maker = function(line)
-        local parts = vim.split(line, ",")
-        local symbol = parts[1]
-        local filename = parts[3]
-        local lnum = parts[4]
-
-        return {
-          valid = true,
-
-          value = symbol,
-          ordinal = symbol,
-          display = symbol,
-
-          filename = filename,
-          lnum = lnum,
-          col = 0
-        }
-      end
-    }
-  }):find()
+  telescope.command_picker({
+    cmd = {'cindex', 'list'},
+    parse_line = function(line)
+      local parts = vim.split(line, ",")
+      return {
+        text = parts[1],
+        filename = parts[3],
+        lnum = parts[4]
+      }
+    end,
+  })
 end
-
 
 return {
   goto_symbol = goto_symbol,
