39bc5eccba499fd97770128de45f3a803db65c00
David Elentok <elentok@google.com>
2021-08-08 09:53:15 +0300

    feat(nvim): add yank markdown keymapping

--- nvim/lua/elentok/keys.lua
+++ nvim/lua/elentok/keys.lua
@@ -8,3 +8,7 @@ map.normal('[m', ':SidewaysLeft<cr>')
 map.normal('<c-_>', ':Commentary<cr>')
 map.visual('<c-_>', ':Commentary<cr>')
 map.insert('<c-_>', '<c-o>:Commentary<cr>')
+
+-- Yank Markdown to HTML
+map.visual('<Leader>ym', ':!pandoc --from markdown --to html | xclip -selection clipboard -t text/html<cr>u')
+map.normal('<Leader>ym', ':%!pandoc --from markdown --to html | xclip -selection clipboard -t text/html<cr>u')
