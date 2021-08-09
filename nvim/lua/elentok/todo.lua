46c55f14c66969b624509f388a173a03af63bb7c
David Elentok <elentok@google.com>
2021-08-09 12:27:02 +0300

    feat(nvim): markdown todo

--- nvim/lua/elentok/todo.lua
+++ nvim/lua/elentok/todo.lua
@@ -1,13 +1,17 @@
 local map = require('elentok/map')
 
 local function toggle_done()
-  local line = vim.fn.getline('.')
-  if line:match("✔") then
-    line = line:gsub("✔", "☐")
-  elseif line:match("☐") then
-    line = line:gsub("☐", "✔")
-  end
-  vim.fn.setline('.', line)
+    local line = vim.fn.getline('.')
+    if line:match("✔") then
+        line = line:gsub("✔", "☐")
+    elseif line:match("☐") then
+        line = line:gsub("☐", "(inprogress)")
+    elseif line:match("%(inprogress%)") then
+        line = line:gsub("%(inprogress%)", "(waiting)")
+    elseif line:match("%(waiting%)") then
+        line = line:gsub("%(waiting%)", "✔")
+    end
+    vim.fn.setline('.', line)
 end
 
 map.normal('<Leader>td', map.lua('require("elentok/todo").toggle_done()'))
@@ -17,6 +21,4 @@ vim.cmd([[
   iabbr #done ✔
 ]])
 
-return {
-  toggle_done = toggle_done,
-}
+return {toggle_done = toggle_done}
