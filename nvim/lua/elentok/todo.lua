bf145873bfc33de45fc7a534c0297404c48237de
David Elentok <3david@gmail.com>
2021-08-09 12:55:15 +0300

    feat(nvim): todo improvements

--- nvim/lua/elentok/todo.lua
+++ nvim/lua/elentok/todo.lua
@@ -1,6 +1,6 @@
 local map = require('elentok/map')
 
-local function toggle_done()
+local function next_state()
     local line = vim.fn.getline('.')
     if line:match("✔") then
         line = line:gsub("✔", "☐")
@@ -14,11 +14,26 @@ local function toggle_done()
     vim.fn.setline('.', line)
 end
 
-map.normal('<Leader>td', map.lua('require("elentok/todo").toggle_done()'))
+local function prev_state()
+    local line = vim.fn.getline('.')
+    if line:match("✔") then
+        line = line:gsub("✔", "(waiting)")
+    elseif line:match("☐") then
+        line = line:gsub("☐", "✔")
+    elseif line:match("%(inprogress%)") then
+        line = line:gsub("%(inprogress%)", "☐")
+    elseif line:match("%(waiting%)") then
+        line = line:gsub("%(waiting%)", "(inprogress)")
+    end
+    vim.fn.setline('.', line)
+end
+
+map.normal('<Leader>tp', map.lua('require("elentok/todo").prev_state()'))
+map.normal('<Leader>tn', map.lua('require("elentok/todo").next_state()'))
 
 vim.cmd([[
   iabbr #todo ☐
   iabbr #done ✔
 ]])
 
-return {toggle_done = toggle_done}
+return {next_state = next_state, prev_state = prev_state}
