47a23fb1abe9fe0a5f020d7a93c32f0a3b53c78d
David Elentok <3david@gmail.com>
2021-08-12 22:06:29 +0300

    fix(nvim): <c-q> in telescope now opens the quickfix window

--- nvim/lua/elentok/telescope.lua
+++ nvim/lua/elentok/telescope.lua
@@ -8,68 +8,73 @@ local previewers = require 'telescope.previewers'
 local utils = require 'telescope.utils'
 
 require('telescope').setup {
-  defaults = {
-    -- file_sorter = require('telescope.sorters').get_fzy_sorter,
-    file_ignore_patterns = {"node_modules/.*", "scuba_goldens/.*"},
-    mappings = {i = {["<C-q>"] = actions.send_to_qflist}}
-  },
-  extensions = {
-    fzf = {
-      fuzzy = true,
-      override_generic_sorter = false,
-      override_file_sorter = true,
-      case_mode = "smart_case"
+    defaults = {
+        -- file_sorter = require('telescope.sorters').get_fzy_sorter,
+        file_ignore_patterns = {"node_modules/.*", "scuba_goldens/.*"},
+        mappings = {
+            i = {["<C-q>"] = actions.send_to_qflist + actions.open_qflist}
+        }
+    },
+    extensions = {
+        fzf = {
+            fuzzy = true,
+            override_generic_sorter = false,
+            override_file_sorter = true,
+            case_mode = "smart_case"
+        }
     }
-  }
 }
 
 require('telescope').load_extension('fzf')
 
 local function create_entry(opts)
-  return {
-    valid = true,
+    return {
+        valid = true,
 
-    value = opts.text,
-    ordinal = opts.text, -- used for the sorting order
-    display = opts.text,
+        value = opts.text,
+        ordinal = opts.text, -- used for the sorting order
+        display = opts.text,
 
-    filename = opts.filename,
-    lnum = tonumber(opts.lnum),
-    col = 0
-  }
+        filename = opts.filename,
+        lnum = tonumber(opts.lnum),
+        col = 0
+    }
 end
 
 local function command_picker(opts, picker_opts)
-  if picker_opts == nil then picker_opts = {} end
-  local results = utils.get_os_command_output(opts.cmd)
-  pickers.new(picker_opts, {
-    prompt_title = opts.title,
-    sorter = opts.sorter or conf.file_sorter({}),
-    finder = finders.new_table {
-      results = results,
-      entry_maker = opts.entry_maker or
-          function(line) return create_entry(opts.parse_line(line)) end
-    }
-  }):find()
+    if picker_opts == nil then picker_opts = {} end
+    local results = utils.get_os_command_output(opts.cmd)
+    pickers.new(picker_opts, {
+        prompt_title = opts.title,
+        sorter = opts.sorter or conf.file_sorter({}),
+        finder = finders.new_table {
+            results = results,
+            entry_maker = opts.entry_maker or
+                function(line)
+                    return create_entry(opts.parse_line(line))
+                end
+        }
+    }):find()
 end
 
 local function buf_tags_picker(opts)
-  opts = opts or {}
-  command_picker({
-    cmd = {
-      'ctags', '-f', '-', '--sort=yes', '--excmd=number', vim.fn.bufname('%')
-    },
-    title = 'Buffer tags',
-    entry_maker = make_entry.gen_from_ctags({path_display = 'hidden'}),
-    previewer = previewers.ctags.new(opts),
-    sorter = conf.generic_sorter(opts)
-  })
+    opts = opts or {}
+    command_picker({
+        cmd = {
+            'ctags', '-f', '-', '--sort=yes', '--excmd=number',
+            vim.fn.bufname('%')
+        },
+        title = 'Buffer tags',
+        entry_maker = make_entry.gen_from_ctags({path_display = 'hidden'}),
+        previewer = previewers.ctags.new(opts),
+        sorter = conf.generic_sorter(opts)
+    })
 end
 
 -- Keys
 
 local function call_telescope(expr)
-  return map.lua('require("telescope.builtin").' .. expr)
+    return map.lua('require("telescope.builtin").' .. expr)
 end
 
 map.normal('<c-p>', call_telescope('find_files{}'))
