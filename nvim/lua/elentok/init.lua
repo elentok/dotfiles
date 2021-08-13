a686da1c6c86dde0c59f80df6896458280119ef0
David Elentok <3david@gmail.com>
2021-08-13 10:46:32 +0300

    chore(nvim): switch from vim-signify to gitsigns

--- nvim/lua/elentok/init.lua
+++ nvim/lua/elentok/init.lua
@@ -1,19 +1,20 @@
-require'elentok/packer-bootstrap'
-require'elentok/plugins'
+require 'elentok/packer-bootstrap'
+require 'elentok/plugins'
 
-require'elentok/commands'
-require'elentok/compe'
-require'elentok/compe-tab'
-require'elentok/format'
-require'elentok/keys'
-require'elentok/lsp'
-require'elentok/statusline'
-require'elentok/symbols'
-require'elentok/telescope'
-require'elentok/todo'
-require'elentok/treesitter'
+require 'elentok/commands'
+require 'elentok/compe'
+require 'elentok/compe-tab'
+require 'elentok/format'
+require 'elentok/keys'
+require 'elentok/lsp'
+require 'elentok/statusline'
+require 'elentok/symbols'
+require 'elentok/telescope'
+require 'elentok/todo'
+require 'elentok/treesitter'
 
-local util = require'elentok/util'
+local util = require 'elentok/util'
 util.safe_require('elentok-local')
 
 require('nvim-autopairs').setup()
+require('gitsigns').setup()
