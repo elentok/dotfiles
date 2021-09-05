9fafbd3d1aa987ef996228a3ec71b53828c7c61e
David Elentok <3david@gmail.com>
2021-09-05 22:16:12 +0300

    feat(nvim): add nvim-treesitter-textobjects

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -24,7 +24,33 @@ require("nvim-treesitter.configs").setup {
   },
   -- disable python and typescript indentation until fixed:
   --   https://github.com/nvim-treesitter/nvim-treesitter/issues/1167#issue-853914044
-  indent = {enable = true, disable = {"python", "typescript", "javascript"}}
+  indent = {enable = true, disable = {"python", "typescript", "javascript"}},
+  textobjects = {
+    select = {
+      enable = true,
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
+
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner"
+      }
+    },
+    swap = {
+      enable = true,
+      swap_next = {["<leader>a"] = "@parameter.inner"},
+      swap_previous = {["<leader>A"] = "@parameter.inner"}
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {["]]"] = "@function.outer"},
+      goto_previous_start = {["[["] = "@function.outer"}
+    }
+  }
 }
 
 local exclude_from_folding = {markdown = true}
