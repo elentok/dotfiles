6d539c4ddedab80b71e30ca754732f361248f64c
David Elentok <3david@gmail.com>
2021-09-07 11:49:08 +0300

    fix(nvim): properly save & restore the cursor

--- nvim/plugin/cursor.lua
+++ nvim/plugin/cursor.lua
@@ -1,12 +1,12 @@
 local util = require("elentok/util")
 
 function _G.WinSaveCursor()
-  vim.w.last_cursor = vim.api.nvim_win_get_cursor(0)
+  vim.w.last_cursor = vim.fn.getpos(".")
 end
 
 function _G.WinRestoreCursor()
   if (vim.w.last_cursor) then
-    util.restore_cursor(0, vim.w.last_cursor)
+    vim.fn.setpos(".", vim.w.last_cursor)
   end
 end
 
