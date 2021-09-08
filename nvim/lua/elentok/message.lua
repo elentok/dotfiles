855b19698a98eaa887ceaa72b1d9d9fc7f41c2d7
David Elentok <3david@gmail.com>
2021-09-08 21:42:51 +0300

    fix(nvim): run lua formatter

--- nvim/lua/elentok/message.lua
+++ nvim/lua/elentok/message.lua
@@ -5,17 +5,17 @@ local M = {}
 -- Opts:
 --   mode = 'normal' or 'error' (defaults to 'normal')
 function M.show(title, lines, opts)
-  opts = vim.tbl_extend("force", { mode = 'normal' }, opts or {})
+  opts = vim.tbl_extend("force", {mode = "normal"}, opts or {})
 
   if vim.t.message_bufnr then
     local winnr = util.tabpage_get_buf_win_number(0, vim.t.message_bufnr)
     if winnr then
       -- jump to the existing message window
-      vim.cmd(winnr .. 'wincmd w')
+      vim.cmd(winnr .. "wincmd w")
     else
       -- split and load the existing message buffer
-      vim.cmd('belowright split')
-      vim.cmd('buffer ' .. vim.t.message_bufnr)
+      vim.cmd("belowright split")
+      vim.cmd("buffer " .. vim.t.message_bufnr)
     end
   else
     -- create a new message buffer
@@ -23,17 +23,18 @@ function M.show(title, lines, opts)
       belowright new
       noswapfile hide enew
     ]])
-    vim.bo.buftype = 'nofile'
-    vim.bo.bufhidden = 'hide'
-    vim.cmd('file ' .. title)
+    vim.bo.buftype = "nofile"
+    vim.bo.bufhidden = "hide"
+    vim.cmd("file " .. title)
     vim.t.message_bufnr = vim.fn.bufnr()
 
-    if opts.mode == 'error' then
-      vim.wo.winhighlight = 'Normal:IncSearch'
+    if opts.mode == "error" then
+      vim.wo.winhighlight = "Normal:IncSearch"
       vim.wo.cursorline = false
     end
 
-    vim.api.nvim_buf_set_keymap(0, 'n', 'q', ':close<cr>', { noremap = true, silent = true})
+    vim.api.nvim_buf_set_keymap(0, "n", "q", ":close<cr>",
+                                {noremap = true, silent = true})
   end
   vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
   vim.cmd([[wincmd p]])
@@ -41,7 +42,7 @@ end
 
 function M.close()
   if vim.t.message_bufnr then
-    vim.cmd('silent bd ' .. vim.t.message_bufnr)
+    vim.cmd("silent bd " .. vim.t.message_bufnr)
     vim.t.message_bufnr = nil
   end
 end
