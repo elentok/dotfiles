262f96a51f1ecb6dd88791e5e8f5436fbbff5b47
David Elentok <3david@gmail.com>
2021-09-14 09:41:04 +0300

    feat(nvim): map "q" to close the command line buffer

--- nvim/lua/elentok/map.lua
+++ nvim/lua/elentok/map.lua
@@ -5,23 +5,27 @@ function M.lua(code)
 end
 
 function M.create_map_func(mode, opts)
-  if opts == nil then
-    opts = {}
-  end
-  if opts.noremap == nil then
-    opts.noremap = true
-  end
-  if opts.silent == nil then
-    opts.silent = true
-  end
+  opts = vim.tbl_extend("force", {noremap = true, silent = true}, opts or {})
 
   return function(lhs, rhs)
     vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
   end
 end
 
+function M.create_buf_map_func(mode, opts)
+  opts = vim.tbl_extend("force", {noremap = true, silent = true}, opts or {})
+
+  return function(lhs, rhs)
+    vim.api.nvim_buf_set_keymap(0, mode, lhs, rhs, opts)
+  end
+end
+
 M.normal = M.create_map_func("n")
 M.visual = M.create_map_func("v")
 M.insert = M.create_map_func("i")
+
+M.buf_normal = M.create_buf_map_func("n")
+M.buf_visual = M.create_buf_map_func("v")
+M.buf_insert = M.create_buf_map_func("i")
 
 return M
