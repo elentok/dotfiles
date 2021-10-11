f0675a3eb88f3de46596f91831e220debcd4192a
David Elentok <3david@gmail.com>
2021-10-11 11:53:08 +0300

    fix(nvim): use fzf for generic telescope

--- core/nvim/lua/elentok/telescope.lua
+++ core/nvim/lua/elentok/telescope.lua
@@ -16,7 +16,7 @@ require("telescope").setup {
   extensions = {
     fzf = {
       fuzzy = true,
-      override_generic_sorter = false,
+      override_generic_sorter = true,
       override_file_sorter = true,
       case_mode = "smart_case"
     }
