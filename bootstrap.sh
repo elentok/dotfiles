4aadaec1e5cf01a44d91f9224fe6deeacfe01e66
David Elentok <3david@gmail.com>
2021-10-12 17:56:07 +0300

    fix(*): improve bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,31 +1,53 @@
 #!/usr/bin/env bash
 #
-# This scripts makes sure all requirements to run the "core/scripts/dotf2"
+# This scripts makes sure all requirements to run "dotf install"
 # script are installed and then runs it.
 
 set -euo pipefail
 
 function main() {
+  echo "=================================================="
   echo "Checking requirements are installed..."
   echo
 
-  echo -n "- Checking for python3... "
-  if has-command python3; then
+  install-if-missing python3 python3
+  install-if-missing pip3 python3-pip
+
+  echo "- Upgrading pip3..."
+  pip3 install pip --upgrade --user
+
+  echo "- Updating git submodules..."
+  git submodule update --init --recursive
+
+  echo
+  echo "All requirements are ready, running dotf install."
+  echo "=================================================="
+  echo
+
+  if [ "${1:-}" = "dotf2" ]; then
+    dotf2 install
+  else
+    dotf install
+  fi
+}
+
+function install-if-missing() {
+  local command="$1"
+  local pkg="$2"
+
+  echo -n "- Checking for ${command}... "
+  if has-command "${command}"; then
     echo "installed."
   else
     echo "missing, installing."
     if has-command apt; then
-      sudo apt install -y python3
+      sudo apt install -y "${pkg}"
     else
       echo 'ERROR: Only Debian is currently supported.'
       exit 1
     fi
   fi
 
-  echo
-  echo "All requirements are ready, running dotf2 install."
-  echo
-  dotf2 install
 }
 
 function has-command() {
