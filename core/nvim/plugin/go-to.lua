5348f31ec6aae0eee69758a53dd0c460551c3b85
David Elentok <3david@gmail.com>
2021-10-16 22:19:49 +0300

    fix(nvim): fix go-to

--- core/nvim/plugin/go-to.lua
+++ core/nvim/plugin/go-to.lua
@@ -6,7 +6,7 @@ local find_config_command = {
   "rg", "-t", "lua", "-t", "vim", "--files", vim.env.DOTF .. "/core/nvim"
 }
 
-util.add_dirs(find_command, {vim.env.DOTL .. "/nvim"})
+util.add_dirs(find_config_command, {vim.env.DOTL .. "/nvim"})
 
 function _G.goto_config()
   builtin.find_files({find_command = find_config_command})
@@ -16,7 +16,7 @@ local find_script_command = {
   "rg", "--files", vim.env.DOTF .. "/core/scripts", vim.env.DOTF .. "/scripts"
 }
 
-util.add_dirs(find_command, {vim.env.DOTL .. "/scripts"})
+util.add_dirs(find_script_command, {vim.env.DOTL .. "/scripts"})
 
 function _G.goto_script()
   builtin.find_files({find_command = find_script_command})
