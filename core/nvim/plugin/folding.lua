4f23e56c26cb99d9ed046acbc64bb9892e64a691
David Elentok <3david@gmail.com>
2021-10-21 13:22:26 +0300

    fix(nvim): make sure current line isn't folded after save

---
+++ core/nvim/plugin/folding.lua
@@ -0,0 +1,6 @@
+local util = require("elentok/util")
+
+-- After saving a buffer run "zv" to make sure current line isn't folded.
+util.augroup("Folding", [[
+  autocmd BufWritePost * normal! zv
+]])
