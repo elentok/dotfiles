a933650cd1eb62c8e57c854fec065c3079df794c
David Elentok <3david@gmail.com>
2021-10-24 21:21:44 +0300

    fix(nvim): completion fixes

--- core/nvim/plugin/completion.lua
+++ core/nvim/plugin/completion.lua
@@ -6,8 +6,21 @@ if cmp == nil then
 end
 
 cmp.setup({
+  snippet = {
+    expand = function(args)
+      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
+    end
+  },
   sources = cmp.config.sources({{name = "nvim_lsp"}, {name = "buffer"}}),
   mapping = {
-    ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), {"i", "s"})
+    ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), {"i", "s"}),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm({select = true})
   }
 })
