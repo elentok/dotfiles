baf9158b02c6bb1af1273b8c97608417db0f5ae0
David Elentok <3david@gmail.com>
2021-10-26 11:03:33 +0300

    fix(mercurial): migrate to lua + telescope

---
+++ core/nvim/plugin/mercurial.lua
@@ -0,0 +1,23 @@
+local builtin = require("telescope.builtin")
+local map = require("elentok/map")
+
+function _G.hg_goto_modified()
+  builtin.find_files({find_command = {"hg", "status", "--no-status"}})
+end
+
+function _G.hg_goto_unresolved()
+  builtin.find_files({
+    find_command = {
+      "hg", "resolve", "--no-status", "--list", "set:unresolved()"
+    }
+  })
+end
+
+vim.cmd([[
+  command! HgModified lua hg_goto_modified()
+  command! HgUnresolved lua hg_goto_unresolved()
+  command! HgResolve QuickShell hg resolve --mark %
+]])
+
+map.normal("<Leader>hm", ":HgModified<cr>")
+map.normal("<Leader>hu", ":HgUnresolved<cr>")
