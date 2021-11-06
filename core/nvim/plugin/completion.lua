a47e3fbb91fbe35a64cb0a09a93a29aac2ec0187
David Elentok <3david@gmail.com>
2021-11-06 21:27:57 +0200

    fix(nvim): add S-Tab completion mapping

--- core/nvim/plugin/completion.lua
+++ core/nvim/plugin/completion.lua
@@ -21,6 +21,7 @@ cmp.setup({
   }),
   mapping = {
     ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), {"i", "s"}),
+    ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), {"i", "s"}),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
