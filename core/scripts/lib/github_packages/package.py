ddf8cb795290ee8def28bbb836d29bb92e69da8b
David Elentok <3david@gmail.com>
2021-11-13 21:54:11 +0200

    fix(dotf-github): switch from json to cfg

--- core/scripts/lib/github_packages/package.py
+++ core/scripts/lib/github_packages/package.py
@@ -1,13 +1,9 @@
 import re
-import sys
-import platform
 from dataclasses import dataclass
 from typing import Optional
 
-MACHINE = platform.machine()
 
-
-#  @dataclass
+@dataclass
 class Package:
     name: str
     github_repo: str
@@ -15,28 +11,13 @@ class Package:
     strip_components: int
     prerelease: bool
     extract: bool
-    platform: Optional["Platform"]
-
-    def __init__(self, raw):
-        self.name = raw["name"]
-        self.github_repo = raw["github_repo"]
-        self.bin_target = raw.get("bin_target", self.name)
-        self.strip_components = raw.get("strip_components", 0)
-        self.prerelease = raw.get("prerelease", False)
-        self.extract = raw.get("extract", True)
-        self.version = raw.get("version", "latest")
-
-        self.platform = None
-        platform = raw["platforms"].get(MACHINE)
-        if platform:
-            self.platform = Platform(platform)
+    version: str
+    platform: Optional["Platform"] = None
 
 
 @dataclass
 class Platform:
+    package_name: str
+    machine: str
     bin_source: Optional[str]
     asset_regexp: re.Pattern
-
-    def __init__(self, raw):
-        self.bin_source = raw.get("bin_source")
-        self.asset_regexp = re.compile(raw["asset_regexp"])
--- core/scripts/lib/github_packages/package_manager.py
+++ core/scripts/lib/github_packages/package_manager.py
@@ -1,46 +1,26 @@
-import json
-from dataclasses import dataclass
-from os import path
-from typing import List
-
-from .package import Package
+from .config import CONFIG
 from .package_installer import PackageInstaller
-
-DOTF = path.join(path.dirname(__file__), "..", "..", "..", "..")
-CONFIG_FILENAME = path.join(DOTF, "config", "dotf-github.json")
-
-
-@dataclass
-class Config:
-    packages: List[Package]
-
-    def __init__(self, filename):
-        raw = json.load(open(filename))
-        self.packages = list(map(Package, raw["packages"]))
 
 
 class PackageManager:
-    def __init__(self):
-        self.config = Config(CONFIG_FILENAME)
-
     def list(self):
-        for pkg in self.config.packages:
+        for pkg in CONFIG.packages:
             print("Name: ", pkg.name)
 
     def update(self):
-        for pkg in self.config.packages:
+        for pkg in CONFIG.packages:
             PackageInstaller(pkg).update()
 
     def update_single(self, name: str, force_prerelease=False):
-        pkgs = [pkg for pkg in self.config.packages if pkg.name == name]
+        pkgs = [pkg for pkg in CONFIG.packages if pkg.name == name]
         for pkg in pkgs:
             PackageInstaller(pkg, force_prerelease).update()
 
     def install(self):
-        for pkg in self.config.packages:
+        for pkg in CONFIG.packages:
             PackageInstaller(pkg).install()
 
     def install_single(self, name: str, force_prerelease=False):
-        pkgs = [pkg for pkg in self.config.packages if pkg.name == name]
+        pkgs = [pkg for pkg in CONFIG.packages if pkg.name == name]
         for pkg in pkgs:
             PackageInstaller(pkg, force_prerelease).install()
