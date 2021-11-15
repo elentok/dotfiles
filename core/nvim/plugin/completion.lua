b968e374f0371903804de94601f98ed1e70e7799
David Elentok <3david@gmail.com>
2021-11-15 15:58:32 +0200

    fix(nvim): temporarily disable commandline completion
    It causes some errors

--- core/nvim/plugin/completion.lua
+++ core/nvim/plugin/completion.lua
@@ -46,9 +46,9 @@ cmp.setup({
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline("/", {sources = {{name = "buffer"}}})
+-- cmp.setup.cmdline("/", {sources = {{name = "buffer"}}})
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(":", {
-  sources = cmp.config.sources({{name = "path"}}, {{name = "cmdline"}})
-})
+-- cmp.setup.cmdline(":", {
+--   sources = cmp.config.sources({{name = "path"}}, {{name = "cmdline"}})
+-- })
