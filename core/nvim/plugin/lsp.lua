c54224ed32febecad31e150177a660875176a0da
David Elentok <3david@gmail.com>
2021-12-19 15:47:19 +0200

    fix(nvim): start moving away from lspsaga
    It hasn't been active for ~8 months and the hover command no longer works.

--- core/nvim/plugin/lsp.lua
+++ core/nvim/plugin/lsp.lua
@@ -46,7 +46,7 @@ require("lsp_signature").setup()
 -- Keys {{{1
 map.normal("gD", map.lua("vim.lsp.buf.declaration()"))
 map.normal("gd", map.lua("vim.lsp.buf.definition()"))
--- map.normal("K", map.lua("vim.lsp.buf.hover()"))
+map.normal("K", map.lua("vim.lsp.buf.hover()"))
 map.normal("gi", map.lua("vim.lsp.buf.implementation()"))
 map.normal("<space>k", map.lua("vim.lsp.buf.signature_help()"))
 map.normal("<leader>wa", map.lua("vim.lsp.buf.add_workspace_folder()"))
@@ -54,13 +54,21 @@ map.normal("<leader>wr", map.lua("vim.lsp.buf.remove_workspace_folder()"))
 map.normal("<leader>wl",
            map.lua("print(vim.inspect(vim.lsp.buf.list_workspace_folders()))"))
 map.normal("gD", map.lua("vim.lsp.buf.type_definition()"))
--- map.normal("<leader>rn", map.lua("vim.lsp.buf.rename()"))
+map.normal("<leader>rn", map.lua("vim.lsp.buf.rename()"))
 -- map.normal("gr", map.lua("vim.lsp.buf.references()"))
 -- map.normal("<space>e", map.lua("vim.lsp.diagnostic.show_line_diagnostics()"))
 -- map.normal("[d", map.lua("vim.lsp.diagnostic.goto_prev()"))
 -- map.normal("]d", map.lua("vim.lsp.diagnostic.goto_next()"))
 map.normal("<space>q", map.lua("vim.lsp.diagnostic.set_loclist()"))
 
+-- Add a border to hover windows {{{1
+vim.lsp.handlers["textDocument/hover"] =
+    vim.lsp.with(vim.lsp.handlers.hover, {
+      -- Use a sharp border with `FloatBorder` highlights
+      border = "single"
+    })
+
+-- Helper commands {{{1
 vim.cmd("command! LspLog :tabe " .. vim.lsp.get_log_path())
 vim.cmd([[
   command! LspDebugOn :lua vim.lsp.set_log_level(1)
