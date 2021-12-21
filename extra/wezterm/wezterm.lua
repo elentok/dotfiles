64483b0138d8bb0347fc20070b0be58ead2eede2
David Elentok <3david@gmail.com>
2021-12-21 10:38:42 +0200

    feat(wezterm): add initial config

---
+++ extra/wezterm/wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require("wezterm")
+return {
+  font = wezterm.font("agave Nerd Font Mono"),
+  line_height = 1.2,
+  font_size = 13.0,
+  color_scheme = "Molokai",
+  audible_bell = "Disabled",
+  keys = {
+    {
+      key = "V",
+      mods = "SUPER|SHIFT",
+      action = wezterm.action {PasteFrom = "Clipboard"}
+    }, {
+      key = "V",
+      mods = "CTRL|SHIFT",
+      action = wezterm.action {PasteFrom = "PrimarySelection"}
+    }
+  }
+}
