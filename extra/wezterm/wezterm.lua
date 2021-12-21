2805ec93515084db202c188a518c0d6f327ed75e
David Elentok <3david@gmail.com>
2021-12-21 12:07:12 +0200

    fix(wezterm): change color scheme

--- extra/wezterm/wezterm.lua
+++ extra/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ return {
   font = wezterm.font("agave Nerd Font Mono"),
   line_height = 1.2,
   font_size = 13.0,
-  color_scheme = "Molokai",
+  color_scheme = "MaterialDarker",
   audible_bell = "Disabled",
   keys = {
     {
