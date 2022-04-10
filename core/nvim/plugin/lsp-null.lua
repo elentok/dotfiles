738a90b5de75fa24533dd4c4a74d643219b21e54
David Elentok <3david@gmail.com>
2022-04-10 19:05:23 +0300

    fix(nvim): allow disabling prettier

--- core/nvim/plugin/lsp-null.lua
+++ core/nvim/plugin/lsp-null.lua
@@ -1,14 +1,18 @@
 local nullLs = require("null-ls")
+local config = require("elentok/config")
 
-nullLs.setup({
-  sources = {
-    nullLs.builtins.diagnostics.shellcheck,
-    nullLs.builtins.code_actions.shellcheck,
-    nullLs.builtins.formatting.black,
-    nullLs.builtins.formatting.lua_format,
-    nullLs.builtins.formatting.prettierd,
-    nullLs.builtins.formatting.shfmt.with({
-      extra_args = {"-i", "2", "-bn", "-ci", "-sr"}
-    })
-  }
-})
+local sources = {
+  nullLs.builtins.diagnostics.shellcheck,
+  nullLs.builtins.code_actions.shellcheck,
+  nullLs.builtins.formatting.black,
+  nullLs.builtins.formatting.lua_format,
+  nullLs.builtins.formatting.shfmt.with({
+    extra_args = {"-i", "2", "-bn", "-ci", "-sr"}
+  })
+}
+
+if config.enable_jsts_prettier then
+  table.insert(sources, nullLs.builtins.formatting.prettierd)
+end
+
+nullLs.setup({sources = sources})
