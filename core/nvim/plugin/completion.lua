0a2aa021395e1feadaf2c0f88e8a4df46f6981ce
David Elentok <3david@gmail.com>
2022-04-12 13:17:31 +0300

    fix(nvim): add supertab-like support for cmp+luasnip

--- core/nvim/plugin/completion.lua
+++ core/nvim/plugin/completion.lua
@@ -1,9 +1,41 @@
 local util = require("elentok/util")
 local cmp = util.safe_require("cmp")
 local lspkind = util.safe_require("lspkind")
+local luasnip = util.safe_require("luasnip")
 
-if cmp == nil or lspkind == nil then
+if cmp == nil or lspkind == nil or luasnip == nil then
+  put("WARNING: [completion.lua] One of cmp, lspkind or luasnip is missing.")
   return
+end
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and
+             vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col)
+                 :match("%s") == nil
+end
+
+-- From https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
+local function handle_tab(fallback)
+  if cmp.visible() then
+    cmp.select_next_item()
+  elseif luasnip.expand_or_jumpable() then
+    luasnip.expand_or_jump()
+  elseif has_words_before() then
+    cmp.complete()
+  else
+    fallback()
+  end
+end
+
+local function handle_shift_tab(fallback)
+  if cmp.visible() then
+    cmp.select_prev_item()
+  elseif luasnip.jumpable(-1) then
+    luasnip.jump(-1)
+  else
+    fallback()
+  end
 end
 
 cmp.setup({
@@ -24,8 +56,8 @@ cmp.setup({
     -- {name = "luasnip"},
   }),
   mapping = {
-    ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), {"i", "s"}),
-    ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), {"i", "s"}),
+    ["<Tab>"] = cmp.mapping(handle_tab, {"i", "s"}),
+    ["<S-Tab>"] = cmp.mapping(handle_shift_tab, {"i", "s"}),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
