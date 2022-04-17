99e31b21d52b7761fa5c614d86eee6f2fa3cc9bc
David Elentok <3david@gmail.com>
2022-04-17 22:12:52 +0300

    chore(nvim): migrate loadtime.lua to new autocmd api

--- core/nvim/lua/elentok/loadtime.lua
+++ core/nvim/lua/elentok/loadtime.lua
@@ -1,8 +1,9 @@
 local start = vim.fn.reltime()
 
-function _G.ShowLoadTime()
+local function show_load_time()
   local elapsed_millisec = vim.fn.reltimefloat(vim.fn.reltime(start)) * 1000
   print(string.format("Loaded in %dms", elapsed_millisec))
 end
 
-vim.cmd("autocmd VimEnter * lua ShowLoadTime()")
+vim.api.nvim_create_autocmd({"VimEnter"},
+                            {pattern = "*", callback = show_load_time})
