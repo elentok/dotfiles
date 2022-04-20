3caf7596a63cd0fa8ec6cff7c538394493f18e62
David Elentok <3david@gmail.com>
2022-04-20 11:26:43 +0300

    feat(nvim): experimenting with a new grep helper

--- core/nvim/plugin/grep.lua
+++ core/nvim/plugin/grep.lua
@@ -1,4 +1,8 @@
 local builtin = require("telescope/builtin")
+local conf = require("telescope/config").values
+local finders = require("telescope/finders")
+local make_entry = require("telescope.make_entry")
+local pickers = require("telescope/pickers")
 
 -- Use "ripgrep" for the :grep command when available.
 if vim.fn.executable("rg") then
@@ -20,8 +24,15 @@ local function grep()
   end)
 end
 
--- Grep with telescope (can't pass ripgrep arguments but includes preview and
--- filtering).
+local function split_args(args)
+  if args == nil or args == "" then
+    return {}
+  end
+
+  return vim.split(args, " ", { plain = true, trimempty = true })
+end
+
+-- Grep with telescope.
 local function telescope_grep()
   vim.ui.input({ prompt = "Grep for? " }, function(query)
     if query ~= nil then
@@ -30,5 +41,27 @@ local function telescope_grep()
   end)
 end
 
-vim.keymap.set("n", "<Leader>ff", telescope_grep)
+local function new_grep(opts)
+  opts = opts or {}
+  opts.entry_maker = opts.entry_maker or make_entry.gen_from_vimgrep(opts)
+
+  vim.ui.input({ prompt = "Grep for? " }, function(query)
+    local command = vim.tbl_flatten({ conf.vimgrep_arguments, split_args(query) })
+
+    put(command)
+
+    if query ~= nil then
+      pickers.new(opts, {
+        prompt_title = "Grep for [" .. query .. "]",
+        finder = finders.new_oneshot_job(command, opts),
+        previewer = conf.grep_previewer(opts),
+        sorter = conf.generic_sorter(opts),
+      }):find()
+    end
+  end)
+end
+
+vim.keymap.set("n", "<Leader>ff", function()
+  new_grep()
+end)
 vim.keymap.set("n", "<Leader>fq", grep)
