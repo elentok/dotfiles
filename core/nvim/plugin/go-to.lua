ee1957b30996dbc34fb337e4234492528d004e2f
David Elentok <3david@gmail.com>
2022-05-11 10:45:14 +0300

    feat(nvim): add go to note keymap

--- core/nvim/plugin/go-to.lua
+++ core/nvim/plugin/go-to.lua
@@ -1,34 +1,46 @@
 local builtin = require("telescope.builtin")
 local util = require("elentok/util")
 
-local find_config_command = {
-  "rg",
-  "-t",
-  "lua",
-  "-t",
-  "vim",
-  "--files",
+-- Configuration -------------------------------------------
+
+local config_dirs = {
   vim.env.DOTF .. "/core/nvim",
 }
+util.add_dirs(config_dirs, { vim.env.DOTL .. "/nvim" })
 
-util.add_dirs(find_config_command, { vim.env.DOTL .. "/nvim" })
-
-local function goto_config()
-  builtin.find_files({ find_command = find_config_command })
-end
-
-local find_script_command = {
-  "rg",
-  "--files",
+local script_dirs = {
   vim.env.DOTF .. "/core/scripts",
   vim.env.DOTF .. "/scripts",
 }
+util.add_dirs(script_dirs, { vim.env.DOTL .. "/scripts" })
 
-util.add_dirs(find_script_command, { vim.env.DOTL .. "/scripts" })
+-- Functions -----------------------------------------------
+
+local function goto_config()
+  builtin.find_files({
+    find_command = { "rg", "-t", "lua", "-t", "vim", "--files" },
+    cwd = vim.env.HOME,
+    search_dirs = config_dirs,
+  })
+end
 
 local function goto_script()
-  builtin.find_files({ find_command = find_script_command })
+  builtin.find_files({
+    find_command = { "rg", "--files" },
+    cwd = vim.env.HOME,
+    search_dirs = script_dirs,
+  })
 end
+
+local function goto_note()
+  builtin.find_files({
+    find_command = { "rg", "-t", "md", "--files" },
+    cwd = vim.env.HOME .. "/notes",
+  })
+end
+
+-- Keys ----------------------------------------------------
 
 vim.keymap.set("n", "<Leader>gb", goto_script)
 vim.keymap.set("n", "<Leader>gc", goto_config)
+vim.keymap.set("n", "<Leader>gn", goto_note)
