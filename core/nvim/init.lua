601da81f04a860b70b1c679e86d8f183871bcc2b
David Elentok <3david@gmail.com>
2022-06-17 21:59:28 +0300

    fix(nvim): enable lua filetype

--- core/nvim/init.lua
+++ core/nvim/init.lua
@@ -2,6 +2,8 @@ require("elentok/loadtime")
 local util = require("elentok/util")
 util.safe_require("impatient")
 
+vim.g.do_filetype_lua = 1
+
 local local_config = vim.fn.expand("~/.dotlocal/nvim")
 if vim.fn.isdirectory(local_config) then
   vim.opt.runtimepath:append(local_config)
