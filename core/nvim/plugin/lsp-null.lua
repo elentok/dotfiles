259e686f98703f21bfc140dfa7c8cf1bdf6d4e86
David Elentok <3david@gmail.com>
2022-06-17 21:59:16 +0300

    feat(nvim): add sca2d linter (for openscad files)

--- core/nvim/plugin/lsp-null.lua
+++ core/nvim/plugin/lsp-null.lua
@@ -1,4 +1,34 @@
 local null_ls = require("null-ls")
+local helpers = require("null-ls.helpers")
+
+local sca2d_diagnostics_source = {
+  name = "SCA2D",
+  method = null_ls.methods.DIAGNOSTICS,
+  filetypes = { "scad", "openscad" },
+  generator = helpers.generator_factory({
+    command = "sca2d",
+    args = { "$FILENAME" },
+    from_temp_file = true,
+    format = "line", -- calls "on_output" once per line
+    on_output = function(params)
+      local columns = vim.split(params, ":")
+      if #columns < 5 then
+        return nil
+      end
+
+      put(columns)
+      local code = vim.trim(columns[4])
+      local data = {
+        row = columns[2],
+        col = columns[3],
+        code = code,
+        message = columns[5] .. " (" .. code .. ")",
+      }
+      put(data)
+      return data
+    end,
+  }),
+}
 
 null_ls.setup({
   debug = true,
@@ -16,5 +46,6 @@ null_ls.setup({
     null_ls.builtins.formatting.shfmt.with({
       extra_args = { "-i", "2", "-bn", "-ci", "-sr" },
     }),
+    sca2d_diagnostics_source,
   },
 })
