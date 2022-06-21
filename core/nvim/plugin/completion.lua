754e2a8e306d34501592cbdb60703a8a63c1b31d
David Elentok <3david@gmail.com>
2022-06-21 17:09:10 +0300

    fix(nvim): disable command line completion
    I think it causes freezes

--- core/nvim/plugin/completion.lua
+++ core/nvim/plugin/completion.lua
@@ -11,7 +11,7 @@ end
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0
-    and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 -- From https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
@@ -90,7 +90,7 @@ cmp.setup.cmdline("/", {
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(":", {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
-})
+-- cmp.setup.cmdline(":", {
+--   mapping = cmp.mapping.preset.cmdline(),
+--   sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
+-- })
