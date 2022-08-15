c204fd16f74ea51f60c36c32d63099eee0dcfc26
David Elentok <3david@gmail.com>
2022-08-15 11:31:12 +0300

    fix(wezterm): better tmux support

--- extra/wezterm/wezterm.lua
+++ extra/wezterm/wezterm.lua
@@ -1,4 +1,6 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
+
 return {
   font = wezterm.font("agave Nerd Font Mono"),
   line_height = 1.2,
@@ -6,10 +8,16 @@ return {
   color_scheme = "MaterialDarker",
   audible_bell = "Disabled",
   keys = {
-    {
-      key = "r",
-      mods = "CMD|SHIFT",
-      action = wezterm.action.ReloadConfiguration,
-    },
+    { key = "r", mods = "CMD|SHIFT", action = act.ReloadConfiguration },
+
+    -- Command + [/] are used for switching tabs in tmux
+    { key = "[", mods = "CMD", action = act.SendKey({ key = "[", mods = "ALT" }) },
+    { key = "]", mods = "CMD", action = act.SendKey({ key = "]", mods = "ALT" }) },
+
+    -- Command + hjkl are used for switching between tmux windows
+    { key = "h", mods = "CMD", action = act.SendKey({ key = "h", mods = "ALT" }) },
+    { key = "j", mods = "CMD", action = act.SendKey({ key = "j", mods = "ALT" }) },
+    { key = "k", mods = "CMD", action = act.SendKey({ key = "k", mods = "ALT" }) },
+    { key = "l", mods = "CMD", action = act.SendKey({ key = "l", mods = "ALT" }) },
   },
 }
