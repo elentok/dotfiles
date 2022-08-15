14f6e896d59e5bcf46ab8f033b8c344da899d47d
David Elentok <3david@gmail.com>
2022-08-15 11:25:34 +0300

    fix(wezterm): cleanup

--- extra/wezterm/wezterm.lua
+++ extra/wezterm/wezterm.lua
@@ -2,19 +2,14 @@ local wezterm = require("wezterm")
 return {
   font = wezterm.font("agave Nerd Font Mono"),
   line_height = 1.2,
-  font_size = 13.0,
+  font_size = 17.0,
   color_scheme = "MaterialDarker",
   audible_bell = "Disabled",
   keys = {
     {
-      key = "V",
-      mods = "SUPER|SHIFT",
-      action = wezterm.action({ PasteFrom = "Clipboard" }),
-    },
-    {
-      key = "V",
-      mods = "CTRL|SHIFT",
-      action = wezterm.action({ PasteFrom = "PrimarySelection" }),
+      key = "r",
+      mods = "CMD|SHIFT",
+      action = wezterm.action.ReloadConfiguration,
     },
   },
 }
