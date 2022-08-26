7049df3c411b86a822446eb1c6c3a06624e6caed
David Elentok <3david@gmail.com>
2022-08-26 21:19:32 +0300

    fix(nvim): ufo: disable fold column

--- core/nvim/plugin/ufo.lua
+++ core/nvim/plugin/ufo.lua
@@ -4,7 +4,7 @@ if not ok then
   return
 end
 
-vim.o.foldcolumn = "1"
+vim.o.foldcolumn = "0"
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
