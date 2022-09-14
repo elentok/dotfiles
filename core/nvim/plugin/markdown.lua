1652a97f400488c502bb1722b14e217d99d6504a
David Elentok <3david@gmail.com>
2022-09-14 10:23:11 +0300

    feat(markdown): add important highlight

--- core/nvim/plugin/markdown.lua
+++ core/nvim/plugin/markdown.lua
@@ -3,11 +3,13 @@ local function setup()
   vim.fn.matchadd("TodoWaiting", "\\[waiting\\].*$")
   vim.fn.matchadd("TodoDone", "\\[x\\].*$")
   vim.fn.matchadd("TodoContext", "@[^ ]*")
+  vim.fn.matchadd("TodoImportant", " !.*$")
 
   vim.api.nvim_set_hl(0, "TodoInprogress", { bg = "#EBCB8B", fg = "#000000" })
   vim.api.nvim_set_hl(0, "TodoWaiting", { fg = "#EBCB8B" })
   vim.api.nvim_set_hl(0, "TodoDone", { fg = "#6C7A96" })
   vim.api.nvim_set_hl(0, "TodoContext", { fg = "#88c0d0", italic = true })
+  vim.api.nvim_set_hl(0, "TodoImportant", { fg = "#d57780" })
 
   vim.wo.foldmethod = "indent"
 end
