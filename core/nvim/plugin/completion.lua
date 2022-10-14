212b46477b7e44359f9023ab532081a16c691f48
David Elentok <3david@gmail.com>
2022-10-14 16:22:59 +0300

    fix(nvim): enable luasnip in completion

--- core/nvim/plugin/completion.lua
+++ core/nvim/plugin/completion.lua
@@ -11,7 +11,7 @@ end
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0
-    and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 -- From https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
@@ -52,7 +52,7 @@ cmp.setup({
     { name = "rg", keyword_length = 4 },
     { name = "path" },
     { name = "dictionary", keyword_length = 5 },
-    -- {name = "luasnip"},
+    { name = "luasnip" },
   }),
   mapping = {
     ["<Tab>"] = cmp.mapping(handle_tab, { "i", "s" }),
