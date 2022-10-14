2f3647cf352a67143c209de441471121e1be3e8b
David Elentok <3david@gmail.com>
2022-10-14 16:24:13 +0300

    feat(nvim): add openscad snippet

--- core/nvim/plugin/snippets.lua
+++ core/nvim/plugin/snippets.lua
@@ -1,6 +1,15 @@
-local ok, _ = pcall(require, "luasnip")
+local ok, luasnip = pcall(require, "luasnip")
 if not ok then
   return
 end
 
 require("luasnip/loaders/from_vscode").lazy_load()
+
+local s = luasnip.snippet
+local t = luasnip.text_node
+
+luasnip.add_snippets("openscad", {
+  s("head", {
+    t({ "include <BOSL2/std.scad>", "$fn = 64;", "", "" }),
+  }),
+})
