3ca5d5df20c3a3b94d075c3e39611cdceeb17264
David Elentok <3david@gmail.com>
2022-11-14 11:12:46 +0200

    fix(nvim): use snipmate format for custom snippets

--- core/nvim/plugin/snippets.lua
+++ core/nvim/plugin/snippets.lua
@@ -4,12 +4,4 @@ if not ok then
 end
 
 require("luasnip/loaders/from_vscode").lazy_load()
-
-local s = luasnip.snippet
-local t = luasnip.text_node
-
-luasnip.add_snippets("openscad", {
-  s("head", {
-    t({ "include <BOSL2/std.scad>", "$fn = 64;", "", "" }),
-  }),
-})
+require("luasnip/loaders/from_snipmate").lazy_load()
