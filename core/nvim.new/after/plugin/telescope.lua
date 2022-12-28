816178b69fec970fb62c2cd700c21e9053449d1b
David Elentok <3david@gmail.com>
2022-12-28 22:42:03 +0200

    feat(nvim.new): add telescope

---
+++ core/nvim.new/after/plugin/telescope.lua
@@ -0,0 +1,2 @@
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
--- core/nvim.new/lua/elentok/packer.lua
+++ core/nvim.new/lua/elentok/packer.lua
@@ -1,4 +1,9 @@
 return require("packer").startup(function(use)
-  -- Packer can manage itself
   use("wbthomason/packer.nvim")
+
+  use({
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.0",
+    requires = { { "nvim-lua/plenary.nvim" } },
+  })
 end)
