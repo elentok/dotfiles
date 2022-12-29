86c384057f80f795478a77acf1622a008cd06980
David Elentok <3david@gmail.com>
2022-12-29 22:52:29 +0200

    feat(nvim.new): add treesitter

--- core/nvim.new/lua/elentok/packer.lua
+++ core/nvim.new/lua/elentok/packer.lua
@@ -7,6 +7,14 @@ return require("packer").startup(function(use)
     requires = { { "nvim-lua/plenary.nvim" } },
   })
 
+  use({
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  })
+
   use 'rmehri01/onenord.nvim'
   use 'mbbill/undotree'
   use 'aserowy/tmux.nvim'
