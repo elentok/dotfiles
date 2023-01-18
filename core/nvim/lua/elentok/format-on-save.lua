12ce49b1b01f9dfd66a459beee757ad1aea83cfb
David Elentok <3david@gmail.com>
2023-01-18 16:01:04 +0200

    fix(nvim): run format-on-save only on allowlisted files

--- core/nvim/lua/elentok/format-on-save.lua
+++ core/nvim/lua/elentok/format-on-save.lua
@@ -23,7 +23,10 @@ function M.enable(filetype)
 end
 
 function M.format()
-  vim.lsp.buf.format({ timeout_ms = 2000 })
+  local filetype = vim.api.nvim_buf_get_option(0, "filetype")
+  if vim.tbl_contains(M.file_types_to_autoformat, filetype) then
+    vim.lsp.buf.format({ timeout_ms = 2000 })
+  end
 end
 
 vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = "*", callback = M.format })
