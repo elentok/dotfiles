60fceb048c775c2892175b29bc9f46ec1e034ff2
David Elentok <3david@gmail.com>
2023-01-26 11:21:01 +0200

    fix(nvim): increase LSP format timeout
    Also make ":Format" not rely on the format on save configuration

--- core/nvim/after/plugin/format-on-save.lua
+++ core/nvim/after/plugin/format-on-save.lua
@@ -1,11 +1,15 @@
 local config = require("elentok/config")
 
 local function format()
+  vim.lsp.buf.format({ timeout_ms = 4000 })
+end
+
+local function format_on_save()
   local filetype = vim.api.nvim_buf_get_option(0, "filetype")
   if vim.tbl_contains(config.format_on_save, filetype) then
-    vim.lsp.buf.format({ timeout_ms = 2000 })
+    format()
   end
 end
 
-vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = "*", callback = format })
+vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = "*", callback = format_on_save })
 vim.api.nvim_create_user_command("Format", format, {})
