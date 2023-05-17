f337c7813d2bdc9eafb25305616689e1b7efb485
David Elentok <3david@gmail.com>
2023-05-17 21:27:57 +0300

    fix(nvim): remove deprecated code

--- core/nvim/after/plugin/snippets.lua
+++ core/nvim/after/plugin/snippets.lua
@@ -1,7 +1,2 @@
-local ok, luasnip = pcall(require, "luasnip")
-if not ok then
-  return
-end
-
 require("luasnip/loaders/from_vscode").lazy_load()
 require("luasnip/loaders/from_snipmate").lazy_load()
