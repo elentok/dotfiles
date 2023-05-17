346eb03cd9f24f438aa910d7523029f6616fb4dd
David Elentok <3david@gmail.com>
2023-05-17 17:15:57 +0300

    fix(nvim): add git.tilde_root() helper

--- core/nvim/lua/elentok/lib/git.lua
+++ core/nvim/lua/elentok/lib/git.lua
@@ -1,12 +1,22 @@
 local M = {}
 
+-- Root with a "tilde" for the user's home directory
+function M.tilde_root()
+  local root = M.root()
+  if root == nil then
+    return nil
+  end
+
+  return root:gsub(vim.env.HOME .. "/", "~/")
+end
+
 function M.root()
   local gitdir = vim.fn.finddir(".git")
   if gitdir == nil then
     return nil
   end
 
-  return vim.fn.fnamemodify(gitdir, ":h")
+  return vim.fn.fnamemodify(vim.fn.fnamemodify(gitdir, ":h"), ":p")
 end
 
 return M
