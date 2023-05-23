f0d3279be767e8df69fa9e9d37ed7daba3b42cd5
David Elentok <3david@gmail.com>
2023-05-23 16:26:41 +0300

    feat(nvim): add mapping to go pick file from last git commit

--- core/nvim/after/plugin/git.lua
+++ core/nvim/after/plugin/git.lua
@@ -1,4 +1,6 @@
-local util = require("elentok/util")
+local conf = require("telescope.config").values
+local finders = require("telescope.finders")
+local pickers = require("telescope.pickers")
 
 vim.cmd([[
   command! -nargs=+ GG lua require('elentok/util').ishell('git <args>')
@@ -12,3 +14,22 @@ local ok, git_conflict = pcall(require, "git-conflict")
 if ok then
   git_conflict.setup()
 end
+
+local function telescope_git_last_commit_files()
+  pickers
+      .new({}, {
+        finder = finders.new_oneshot_job({
+          "git",
+          "diff-tree",
+          "--no-commit-id",
+          "--name-only",
+          "-r",
+          "HEAD",
+        }),
+        previewer = conf.file_previewer({}),
+      })
+      :find()
+end
+
+vim.api.nvim_create_user_command("Glast", telescope_git_last_commit_files, {})
+vim.keymap.set("n", "<leader>gl", telescope_git_last_commit_files)
