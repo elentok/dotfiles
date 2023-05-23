37c179c2c0851280be50ac4ed42b8522a54ac4f9
David Elentok <3david@gmail.com>
2023-05-23 10:54:43 +0300

    feat(node): use config file for node-version
    So it can be overridden in dotprivate

---
+++ core/scripts/lib/node.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+dotf-node-version() {
+  local node_version_file="$DOTF/config/node-version"
+  local private_node_version_file="$DOTP/config/node-version"
+
+  if [ -e "$private_node_version_file" ]; then
+    cat "$private_node_version_file"
+  else
+    cat "$node_version_file"
+  fi
+}
