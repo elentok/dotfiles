b303282bf326ea3b9df4118aa4f0e993a25d7575
David Elentok <3david@gmail.com>
2023-06-09 21:57:39 +0300

    feat(nvim): add <c-w>m mapping to open messages in a buffer

---
+++ core/nvim/after/plugin/messages.lua
@@ -0,0 +1,15 @@
+local function messages_buffer()
+  vim.cmd("new")
+  vim.cmd("noswapfile hide enew")
+  vim.cmd("put = execute('message')")
+
+  vim.bo.buftype = "nofile"
+  vim.bo.bufhidden = "hide"
+  vim.bo.modifiable = false
+  vim.cmd("file Messages")
+
+  vim.api.nvim_buf_set_keymap(0, "n", "q", ":bd<cr>", { noremap = true, silent = true })
+end
+
+vim.api.nvim_create_user_command("Messages", messages_buffer, {})
+vim.keymap.set("n", "<c-w>m", messages_buffer)
