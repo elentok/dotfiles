0aad3b55a2038bca81d76151a4b3dc67809a90e7
David Elentok <3david@gmail.com>
2023-06-11 22:09:13 +0300

    fix(nvim): fix log line

--- core/nvim/after/plugin/abbr.vim
+++ core/nvim/after/plugin/abbr.vim
@@ -2,7 +2,6 @@ iabbr doctype5 <!DOCTYPE html>
 iabbr date@ <c-r>=strftime("%Y-%m-%d")<cr>
 iabbr time@ <c-r>=strftime("%H:%M:%S")<cr>
 iabbr now@ <c-r>=strftime("%A, %B %d, %H:%M")<cr>
-iabbr log@ <c-r>=v:lua.require('elentok/lib/treesitter').get_logger_line()<cr><left><left>,
 
 cabbr Gca Gcommit --amend
 cabbr Gam Gcommit --amend
---
+++ core/nvim/after/plugin/log-line.lua
@@ -0,0 +1,61 @@
+local navic = require("nvim-navic")
+local util = require("elentok/util")
+
+local prefix = "[" .. vim.env.USER .. "]"
+
+local function get_logger_context()
+  local data = navic.get_data()
+  if data == "" then
+    return nil
+  end
+
+  local container = nil
+  local name = nil
+
+  for _, part in pairs(data) do
+    if part.type == "Function" then
+      name = part.name
+    elseif part.type == "Method" then
+      name = part.name
+    elseif part.type == "Class" then
+      container = part.name
+    end
+  end
+
+  if container == nil then
+    container = vim.fn.expand("%:t")
+  end
+
+  if name == nil then
+    return container
+  end
+
+  return container .. " > " .. name .. "()"
+end
+
+local function get_logger_line()
+  local context = get_logger_context()
+  if context == nil then
+    return ""
+  end
+
+  context = "[" .. context .. "]"
+
+  local filetype = util.buf_get_filetype()
+  if filetype == "typescript" or filetype == "javascript" then
+    return "console.log('" .. prefix .. " " .. context .. "');"
+  elseif filetype == "lua" then
+    return "put('" .. prefix .. " " .. context .. "');"
+  else
+    return ""
+  end
+end
+
+local function feedkeys(keys)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(keys, true, false, true), "n", true)
+end
+
+vim.keymap.set("i", "<c-l>", function()
+  vim.api.nvim_put({ get_logger_line() }, "c", true, true)
+  feedkeys("<Left><Left>")
+end)
