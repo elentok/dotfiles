3251aa6ddb543e1f6facf6a88cbdddd28cd518a9
David Elentok <3david@gmail.com>
2023-06-17 22:36:15 +0300

    feat(nvim): restore buf cursor in all windows after saving

--- core/nvim/after/plugin/format-on-save.lua
+++ core/nvim/after/plugin/format-on-save.lua
@@ -1,6 +1,8 @@
 local config = require("elentok/config")
+local buf_cursors = require("elentok/lib/buf_cursors")
 
 local function format()
+  buf_cursors.save_buf_cursors()
   local filetype = vim.api.nvim_buf_get_option(0, "filetype")
   local opts = config.format_on_save[filetype]
   if opts == nil then
@@ -18,4 +20,8 @@ local function format()
 end
 
 vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = "*", callback = format })
+vim.api.nvim_create_autocmd(
+  { "BufWritePost" },
+  { pattern = "*", callback = buf_cursors.restore_buf_cursors }
+)
 vim.api.nvim_create_user_command("Format", format, {})
---
+++ core/nvim/lua/elentok/lib/buf_cursors.lua
@@ -0,0 +1,45 @@
+local M = {}
+
+local function find_buffer_windows()
+  local current_buf = vim.api.nvim_get_current_buf()
+
+  local windows = vim.api.nvim_list_wins()
+  local buffer_windows = {}
+  for _, win in ipairs(windows) do
+    local win_buf = vim.api.nvim_win_get_buf(win)
+
+    if win_buf == current_buf then
+      table.insert(buffer_windows, win)
+    end
+  end
+
+  return buffer_windows
+end
+
+local function find_buffer_win_cursors()
+  local cursors = {}
+
+  local buffer_windows = find_buffer_windows()
+  for _, win in ipairs(buffer_windows) do
+    cursors[win] = vim.api.nvim_win_get_cursor(win)
+  end
+
+  return cursors
+end
+
+local win_cursor_cache_by_buf = {}
+
+function M.save_buf_cursors()
+  local cursors = find_buffer_win_cursors()
+  win_cursor_cache_by_buf[vim.api.nvim_get_current_buf()] = cursors
+end
+
+function M.restore_buf_cursors()
+  local current_buf = vim.api.nvim_get_current_buf()
+
+  for win, cursor in pairs(win_cursor_cache_by_buf[current_buf] or {}) do
+    vim.api.nvim_win_set_cursor(win, cursor)
+  end
+end
+
+return M
