5366d6023e798bb50c21b313d2831b94c52dd3eb
David Elentok <3david@gmail.com>
2023-06-27 09:54:35 +0300

    feat(nvim): bring back ufo folding

--- core/nvim/after/plugin/folding.lua
+++ core/nvim/after/plugin/folding.lua
@@ -1,12 +1,12 @@
-vim.opt.foldlevel = 20
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldtext = "getline(v:foldstart)"
-
--- Deal with issue where folding disappears on save
-local group_id = vim.api.nvim_create_augroup("Elentok_Folding", {})
-
-vim.api.nvim_create_autocmd(
-  "BufWritePost",
-  { group = group_id, pattern = "*", command = "e | normal zv" }
-)
+-- vim.opt.foldlevel = 20
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- vim.opt.foldtext = "getline(v:foldstart)"
+--
+-- -- Deal with issue where folding disappears on save
+-- local group_id = vim.api.nvim_create_augroup("Elentok_Folding", {})
+--
+-- vim.api.nvim_create_autocmd(
+--   "BufWritePost",
+--   { group = group_id, pattern = "*", command = "e | normal zv" }
+-- )
--- core/nvim/after/plugin/ufo.lua
+++ core/nvim/after/plugin/ufo.lua
@@ -1,16 +1,17 @@
--- local ufo = require("ufo")
---
--- vim.o.foldcolumn = "1"
--- vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
--- vim.o.foldlevelstart = 99
--- vim.o.foldenable = true
---
--- -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
--- vim.keymap.set("n", "zR", ufo.openAllFolds)
--- vim.keymap.set("n", "zM", ufo.closeAllFolds)
---
+local ufo = require("ufo")
+
+vim.o.foldcolumn = "0"
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+vim.keymap.set("n", "zR", ufo.openAllFolds)
+vim.keymap.set("n", "zM", ufo.closeAllFolds)
+
+ufo.setup({})
 -- ufo.setup({
 --   provider_selector = function(bufnr, filetype, buftype)
---     return { "treesitter", "indent" }
+--     return { "lsp", "treesitter", "indent" }
 --   end,
 -- })
