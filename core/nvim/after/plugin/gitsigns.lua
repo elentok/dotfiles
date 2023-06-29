d0b561c1000b094ea70c82f2e4c910c76d6bc312
David Elentok <3david@gmail.com>
2023-06-29 15:53:31 +0300

    feat(nvim): make <space>b show the full message

--- core/nvim/after/plugin/gitsigns.lua
+++ core/nvim/after/plugin/gitsigns.lua
@@ -1,9 +1,13 @@
 -- Separate column for git signs so it doesn't override the line numbers
 vim.opt.signcolumn = "yes"
 
-require("gitsigns").setup({})
+local gitsigns = require("gitsigns")
 
-vim.keymap.set("n", "<space>b", ":Gitsigns blame_line<cr>")
+gitsigns.setup({})
+
+vim.keymap.set("n", "<space>b", function()
+  gitsigns.blame_line({ full = true })
+end)
 vim.keymap.set("n", "<space>gs", ":Gitsigns<cr>")
 vim.keymap.set("n", "<space>gp", ":Gitsigns preview_hunk<cr>")
 vim.keymap.set("n", "<space>gi", ":Gitsigns preview_hunk_inline<cr>")
