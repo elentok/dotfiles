31afe925885a47afd7cc2828b7f2522365eddac5
David Elentok <3david@gmail.com>
2023-07-07 14:03:57 +0300

    feat(nvim): add flash plugin

--- core/nvim/lua/elentok/plugins.lua
+++ core/nvim/lua/elentok/plugins.lua
@@ -167,4 +167,53 @@ return {
   },
 
   { "kevinhwang91/nvim-bqf", ft = "qf" },
+
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        mode = { "o", "x" },
+        function()
+          require("flash").treesitter_search()
+        end,
+        desc = "Flash Treesitter Search",
+      },
+      {
+        "<Leader>fs",
+        mode = { "c" },
+        function()
+          require("flash").toggle()
+        end,
+        desc = "Toggle Flash Search",
+      },
+    },
+  },
 }
