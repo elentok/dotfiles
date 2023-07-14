18fa27671bf971a52e66628b125fcd9ac6ac4bdf
David Elentok <3david@gmail.com>
2023-07-14 16:36:11 +0300

    fix(*): proper eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+
+  // "env": {
+  //   "node": true,
+  //   "es2021": true
+  // },
+  // "extends": "standard-with-typescript",
+  // "overrides": [
+  //   {
+  //     "files": [
+  //       ".eslintrc.{js,cjs}"
+  //     ],
+  //     "parserOptions": {
+  //       "sourceType": "script"
+  //     }
+  //   },
+  // ],
+  // "parserOptions": {
+  //   "ecmaVersion": "latest",
+  //   "sourceType": "module",
+  //   "project": "./tsconfig.json"
+  // },
+  // "rules": {
+  // }
+}
