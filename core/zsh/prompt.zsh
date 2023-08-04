1a666872848997d8d66f1b0ad8065b5f0b770308
David Elentok <3david@gmail.com>
2023-08-04 14:57:48 +0300

    feat(zsh): prompt improvements

--- core/zsh/prompt.zsh
+++ core/zsh/prompt.zsh
@@ -6,38 +6,86 @@
 # and arithmetic expansion:
 setopt PROMPT_SUBST
 
-# Random animals {{{1
+# Execution time {{{1
+#
+# Here's the basic order of events when a command is executed in Zsh:
+#
+# 1. The preexec hook (if defined) is called before the command is executed.
+# 2. The command is executed.
+# 3. The precmd hook (if defined) is called just before the prompt is displayed.
 
-# Credit to https://github.com/aviaron
-function random_animal {
-  FIRST_ANIMAL=128045
-  ICONS_COUNT=17
-  UTF_CODE=$(( $RANDOM % $ICONS_COUNT + $FIRST_ANIMAL ))
-  EMOJI="\U`printf '%x\n' $UTF_CODE`"
-  echo -n $EMOJI
+# Runs before the command is executed
+function preexec() {
+  DOTF_CMD_START_TIME=$SECONDS
 }
 
-# Only support on Mac and outside of neovim
-# if [ "$(uname -s)" = "Darwin" -a -z "$NVIM" ]; then
-#   random_animal="$(random_animal)  "
-# fi
+# Runs after the command is executed
+function precmd() {
+  _last_cmd_runtime=''
+  if [ -n "${DOTF_CMD_START_TIME:-}" ]; then
+    local runtime=$(($SECONDS - $DOTF_CMD_START_TIME))
+    _last_cmd_runtime=$(dotf-format-seconds ${runtime})
+  fi
+}
+
+function dotf-format-seconds() {
+  local seconds="$1"
+  if [[ "$seconds" -lt 1 ]]; then
+    millisec=$((seconds * 1000))
+    dotf-format-number $millisec ms
+  elif [[ "$seconds" -ge 3600 ]]; then
+    hours=$((seconds / 3600.0))
+    dotf-format-number $hours hr
+  elif [[ "$seconds" -ge 60 ]]; then
+    minutes=$((seconds / 60.0))
+    dotf-format-number $minutes m
+  else
+    dotf-format-number $seconds s
+  fi
+}
+
+function dotf-format-number() {
+  local number="$1"
+  local unit="$2"
+  number=$(printf '%.2f' ${number})
+  number="${number/%.00/}"
+  echo -n "${number}${unit}"
+}
 
 # Exit code {{{1
-prefix_char=''
-# prefix_char=''
+# prefix_char=''
+prefix_char=''
 # prefix_char='󰌕'
 success="%F{green}${prefix_char}%f"
 error="%F{red}${prefix_char}%f"
 exit_code="%(?.$success.$error) "
 
-# Directory {{{1
-directory="%F{blue}%f%K{blue}%F{black}%~%k%F{blue}%f"
-# directory="%K{blue} %F{black}%~%k%F{blue}%f"
-# User and host {{{1
-# user_and_host="%{\$fg_bold[cyan]%}$USERNAME at $(hostname)%f"
-# user_and_host="%{\$fg_bold[gray]%}$USERNAME at $(hostname)%f"
-user_and_host="%F{black}$USERNAME@${SHORT_HOST}%f"
+function dotf-half-bubble() {
+  local fg="$1"
+  local bg="$2"
+  local text="$3"
+  echo -ne "%K{$bg}%F{$fg}$text%f%k"
+  echo -ne "%F{$bg}%f"
+}
 
+function bubble() {
+  local fg="$1"
+  local bg="$2"
+  local text="$3"
+  echo -ne "%F{$bg}%f"
+  echo -ne "%K{$bg}%F{$fg}$text%f%k"
+  echo -ne "%F{$bg}%f"
+}
+
+# Line 1 (user, host and last runtime) {{{1
+_prompt_user_and_host="$(bubble gray black " $USERNAME@${SHORT_HOST}")"
+_prompt_cmd_runtime="$(bubble gray black ' ${_last_cmd_runtime}')"
+_prompt_line1="${_prompt_user_and_host} ${_prompt_cmd_runtime}"
+
+# Line 2 (Directory) {{{1
+# directory="%F{blue}%f%K{blue}%F{black}%~%k%F{blue}%f"
+directory="$(bubble black blue '%~')"
+# directory="%K{blue} %F{black}%~%k%F{blue}%f"
 # Time {{{1
 if [ "$ZSH_VERSION" = "5.0.5" ]; then
   time="%D{%H:%M:%S}"
@@ -75,12 +123,11 @@ git_status='${vcs_info_msg_0_}'
 fi
 
 # Full prompt {{{1
-
 PROMPT="$directory$git_status
 $exit_code"
 
 if [ ! -e ~/.miniprompt ]; then
-  PROMPT="$user_and_host $time
+  PROMPT="$_prompt_line1
 $PROMPT"
 fi
 
