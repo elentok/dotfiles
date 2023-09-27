7674610db51d72a988d8852d4be3fb048addbcdd
David Elentok <3david@gmail.com>
2023-09-27 16:24:55 +0300

    fix(nvim): oil improvements

--- core/nvim/lua/elentok/plugins/plugins.lua
+++ core/nvim/lua/elentok/plugins/plugins.lua
@@ -11,13 +11,19 @@ return {
   {
     "stevearc/oil.nvim",
     version = "*",
+    ft = "oil",
     opts = {
       skip_confirm_for_simple_edits = false,
+      win_options = {
+        winbar = "%f",
+      },
       keymaps = {
         ["<C-h>"] = false,
         ["<C-l>"] = false,
         ["<C-c>"] = false,
+        ["<C-p>"] = false,
         ["<C-s>"] = "actions.save",
+        ["<space>p"] = "actions.preview",
         ["q"] = "actions.close",
         ["L"] = "actions.select",
         ["J"] = "actions.select",
