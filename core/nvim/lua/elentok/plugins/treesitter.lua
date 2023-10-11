ec47d975dc9a6b803edf4049a8fa4501bea8da20
David Elentok <3david@gmail.com>
2023-10-11 22:19:37 +0300

    nvim: cleanup treesitter setup

--- core/nvim/lua/elentok/plugins/treesitter.lua
+++ core/nvim/lua/elentok/plugins/treesitter.lua
@@ -1,3 +1,39 @@
+local ensure_installed = {
+  "bash",
+  "c",
+  "cmake",
+  "comment",
+  "cpp",
+  "css",
+  "dot",
+  "go",
+  "graphql",
+  "hcl", -- terraform
+  "html",
+  "java",
+  "javascript",
+  "jsdoc",
+  "json",
+  "json5",
+  "jsonc",
+  "lua",
+  -- These are causing Neovim to crash in some specific scenarios,
+  -- not sure why.
+  -- "markdown",
+  -- "markdown_inline",
+  "python",
+  "regex",
+  "rst",
+  "ruby",
+  "scss",
+  "svelte",
+  "toml",
+  "tsx",
+  "typescript",
+  "vim",
+  "yaml",
+}
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -5,6 +41,62 @@ return {
     build = function()
       local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
       ts_update()
+    end,
+
+    config = function()
+      local treesitter_configs = require("nvim-treesitter.configs")
+
+      treesitter_configs.setup({
+        -- additional_vim_regex_highlighting = true,
+        playground = {
+          enable = true,
+        },
+        ensure_installed = ensure_installed,
+        highlight = { enable = true },
+        autopairs = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+          },
+        },
+        -- disable python and typescript indentation until fixed:
+        --   https://github.com/nvim-treesitter/nvim-treesitter/issues/1167#issue-853914044
+        indent = {
+          enable = true,
+          disable = { "python", "typescript", "javascript" },
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+              ["aa"] = "@parameter.outer",
+              ["ia"] = "@parameter.inner",
+            },
+          },
+          swap = {
+            enable = true,
+            swap_next = { ["<leader>a"] = "@parameter.inner" },
+            swap_previous = { ["<leader>A"] = "@parameter.inner" },
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = { ["]]"] = "@function.outer" },
+            goto_previous_start = { ["[["] = "@function.outer" },
+          },
+        },
+      })
     end,
   },
   "nvim-treesitter/playground",
