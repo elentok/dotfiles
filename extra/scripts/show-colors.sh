da53deb5330a572f02b9c3edcbe9ffcff5e03644
David Elentok <3david@gmail.com>
2023-10-27 18:46:31 +0300

    extra/scripts: add show-colors

---
+++ extra/scripts/show-colors.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+for i in {0..255}; do
+  printf "\x1b[48;5;%sm%3d\e[0m " "$i" "$i"
+  if ((i == 15)) || ((i > 15)) && (((i - 15) % 6 == 0)); then
+    printf "\n"
+  fi
+done
