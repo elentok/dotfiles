475bfc87e34a75d61dcebafa7b8b3bbf3f439160
David Elentok <3david@gmail.com>
2023-12-21 11:45:18 +0200

    nvim: folding

--- core/nvim/after/ftplugin/markdown.lua
+++ core/nvim/after/ftplugin/markdown.lua
@@ -1,3 +1,5 @@
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.tabstop = 2
+vim.opt_local.foldmethod = "expr"
+vim.opt_local.foldexpr = "v:lua.MarkdownFoldExpr(v:lnum)"
--- core/nvim/after/plugin/folding.lua
+++ core/nvim/after/plugin/folding.lua
@@ -11,8 +11,42 @@
 --   { group = group_id, pattern = "*", command = "e | normal zv" }
 -- )
 
--- vim.opt.foldmethod = "expr"
--- vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
--- vim.opt.foldtext = "getline(v:foldstart)"
 --
 -- vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldtext = "getline(v:foldstart)"
+
+-- Setting the foldexpr like this causes vim to crash so I'm using the
+-- nvim_treesitter variation (https://github.com/neovim/neovim/issues/25608)
+-- vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+-- local group_id = vim.api.nvim_create_augroup("Elentok_Folding", {})
+
+-- local function setupFolding()
+-- print("setup folding")
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldtext = "getline(v:foldstart)"
+-- if vim.bo.filetype == "markdown" then
+--   vim.opt.foldexpr = "v:lua.MarkdownFoldExpr(v:lnum)"
+-- else
+--   vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+-- end
+-- vim.cmd("normal zx")
+-- end
+
+-- vim.api.nvim_create_autocmd("BufRead", { group = group_id, pattern = "*", callback = setupFolding })
+
+function MarkdownFoldExpr(lnum)
+  local line = vim.fn.getline(lnum)
+
+  -- Check for a Markdown heading
+  local _, _, level = string.find(line, "^(#+)")
+  if level then
+    return ">" .. string.len(level)
+  end
+
+  -- Return 0 if not a heading (i.e., do not fold)
+  return "="
+end
