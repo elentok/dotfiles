9640e7a6a87163e2d30c0f1f332772bb82249066
David Elentok <3david@gmail.com>
2024-01-07 10:04:05 +0200

    nvim: change window splitter color

--- core/nvim/lua/elentok/plugins/catppuccin.lua
+++ core/nvim/lua/elentok/plugins/catppuccin.lua
@@ -32,6 +32,7 @@ return {
     custom_highlights = function(colors)
       return {
         VertSplit = { fg = colors.surface2 },
+        WinSeparator = { fg = colors.surface2 },
       }
     end,
   },
