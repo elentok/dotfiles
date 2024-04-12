9d33c1c905c2a99bd9c882d79bb6f6232c62936d
David Elentok <3david@gmail.com>
2024-04-12 23:19:33 +0300

    fish: remove fisher so I can reinstall it properly

---
+++ core/fish/conf.d/fisher.fish
@@ -0,0 +1,8 @@
+set fisher_path $__fish_config_dir/fisher
+
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..]
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..]
+
+for file in $fisher_path/conf.d/*.fish
+    source $file
+end
