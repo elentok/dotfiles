72b25cf08a3fa62b86aa9342e21fcd05cc3aa5a9
David Elentok <3david@gmail.com>
2024-04-30 23:09:54 +0300

    nvim: disable which-key

--- core/nvim/lua/elentok/plugins/which-key.lua
+++ core/nvim/lua/elentok/plugins/which-key.lua
@@ -1,29 +1,30 @@
-return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
+return {}
+-- return {
+--   "folke/which-key.nvim",
+--   event = "VeryLazy",
+--   init = function()
+--     vim.o.timeout = true
+--     vim.o.timeoutlen = 300
 
-    local wk = require("which-key")
+--     local wk = require("which-key")
 
-    wk.register({
-      j = {
-        name = "jump",
-      },
-      f = {
-        name = "find",
-      },
-      g = {
-        name = "git",
-      },
-      o = {
-        name = "open",
-      },
-      t = {
-        name = "toggle/test",
-      },
-    }, { prefix = "<leader>" })
-  end,
-  opts = {},
-}
+--     wk.register({
+--       j = {
+--         name = "jump",
+--       },
+--       f = {
+--         name = "find",
+--       },
+--       g = {
+--         name = "git",
+--       },
+--       o = {
+--         name = "open",
+--       },
+--       t = {
+--         name = "toggle/test",
+--       },
+--     }, { prefix = "<leader>" })
+--   end,
+--   opts = {},
+-- }
