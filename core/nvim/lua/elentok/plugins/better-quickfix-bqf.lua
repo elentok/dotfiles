0f7137fea417021192e3bc191a2b0e7d97203501
David Elentok <3david@gmail.com>
2024-05-11 23:54:18 +0300

    nvim: remove fzf

--- core/nvim/lua/elentok/plugins/better-quickfix-bqf.lua
+++ core/nvim/lua/elentok/plugins/better-quickfix-bqf.lua
@@ -1,12 +1,4 @@
 return {
   "kevinhwang91/nvim-bqf",
   ft = "qf",
-  dependencies = {
-    {
-      "junegunn/fzf",
-      run = function()
-        vim.fn["fzf#install"]()
-      end,
-    },
-  },
 }
