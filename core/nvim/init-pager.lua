ec02d5b99a6ff4436a81ba327ba453721c546e4d
David Elentok <3david@gmail.com>
2024-05-21 10:45:28 +0300

    kitty: add nvim pager to browse scrollback

---
+++ core/nvim/init-pager.lua
@@ -0,0 +1,6 @@
+vim.keymap.set("n", "q", "<Cmd>q<cr>")
+vim.keymap.set("n", "vv", "V")
+vim.keymap.set("n", "vc", "<c-v>")
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.wildignorecase = true
