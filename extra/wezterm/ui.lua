6951b70bf5a484d1b07f9f993b93ea2297091495
David Elentok <3david@gmail.com>
2024-05-22 08:08:54 +0300

    wezterm: extract ui settings to ui.lua

---
+++ extra/wezterm/ui.lua
@@ -0,0 +1,28 @@
+local wezterm = require("wezterm")
+local h = require("helpers")
+
+local function setupFonts(config)
+  config.font = wezterm.font("ComicShannsMono Nerd Font Mono")
+  config.font_rules = {
+    {
+      italic = true,
+      font = wezterm.font("Operator Mono", { weight = 325, italic = true }),
+    },
+  }
+  config.line_height = 1.2
+  config.cell_width = 1.0
+  config.font_size = 14.0
+end
+
+local function setupUi(config)
+  setupFonts(config)
+  config.color_scheme = "Catppuccin Mocha"
+  -- config.color_scheme = "MaterialDarker",
+  config.audible_bell = "Disabled"
+
+  config.tab_bar_at_bottom = true
+  config.use_fancy_tab_bar = false
+  config.tab_max_width = 30
+end
+
+return setupUi
--- extra/wezterm/wezterm.lua
+++ extra/wezterm/wezterm.lua
@@ -1,52 +1,12 @@
 local wezterm = require("wezterm")
-local h = require("helpers")
+local setupUi = require("ui")
 local setupKeys = require("keys")
 local setupSplitPanes = require("split-panes")
 
 local local_config_dir = os.getenv("HOME") .. "/.dotprivate/wezterm"
 
-print(h.ctrl_or_cmd)
-
-local config = {
-  -- font = wezterm.font("agave Nerd Font Mono"),
-  -- font = wezterm.font("CaskaydiaMono NFM Light"),
-  -- font = wezterm.font("Hurmit Nerd Font"),
-  font = wezterm.font("ComicShannsMono Nerd Font Mono"),
-  font_rules = {
-    {
-      italic = true,
-      font = wezterm.font("Operator Mono", { weight = 325, italic = true }),
-    },
-  },
-  line_height = 1.2,
-  cell_width = 1.0,
-  font_size = 14.0,
-  color_scheme = "MaterialDarker",
-  audible_bell = "Disabled",
-
-  tab_bar_at_bottom = true,
-  use_fancy_tab_bar = false,
-  tab_max_width = 30,
-  -- window_frame = {
-  --   font_size = 16,
-  --   font = wezterm.font("ComicShannsMono Nerd Font Mono"),
-  -- },
-  -- key_tables = {
-  --   copy_mode = {
-  --     { key = "u", mods = "CTRL", action = act.CopyMode("PageUp") },
-  --     { key = "d", mods = "CTRL", action = act.CopyMode("PageDown") },
-  --   },
-  -- },
-  -- leader = { key = "a", mods = "CTRL" },
-
-  -- Work like tmux locally
-  -- unix_domains = {
-  --   { name = "unix" },
-  -- },
-  --
-  -- default_gui_startup_args = { "connect", "unix" },
-}
-
+local config = {}
+setupUi(config)
 setupKeys(config)
 setupSplitPanes(config)
 
