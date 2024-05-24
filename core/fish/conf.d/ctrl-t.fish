f9180b75a1defda1466132c49497f60679ae9086
David Elentok <3david@gmail.com>
2024-05-24 22:29:14 +0300

    fish: use fzf.fish's directory search for ctrl-t

--- core/fish/conf.d/ctrl-t.fish
+++ core/fish/conf.d/ctrl-t.fish
@@ -1,9 +1,9 @@
 function complete_filename
-    set filename "$(fd | fzf-tmux -p --exit-0)"
+    set filename "$(fd | fzf-tmux -w 85% -h 70% -p --exit-0)"
     if test -n "$filename"
         commandline -a "$filename"
         commandline -f end-of-line
     end
 end
 
-bind -M insert \ct complete_filename
+# bind -M insert \ct complete_filename
---
+++ core/fish/conf.d/fzf.fish
@@ -0,0 +1,1 @@
+fzf_configure_bindings --directory=\ct
