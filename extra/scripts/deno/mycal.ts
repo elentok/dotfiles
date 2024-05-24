06a0d74a93d98bae09adff43b566160c6b674173
David Elentok <3david@gmail.com>
2024-05-24 23:02:13 +0300

    mycal: refactor to functional approach

--- extra/scripts/deno/mycal.ts
+++ extra/scripts/deno/mycal.ts
@@ -9,117 +9,109 @@ const SATURDAY = 6
 const TODAY = new Date()
 
 function main(): void {
-  const date = Deno.args.length > 0
-    ? new Date(Date.parse(Deno.args[0]))
-    : new Date()
-  const month = new Month(date)
+  const date = Deno.args.length > 0 ? dayjs(Deno.args[0]) : dayjs()
+  const month = createMonth(date)
   console.info()
-  month.prev().print()
-  month.print()
-  month.next().print()
+  printMonth(prevMonth(month))
+  printMonth(month)
+  printMonth(nextMonth(month))
 }
 
-class Month {
-  private start: dayjs.Dayjs
-  private weeks: Week[] = []
+interface Month {
+  start: dayjs.Dayjs
+  weeks: Week[]
+}
 
-  constructor(date: dayjs.ConfigType) {
-    const d = dayjs(date)
-    this.start = d.startOf("month")
-    this.buildWeeks()
-  }
+interface Week {
+  days: Array<dayjs.Dayjs | undefined>
+}
 
-  public print(): void {
-    const date = new Intl.DateTimeFormat(undefined, {
-      month: "short",
-      year: "numeric",
-    }).format(
-      this.start.toDate(),
-    )
-    console.info(chalk.green(center(date, 27)))
-
-    console.info("Sun Mon Tue Wed Thu Fri Sat")
-    this.weeks.forEach((week) => console.info(week.pretty()))
-    console.info()
-  }
-
-  public next(): Month {
-    return new Month(this.start.add(1, "month"))
-  }
-
-  public prev(): Month {
-    return new Month(this.start.subtract(1, "month"))
-  }
-
-  private buildWeeks(): void {
-    let week = new Week()
-    let date = this.start.startOf("week")
-
-    while (date < this.start) {
-      week.days.push(undefined)
-      date = date.add(1, "day")
-    }
-
-    while (date.isSame(this.start, "month")) {
-      week.days.push(new Day(date))
-
-      if (date.day() === SATURDAY) {
-        this.weeks.push(week)
-        week = new Week()
-      }
-
-      date = date.add(1, "day")
-    }
-
-    if (week.days.length > 0) {
-      this.weeks.push(week)
-    }
+function createMonth(date: dayjs.ConfigType) {
+  const d = dayjs(date)
+  const start = d.startOf("month")
+  return {
+    start,
+    weeks: buildWeeks(start),
   }
 }
 
-class Week {
-  public days: Array<Day | undefined> = []
+function printMonth({ start, weeks }: Month): void {
+  const date = new Intl.DateTimeFormat(undefined, {
+    month: "short",
+    year: "numeric",
+  }).format(
+    start.toDate(),
+  )
+  console.info(chalk.green(center(date, 27)))
 
-  public pretty(): string {
-    return this.days
+  console.info("Sun Mon Tue Wed Thu Fri Sat")
+  weeks.forEach(printWeek)
+  console.info()
+}
+
+function nextMonth({ start }: Month): Month {
+  return createMonth(start.add(1, "month"))
+}
+
+function prevMonth({ start }: Month): Month {
+  return createMonth(start.subtract(1, "month"))
+}
+
+function buildWeeks(start: dayjs.Dayjs): Week[] {
+  let week: Week = { days: [] }
+  let date = start.startOf("week")
+  const weeks: Week[] = []
+
+  while (date < start) {
+    week.days.push(undefined)
+    date = date.add(1, "day")
+  }
+
+  while (date.isSame(start, "month")) {
+    week.days.push(date)
+
+    if (date.day() === SATURDAY) {
+      weeks.push(week)
+      week = { days: [] }
+    }
+
+    date = date.add(1, "day")
+  }
+
+  if (week.days.length > 0) {
+    weeks.push(week)
+  }
+
+  return weeks
+}
+
+function printWeek({ days }: Week): void {
+  console.info(
+    days
       .map((day) => {
         if (day == null) return "   "
-        return day.pretty()
+        return formatDay(day)
       })
-      .join(" ")
-  }
+      .join(" "),
+  )
 }
 
-class Day {
-  public date: dayjs.Dayjs
+function formatDay(day: dayjs.Dayjs): string {
+  const text = day.date().toString().padStart(3)
 
-  constructor(date: dayjs.ConfigType) {
-    this.date = dayjs(date)
+  if (day.isSame(TODAY, "day")) {
+    return chalk.red(text)
   }
 
-  public pretty(): string {
-    if (this.isToday()) {
-      return chalk.red(this.text())
-    }
-
-    if (this.isWeekend()) {
-      return chalk.gray(this.text())
-    }
-
-    return this.text()
+  if (isWeekend(day)) {
+    return chalk.gray(text)
   }
 
-  public isToday(): boolean {
-    return this.date.isSame(TODAY, "day")
-  }
+  return text
+}
 
-  public isWeekend(): boolean {
-    return this.date.day() >= FRIDAY
-  }
-
-  public text(): string {
-    return this.date.date().toString().padStart(3)
-  }
+function isWeekend(day: dayjs.Dayjs): boolean {
+  return day.day() >= FRIDAY
 }
 
 main()
