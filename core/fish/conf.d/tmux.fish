62cf259e5e86390d4eb30dcd3322c10127832db0
David Elentok <3david@gmail.com>
2024-05-27 16:59:19 +0300

    tmux: prevent processes inside neovim from changing title

--- core/fish/conf.d/tmux.fish
+++ core/fish/conf.d/tmux.fish
@@ -1,18 +1,25 @@
 function tmux-set-title
-    if test -n "$TMUX"
-        if test -z "$argv[1]"
-            set suffix ":\$"
-        else
-            set suffix "$argv[1]"
-        end
-
-        if test "$PWD" = "$HOME"
-            set dir "~"
-        else
-            set dir "$(basename $PWD)"
-        end
-        tmux rename-window "$dir$suffix"
+    if test -z "$TMUX"
+        return
     end
+
+    # Prevent processes running from inside Neovim from changing the title
+    if test -n "$VIMRUNTIME"
+        return
+    end
+
+    if test -z "$argv[1]"
+        set suffix ":\$"
+    else
+        set suffix "$argv[1]"
+    end
+
+    if test "$PWD" = "$HOME"
+        set dir "~"
+    else
+        set dir "$(basename $PWD)"
+    end
+    tmux rename-window "$dir$suffix"
 end
 
 function change-tmux-title-on-prompt --on-event fish_prompt
--- core/nvim/after/plugin/tmux.lua
+++ core/nvim/after/plugin/tmux.lua
@@ -1,14 +1,19 @@
 local uv = vim.loop
 
+local function set_tmux_title()
+  if not vim.env.TMUX then
+    return
+  end
+  local cwd = vim.fn.getcwd()
+  local dir = "~"
+  if cwd ~= uv.os_homedir() then
+    dir = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+  end
+  vim.fn.system("tmux rename-window " .. dir .. ":", {})
+end
+
 vim.api.nvim_create_autocmd({ "VimEnter", "DirChanged" }, {
   callback = function()
-    if vim.env.TMUX then
-      local cwd = vim.fn.getcwd()
-      local dir = "~"
-      if cwd ~= uv.os_homedir() then
-        dir = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
-      end
-      vim.fn.system("tmux rename-window " .. dir .. ":", {})
-    end
+    set_tmux_title()
   end,
 })
