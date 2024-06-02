94a26c4376d97fbe4b0b1df813c47368df2fe583
David Elentok <3david@gmail.com>
2024-06-02 22:15:09 +0300

    cleanup

--- extra/scripts/webpack/test-index.js
+++
@@ -1,4 +0,0 @@
-// require all modules ending in "_test" from the
-// current directory and all subdirectories
-const testsContext = require.context(".", true, /_test$/)
-testsContext.keys().forEach(testsContext)
