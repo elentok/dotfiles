c32f9ff374aa95b635165ef6b3cd4641bbfc98ec
David Elentok <3david@gmail.com>
2024-06-11 19:51:52 +0300

    fish: update fzf keybindings

--- core/fish/functions/fish_user_key_bindings.fish
+++ core/fish/functions/fish_user_key_bindings.fish
@@ -11,3 +11,5 @@ function fish_user_key_bindings
     bind -M insert -m default jk backward-char force-repaint
     # bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
 end
+
+fzf --fish | source
