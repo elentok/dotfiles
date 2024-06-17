5d45d4c5ea978cf38f816e485919a0c490f2020d
David Elentok <3david@gmail.com>
2024-06-17 22:01:39 +0300

    nvim: add toggleterm

---
+++ core/nvim/lua/elentok/plugins/toggleterm.lua
@@ -0,0 +1,9 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  opts = {
+    open_mapping = { [[<c-q>]], [[<D-q>]] },
+    -- open_mapping = { [[<c-t>]], [[<space>.]] },
+    --[[ things you want to change go here]]
+  },
+}
