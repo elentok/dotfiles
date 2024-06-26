4baf13008ffc806d25dd3331d953b44fc1559e23
David Elentok <3david@gmail.com>
2024-06-26 09:53:25 +0300

    fish: add git-nf function

---
+++ core/fish/conf.d/git.fish
@@ -0,0 +1,25 @@
+function git-nf
+    if test (count $argv) -eq 0
+        echo "Usage: git-nf <new-branch-name>"
+        return 1
+    end
+
+    cd (git-bare-root)
+    set branch_name $argv[1]
+
+    echo "About to create branch $(set_color green)$branch_name$(set_color normal)"
+    echo "  in $PWD"
+    echo
+    read -P "$(set_color yellow)Are you sure$(set_color normal) [y/N]? " -n1 -l response
+
+    if test "$response" != y
+        return
+    end
+
+    echo creating
+
+    git remote update upstream
+    git worktree add -b "$branch_name" "$branch_name"
+    cd "$branch_name"
+    git reset --hard upstream/main
+end
