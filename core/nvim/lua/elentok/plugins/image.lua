ded89aa5c916b0c6696caaa73c3dcb2a6dcc761e
David Elentok <3david@gmail.com>
2024-07-05 16:42:49 +0300

    nvim: image: better config

--- core/nvim/lua/elentok/plugins/image.lua
+++ core/nvim/lua/elentok/plugins/image.lua
@@ -18,6 +18,8 @@ return {
           only_render_image_at_cursor = true,
         },
       },
+      editor_only_render_when_focused = true, -- auto show/hide images when the editor gains/looses focus
+      tmux_show_only_in_active_window = true, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
       max_width = 100,
       max_wdith_window_percentage = 90,
     })
