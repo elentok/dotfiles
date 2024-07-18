ec79abba824ffcaeeed8753bd80391d7b51477da
David Elentok <3david@gmail.com>
2024-07-18 21:58:27 +0300

    nvim: move from format-on-save to conform

--- core/nvim/lua/elentok/lib/lsp.lua
+++ core/nvim/lua/elentok/lib/lsp.lua
@@ -1,57 +1,9 @@
-local has_lspconfig, lspconfig = pcall(require, "lspconfig")
-local has_cmp, cmp = pcall(require, "cmp_nvim_lsp")
-local has_aerial, aerial = pcall(require, "aerial")
-
-if not (has_lspconfig and has_cmp and has_aerial) then
-  return {
-    setup = function()
-      print("Warning: LSP not loaded, missing plugins.")
-    end,
-  }
+---@param name string
+---@return boolean
+local function hasLspClient(name)
+  return #vim.tbl_filter(function(client)
+    return client.name == name
+  end, vim.lsp.buf_get_clients()) > 0
 end
 
-local M = {}
-
-local function on_attach(client, bufnr)
-  put("Client", client.name, "attached to", bufnr)
-
-  -- Use <c-]> to go to definition
-  -- vim.api.nvim_buf_set_option(bufnr, "tagfunc", "v:lua.vim.lsp.tagfunc")
-end
-
-M.capabilities = cmp.default_capabilities()
-
--- For nvim-ufo
-M.capabilities.textDocument.foldingRange = {
-  dynamicRegistration = false,
-  lineFoldingOnly = true,
-}
-
-function M.setup(servers, capabilities)
-  for server, config in pairs(servers) do
-    if config == true then
-      config = {}
-    elseif config == false then
-      goto continue
-    end
-
-    config.capabilities = capabilities or M.capabilities
-
-    local original_on_attach
-    if config.on_attach then
-      original_on_attach = config.on_attach
-    end
-
-    config.on_attach = function(client, bufnr)
-      on_attach(client, bufnr)
-      if original_on_attach then
-        original_on_attach(client, bufnr)
-      end
-    end
-
-    lspconfig[server].setup(config)
-    ::continue::
-  end
-end
-
-return M
+return { hasLspClient = hasLspClient }
---
+++ core/nvim/lua/elentok/plugins/conform.lua
@@ -0,0 +1,53 @@
+local lsp = require("elentok.lib.lsp")
+
+local function typescript_formatter()
+  if lsp.hasLspClient("denols") then
+    return {} -- fallback to LSP
+  else
+    return { "prettierd" }
+  end
+end
+
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    { "<leader>oc", "<Cmd>ConformInfo<cr>", desc = "Open conform dialog" },
+  },
+  opts = {
+    format_on_save = function(bufnr)
+      local bufname = vim.api.nvim_buf_get_name(bufnr)
+      if bufname:match("/node_modules/") or bufname:match(".local/share/nvim/lazy") then
+        return
+      end
+
+      return {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      }
+    end,
+    formatters = {
+      qmkmd = {
+        command = "qmkmd",
+        args = { "format", "$FILENAME" },
+        stdin = false,
+        condition = function(_, ctx)
+          return vim.fs.basename(ctx.filename) ~= "layout.md"
+        end,
+      },
+    },
+    formatters_by_ft = {
+      lua = { "stylua" },
+      typescript = typescript_formatter,
+      typescriptreact = typescript_formatter,
+      javascript = { "prettierd" },
+      html = { "prettierd" },
+      markdown = { "prettierd", "qmkmd" },
+      python = { "black" },
+      sh = { "shfmt" },
+      zsh = { "shfmt" },
+      fish = { "fish_indent" },
+    },
+  },
+}
