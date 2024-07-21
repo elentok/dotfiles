46f3e6b21b6953ea1793ae73627d2ee6bc711a2b
David Elentok <3david@gmail.com>
2024-07-21 13:59:40 +0300

    nvim: dropbar: fix typescriptreact issue

--- core/nvim/lua/elentok/plugins/dropbar.lua
+++ core/nvim/lua/elentok/plugins/dropbar.lua
@@ -4,6 +4,24 @@ return {
   dependencies = {
     "nvim-telescope/telescope-fzf-native.nvim",
   },
+  opts = {
+    general = {
+      -- TODO: report this issue on dropbar.nvim
+      ---@type boolean|fun(buf: integer, win: integer, info: table?): boolean
+      enable = function(buf, win, _)
+        local ft = vim.bo[buf].ft
+        if ft == "typescriptreact" then
+          ft = "typescript"
+        end
+
+        return vim.api.nvim_buf_is_valid(buf)
+          and vim.api.nvim_win_is_valid(win)
+          and vim.wo[win].winbar == ""
+          and vim.fn.win_gettype(win) == ""
+          and ((pcall(vim.treesitter.get_parser, buf, ft)) and true or false)
+      end,
+    },
+  },
   lazy = false,
   keys = {
     {
