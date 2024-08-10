1b593d9729a7cdcc3b2bbcd0223b6373a18c6044
David Elentok <3david@gmail.com>
2024-08-10 22:05:32 +0300

    nvim: dropbar: remove obsolete bugfix

--- core/nvim/lua/elentok/plugins/dropbar.lua
+++ core/nvim/lua/elentok/plugins/dropbar.lua
@@ -4,32 +4,7 @@ return {
   dependencies = {
     "nvim-telescope/telescope-fzf-native.nvim",
   },
-  opts = {
-    general = {
-      -- TODO: report this issue on dropbar.nvim
-      ---@type boolean|fun(buf: integer, win: integer, info: table?): boolean
-      enable = function(buf, win, _)
-        local ft = vim.bo[buf].ft
-        if ft == "typescriptreact" then
-          ft = "typescript"
-        elseif ft == "sh" then
-          ft = "bash"
-        end
-
-        -- put("buf_is_valid", vim.api.nvim_buf_is_valid(buf))
-        -- put("win_is_valid", vim.api.nvim_win_is_valid(win))
-        -- put("vim.wo[win].winbar", vim.wo[win].winbar)
-        -- put("vim.fn.win_gettype(win)", vim.fn.win_gettype(win))
-        -- put("get_parser", pcall(vim.treesitter.get_parser, buf, ft))
-
-        return vim.api.nvim_buf_is_valid(buf)
-          and vim.api.nvim_win_is_valid(win)
-          and vim.wo[win].winbar == ""
-          and vim.fn.win_gettype(win) == ""
-          and ((pcall(vim.treesitter.get_parser, buf, ft)) and true or false)
-      end,
-    },
-  },
+  opts = {},
   lazy = false,
   keys = {
     {
