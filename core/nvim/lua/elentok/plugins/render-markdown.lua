6cce530afb0c3baa291a7950b1a2ce6045e7183b
David Elentok <3david@gmail.com>
2024-08-10 22:30:46 +0300

    nvim: render-markdown: better config

--- core/nvim/lua/elentok/plugins/render-markdown.lua
+++ core/nvim/lua/elentok/plugins/render-markdown.lua
@@ -6,6 +6,15 @@ return {
       icons = { "󰉫 ", "󰉬 ", "󰉭 ", "󰉮 ", "󰉯 ", "󰉰 " },
       sign = false,
     },
+    code = {
+      sign = false,
+      style = "normal",
+      width = "block",
+      left_pad = 1,
+      right_pad = 1,
+      position = "right",
+      border = "thin",
+    },
     checkbox = {
       unchecked = {
         icon = "󰄱",
@@ -26,15 +35,18 @@ return {
         },
       },
     },
+    -- Fix a bug with the callouts where it shows the "]" after the rendered
+    -- text (just added a space at the end)
+    callout = {
+      note = { raw = "[!NOTE]", rendered = "󰋽 Note " },
+      tip = { raw = "[!TIP]", rendered = "󰌶 Tip " },
+      important = { raw = "[!IMPORTANT]", rendered = "󰅾 Important " },
+      warning = { raw = "[!WARNING]", rendered = "󰀪 Warning " },
+      caution = { raw = "[!CAUTION]", rendered = "󰳦 Caution " },
+    },
   },
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons",
   },
 }
-
--- vim.api.nvim_set_hl()
-
--- ObsidianInProgress = { fg = "#EBCB8B" },
--- ObsidianWaiting = { fg = "#C27D00" },
--- ObsidianDone = { fg = "#6C7A96", bold = false },
