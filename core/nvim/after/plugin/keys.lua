011f27c900e4469f7dffe4ed7138569cc5b1ccbe
David Elentok <3david@gmail.com>
2024-08-21 10:04:05 +0300

    nvim: fix yank filename mapping

--- core/nvim/after/plugin/keys.lua
+++ core/nvim/after/plugin/keys.lua
@@ -93,7 +93,7 @@ vim.keymap.set("v", "<leader>ss", ":sort<cr>", { desc = "Sort" })
 vim.keymap.set("v", "<leader>st", ":!todo-sort<cr>", { desc = "Sort (todo)" })
 vim.keymap.set("n", "<leader>st", ":%!todo-sort<cr>", { desc = "Sort (todo)" })
 vim.keymap.set("n", "<leader>ya", ":%y+<cr>", { desc = "Yank entires file" })
-vim.keymap.set("n", "<leader>yf", ':let @+ = expand("%")<cr>', { desc = "Yank current filename" })
+vim.keymap.set("n", "<leader>yf", ':let @+ = expand("%:.")<cr>', { desc = "Yank current filename" })
 
 -- vim.keymap.set("n", "<leader>cl", function()
 --   term.run("FORCE_COLOR=1 mycal.ts | less", { w = 0.5 })
