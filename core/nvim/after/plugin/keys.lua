26f49d6e3c3ff12c7f20dcb9e5c9139edfcffc16
David Elentok <3david@gmail.com>
2024-09-03 16:49:52 +0300

    nvim: try m, as escape

--- core/nvim/after/plugin/keys.lua
+++ core/nvim/after/plugin/keys.lua
@@ -1,6 +1,7 @@
 local term = require("elentok/lib/terminal")
 -- vim: foldmethod=marker
 vim.keymap.set("i", "jk", "<esc>")
+vim.keymap.set("i", "m,", "<esc>")
 vim.keymap.set("n", "<leader>k", ":")
 vim.keymap.set("n", "<leader>;", ":")
 
