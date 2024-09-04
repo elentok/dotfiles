8ad4ae9502826c654e2d852ac5664fe36198aa83
David Elentok <3david@gmail.com>
2024-09-04 22:56:57 +0300

    nvim: fix rtl switcher

--- core/nvim/after/plugin/hebrew.lua
+++ core/nvim/after/plugin/hebrew.lua
@@ -6,6 +6,11 @@ local function toggleHebrew()
     vim.wo.rightleft = true
     vim.bo.keymap = "hebrew"
   end
+
+  vim.cmd("stopinsert")
+  vim.fn.timer_start(0, function()
+    vim.cmd("startinsert")
+  end)
 end
 
 vim.keymap.set({ "n", "i" }, "<c-\\>", toggleHebrew)
