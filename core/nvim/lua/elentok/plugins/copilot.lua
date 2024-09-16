d84ad460635fc5a509cfd16331939e01dd59cda0
David Elentok <3david@gmail.com>
2024-09-16 13:22:45 +0300

    nvim: add copilot chat

--- core/nvim/lua/elentok/plugins/copilot.lua
+++ core/nvim/lua/elentok/plugins/copilot.lua
@@ -16,4 +16,18 @@ return {
       require("copilot_cmp").setup()
     end,
   },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 }
