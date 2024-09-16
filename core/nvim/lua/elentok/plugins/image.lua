d696b6d7c52fc5a66ebc86f918cbe8721b7c09a8
David Elentok <3david@gmail.com>
2024-09-16 18:44:38 +0300

    nvim: disable image.nvim (not working well with wezterm)

--- core/nvim/lua/elentok/plugins/image.lua
+++ core/nvim/lua/elentok/plugins/image.lua
@@ -1,28 +1,31 @@
-package.path = package.path
-  .. ";"
-  .. vim.fn.expand("$HOME")
-  .. "/.luarocks/share/lua/5.1/?/init.lua;"
-package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
+return {}
 
-return {
-  "3rd/image.nvim",
-  config = function()
-    if vim.g.neovide then
-      return
-    end
+-- package.path = package.path
+--   .. ";"
+--   .. vim.fn.expand("$HOME")
+--   .. "/.luarocks/share/lua/5.1/?/init.lua;"
+-- package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
 
-    require("image").setup({
-      integrations = {
-        markdown = {
-          enabled = true,
-          only_render_image_at_cursor = true,
-        },
-      },
-      editor_only_render_when_focused = true, -- auto show/hide images when the editor gains/looses focus
-      tmux_show_only_in_active_window = true, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
-      max_width = 100,
-      max_width_window_percentage = 90,
-    })
-  end,
-  ft = { "markdown" },
-}
+-- return {
+--   "3rd/image.nvim",
+--   config = function()
+--     if vim.g.neovide then
+--       return
+--     end
+
+--     require("image").setup({
+--       -- backend = "ueberzug",
+--       integrations = {
+--         markdown = {
+--           enabled = true,
+--           -- only_render_image_at_cursor = true,
+--         },
+--       },
+--       -- editor_only_render_when_focused = true, -- auto show/hide images when the editor gains/looses focus
+--       -- tmux_show_only_in_active_window = true, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+--       -- max_width = 100,
+--       -- max_width_window_percentage = 90,
+--     })
+--   end,
+--   ft = { "markdown" },
+-- }
