6e7b25c1bfa4de8b30e3e9c15410f04bba2dc45d
David Elentok <3david@gmail.com>
2024-09-16 17:18:46 +0300

    nvim: remove c-u/d mappings from terminal

--- core/nvim/after/plugin/terminal.lua
+++ core/nvim/after/plugin/terminal.lua
@@ -1,7 +1,7 @@
 -- vim.keymap.set("t", "")
 
-vim.keymap.set("t", "<c-u>", "<c-\\><c-n><c-u>")
-vim.keymap.set("t", "<c-d>", "<c-\\><c-n><c-d>")
+-- vim.keymap.set("t", "<c-u>", "<c-\\><c-n><c-u>")
+-- vim.keymap.set("t", "<c-d>", "<c-\\><c-n><c-d>")
 vim.keymap.set("t", "<c-;>", "<c-\\><c-n>")
 
 -- local function map(mode, lhs, rhs)
