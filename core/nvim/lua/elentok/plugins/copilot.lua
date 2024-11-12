bf4dfded5f82cb544f2844640a39daddce145142
David Elentok <3david@gmail.com>
2024-11-12 11:05:25 +0200

    copilot: use claude

--- core/nvim/lua/elentok/plugins/copilot.lua
+++ core/nvim/lua/elentok/plugins/copilot.lua
@@ -25,7 +25,8 @@ return {
     },
     build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
-      debug = true, -- Enable debugging
+      model = "claude-3.5-sonnet",
+      -- debug = true, -- Enable debugging
       -- See Configuration section for rest
     },
     -- See Commands section for default commands if you want to lazy load on them
