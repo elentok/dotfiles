f7006e10fcc169dfe2255cc963d4e16c53d49b81
David Elentok <3david@gmail.com>
2025-01-02 21:29:31 +0200

    nvim: add image.nvim

--- core/nvim/lua/elentok/plugins/image.lua
+++ core/nvim/lua/elentok/plugins/image.lua
@@ -1,31 +1,28 @@
-return {}
+package.path = package.path
+  .. ";"
+  .. vim.fn.expand("$HOME")
+  .. "/.luarocks/share/lua/5.1/?/init.lua;"
+package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
 
--- package.path = package.path
---   .. ";"
---   .. vim.fn.expand("$HOME")
---   .. "/.luarocks/share/lua/5.1/?/init.lua;"
--- package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
+return {
+  "3rd/image.nvim",
+  config = function()
+    if vim.g.neovide then
+      return
+    end
 
--- return {
---   "3rd/image.nvim",
---   config = function()
---     if vim.g.neovide then
---       return
---     end
-
---     require("image").setup({
---       -- backend = "ueberzug",
---       integrations = {
---         markdown = {
---           enabled = true,
---           -- only_render_image_at_cursor = true,
---         },
---       },
---       -- editor_only_render_when_focused = true, -- auto show/hide images when the editor gains/looses focus
---       -- tmux_show_only_in_active_window = true, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
---       -- max_width = 100,
---       -- max_width_window_percentage = 90,
---     })
---   end,
---   ft = { "markdown" },
--- }
+    require("image").setup({
+      integrations = {
+        markdown = {
+          enabled = true,
+          only_render_image_at_cursor = true,
+        },
+      },
+      editor_only_render_when_focused = true, -- auto show/hide images when the editor gains/looses focus
+      tmux_show_only_in_active_window = true, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+      -- max_width = 100,
+      -- max_width_window_percentage = 90,
+    })
+  end,
+  ft = { "markdown" },
+}
