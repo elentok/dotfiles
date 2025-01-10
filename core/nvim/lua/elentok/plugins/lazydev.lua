feebc90b8d2ae3385d8afbf2393cdd8caa8fbefa
David Elentok <3david@gmail.com>
2025-01-10 22:26:54 +0200

    nvim: lazydev: update config

--- core/nvim/lua/elentok/plugins/lazydev.lua
+++ core/nvim/lua/elentok/plugins/lazydev.lua
@@ -3,12 +3,7 @@ return {
   ft = "lua", -- only load on lua files
   opts = {
     library = {
-      -- Library items can be absolute paths
-      -- "~/projects/my-awesome-lib",
-      -- Or relative, which means they will be resolved as a plugin
-      -- "LazyVim",
-      -- When relative, you can also provide a path to the library in the plugin dir
-      "luvit-meta/library", -- see below
+      { path = "${3rd}/luv/library", words = { "vim%.uv" } },
     },
   },
   dependencies = {
