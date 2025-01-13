b5990020f6e88904843d0f03b70e90ad54a50577
David Elentok <3david@gmail.com>
2025-01-13 19:17:57 +0200

    nvim: add treesitter textobjects

--- core/nvim/lua/elentok/plugins/mini.lua
+++ core/nvim/lua/elentok/plugins/mini.lua
@@ -6,5 +6,21 @@ return {
   { "echasnovski/mini.jump", version = "*", opts = {} },
   { "echasnovski/mini.comment", version = "*", opts = {} },
   { "echasnovski/mini.pairs", version = "*", opts = {} },
-  { "echasnovski/mini.ai", version = "*", opts = {} },
+  {
+    "echasnovski/mini.ai",
+    version = "*",
+    config = function()
+      local spec_treesitter = require("mini.ai").gen_spec.treesitter
+      require("mini.ai").setup({
+        custom_textobjects = {
+          -- Function definition (needs treesitter queries with these captures)
+          f = spec_treesitter({ a = "@function.outer", i = "@function.inner" }),
+          o = spec_treesitter({
+            a = { "@conditional.outer", "@loop.outer" },
+            i = { "@conditional.inner", "@loop.inner" },
+          }),
+        },
+      })
+    end,
+  },
 }
