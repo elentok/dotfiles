7861a3a19bc8b1208b50122b9206122d78f652d5
David Elentok <3david@gmail.com>
2025-01-21 19:08:33 +0200

    nvim: fix gitsigns mappings

--- core/nvim/lua/elentok/plugins/gitsigns.lua
+++ core/nvim/lua/elentok/plugins/gitsigns.lua
@@ -24,8 +24,8 @@ return {
     },
     { "<leader>gpp", ":Gitsigns preview_hunk<cr>", desc = "Git preview hunk" },
     { "<leader>gi", ":Gitsigns preview_hunk_inline<cr>", desc = "Git preview hunk (inline)" },
-    { "[c", ":Gitsigns prev_hunk<cr>", desc = "Prev git hunk" },
-    { "]c", ":Gitsigns next_hunk<cr>", desc = "Next git hunk" },
+    { "[n", ":Gitsigns prev_hunk<cr>", desc = "Prev git hunk" },
+    { "]n", ":Gitsigns next_hunk<cr>", desc = "Next git hunk" },
 
     -- Text object
     { "ih", ":<C-U>Gitsigns select_hunk<CR>", mode = { "o", "x" } },
