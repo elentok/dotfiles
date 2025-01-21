137a9852c82b3d8e2047b80fac30354fef4a9f75
David Elentok <3david@gmail.com>
2025-01-21 20:19:43 +0200

    nvim: map <c-p> to toggle lazygit

--- core/nvim/after/plugin/lazygit.lua
+++ core/nvim/after/plugin/lazygit.lua
@@ -4,10 +4,19 @@ require("toggleterm.terminal")
 --- @type Terminal | nil
 local lazygit = nil
 
-vim.keymap.set("n", "<leader>lg", function()
+local function openLazyGit()
   if lazygit == nil then
-    lazygit = term.run("lazygit", { wait = false })
+    lazygit = term.run("lazygit", {
+      wait = false,
+      ---@param trm Terminal
+      on_open = function(trm)
+        vim.keymap.set({ "i", "t", "n" }, "<c-p>", "<cmd>close<cr>", { buffer = trm.bufnr })
+      end,
+    })
   else
     lazygit:open()
   end
-end)
+end
+
+vim.keymap.set("n", "<leader>lg", openLazyGit)
+vim.keymap.set("n", "<c-p>", openLazyGit)
