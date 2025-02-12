1e8805164e6567cb7f297ad25309a4e2881086ff
David Elentok <3david@gmail.com>
2025-02-12 11:41:09 +0200

    nvim: organize typescript imports

--- core/nvim/lua/elentok/plugins/vtsls.lua
+++ core/nvim/lua/elentok/plugins/vtsls.lua
@@ -1,4 +1,11 @@
 return {
   "yioneko/nvim-vtsls",
   ft = { "typescript", "typescriptreact", "javascript" },
+  init = function()
+    -- Organize imports before saving
+    vim.api.nvim_create_autocmd(
+      { "BufWritePre" },
+      { pattern = { "*.ts", "*.tsx", "*.js" }, command = "VtsExec organize_imports" }
+    )
+  end,
 }
