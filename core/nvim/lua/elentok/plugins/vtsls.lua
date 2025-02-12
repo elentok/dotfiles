31f217746bd8314df6c299b1cc56b3970de4f5c0
David Elentok <3david@gmail.com>
2025-02-12 14:25:29 +0200

    nvim: disable auto organize imports

--- core/nvim/lua/elentok/plugins/vtsls.lua
+++ core/nvim/lua/elentok/plugins/vtsls.lua
@@ -3,9 +3,13 @@ return {
   ft = { "typescript", "typescriptreact", "javascript" },
   init = function()
     -- Organize imports before saving
-    vim.api.nvim_create_autocmd(
-      { "BufWritePre" },
-      { pattern = { "*.ts", "*.tsx", "*.js" }, command = "VtsExec organize_imports" }
-    )
+    -- vim.api.nvim_create_autocmd(
+    --   { "BufWritePre" },
+    --   { pattern = { "*.ts", "*.tsx", "*.js" }, command = "VtsExec organize_imports" }
+    -- )
   end,
+  keys = {
+    "<leader>oi",
+    "VtsExec organize_imports",
+  },
 }
