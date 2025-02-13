6fa74d47de0ee4d5d6adc349687c59789405291e
David Elentok <3david@gmail.com>
2025-02-13 18:00:17 +0200

    nvim: vtsls: fix organize imports mapping

--- core/nvim/lua/elentok/plugins/vtsls.lua
+++ core/nvim/lua/elentok/plugins/vtsls.lua
@@ -1,15 +1,15 @@
 return {
   "yioneko/nvim-vtsls",
   ft = { "typescript", "typescriptreact", "javascript" },
-  init = function()
-    -- Organize imports before saving
-    -- vim.api.nvim_create_autocmd(
-    --   { "BufWritePre" },
-    --   { pattern = { "*.ts", "*.tsx", "*.js" }, command = "VtsExec organize_imports" }
-    -- )
-  end,
-  keys = {
+  -- init = function()
+  --   -- Organize imports before saving
+  --   -- vim.api.nvim_create_autocmd(
+  --   --   { "BufWritePre" },
+  --   --   { pattern = { "*.ts", "*.tsx", "*.js" }, command = "VtsExec organize_imports" }
+  --   -- )
+  -- end,
+  keys = { {
     "<leader>oi",
-    "VtsExec organize_imports",
-  },
+    ":VtsExec organize_imports<cr>",
+  } },
 }
