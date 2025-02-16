47a5d32c7692df84c16d18bda9c1c77403e3e4e1
David Elentok <3david@gmail.com>
2025-02-16 15:18:03 +0200

    nvim: disable marksman
    It's not helping much atm and interfers with obsidian

--- core/nvim/plugin/lsp-setup.lua
+++ core/nvim/plugin/lsp-setup.lua
@@ -63,8 +63,6 @@ setup("ruff")
 --   },
 -- })
 
-setup("marksman")
-
 require("lspconfig.configs").vtsls = require("vtsls").lspconfig
 
 -- Find the absolute path to the local project's tsserver (to avoid using the
