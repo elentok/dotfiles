639a759c13e12dcaf745851be2c2e2dd906340fe
David Elentok <3david@gmail.com>
2025-02-17 19:08:09 +0200

    nvim: replace smart-splits with tmux.nvim

--- core/nvim/lua/elentok/plugins/smart-splits.lua
+++ core/nvim/lua/elentok/plugins/smart-splits.lua
@@ -1,38 +1,41 @@
----@module "lazy"
----@type LazySpec
-return {
-  "mrjones2014/smart-splits.nvim",
-  -- version = ">= 1.0.0",
-  keys = {
-    {
-      "<C-h>",
-      function()
-        require("smart-splits").move_cursor_left()
-      end,
-    },
-    {
-      "<C-j>",
-      function()
-        require("smart-splits").move_cursor_down()
-      end,
-    },
-    {
-      "<C-k>",
-      function()
-        require("smart-splits").move_cursor_up()
-      end,
-    },
-    {
-      "<C-l>",
-      function()
-        require("smart-splits").move_cursor_right()
-      end,
-    },
-    {
-      "<C-\\>",
-      function()
-        require("smart-splits").move_cursor_previous()
-      end,
-    },
-  },
-}
+return {}
+
+-- ---@module "lazy"
+-- ---@type LazySpec
+-- return {
+--   "mrjones2014/smart-splits.nvim",
+--   -- lazy = false,
+--   opts = {},
+--   keys = {
+--     {
+--       "<C-h>",
+--       function()
+--         require("smart-splits").move_cursor_left()
+--       end,
+--     },
+--     {
+--       "<c-j>",
+--       function()
+--         require("smart-splits").move_cursor_down()
+--       end,
+--     },
+--     {
+--       "<C-k>",
+--       function()
+--         require("smart-splits").move_cursor_up()
+--       end,
+--     },
+--     {
+--       "<C-l>",
+--       function()
+--         require("smart-splits").move_cursor_right()
+--       end,
+--     },
+--     {
+--       "<C-\\>",
+--       function()
+--         require("smart-splits").move_cursor_previous()
+--       end,
+--     },
+--   },
+-- }
---
+++ core/nvim/lua/elentok/plugins/tmux.lua
@@ -0,0 +1,6 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  "aserowy/tmux.nvim",
+  opts = {},
+}
