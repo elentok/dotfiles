3c697dbbab5407ddea6b598ae0a70fb282cd20f5
David Elentok <3david@gmail.com>
2025-02-27 13:43:05 +0200

    nvim: bring back flash plugin

--- core/nvim/lua/elentok/plugins/flash.lua
+++ core/nvim/lua/elentok/plugins/flash.lua
@@ -1,44 +1,43 @@
-return {}
--- return {
---   "folke/flash.nvim",
---   event = "VeryLazy",
---   opts = {
---     search = {
---       multi_window = false,
---     },
---     modes = {
---       search = {
---         enabled = false,
---       },
---       char = {
---         enabled = false,
---       },
---     },
---   },
---   keys = {
---     {
---       "s",
---       mode = { "n", "x", "o" },
---       function()
---         require("flash").jump()
---       end,
---       desc = "Flash",
---     },
---     {
---       "<space>tv",
---       mode = { "n", "x", "o" },
---       function()
---         require("flash").treesitter()
---       end,
---       desc = "Flash",
---     },
---     {
---       "<space>ts",
---       mode = { "n", "x", "o" },
---       function()
---         require("flash").treesitter_search()
---       end,
---       desc = "Flash (treesitter)",
---     },
---   },
--- }
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {
+    search = {
+      multi_window = false,
+    },
+    modes = {
+      search = {
+        enabled = false,
+      },
+      char = {
+        enabled = false,
+      },
+    },
+  },
+  keys = {
+    {
+      "ss",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
+    },
+    {
+      "<space>tv",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Flash",
+    },
+    {
+      "<space>ts",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").treesitter_search()
+      end,
+      desc = "Flash (treesitter)",
+    },
+  },
+}
--- core/nvim/lua/elentok/plugins/mini.lua
+++ core/nvim/lua/elentok/plugins/mini.lua
@@ -3,7 +3,7 @@ return {
   { "echasnovski/mini.icons", version = "*", opts = {} },
   { "echasnovski/mini.bracketed", version = "*", opts = {} },
   { "echasnovski/mini.surround", version = "*", opts = {} },
-  { "echasnovski/mini.jump", version = "*", opts = {} },
+  -- { "echasnovski/mini.jump", version = "*", opts = {} },
   { "echasnovski/mini.comment", version = "*", opts = {} },
   { "echasnovski/mini.pairs", version = "*", opts = {} },
   {
