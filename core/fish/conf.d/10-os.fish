13f18efdaa687b5bb5af8cf7cb33c0966feb6c1e
David Elentok <3david@gmail.com>
2025-02-27 13:40:22 +0200

    fish: optimize os identification

--- core/fish/conf.d/10-os.fish
+++ core/fish/conf.d/10-os.fish
@@ -1,13 +1,14 @@
-if uname -s | string match -q Darwin
-    set -x OS mac
+# if uname -s | string match -q Darwin
+if test -f /System/Library/CoreServices/SystemVersion.plist
+    set -x DOTF_OS mac
 else
-    set -x OS linux
+    set -x DOTF_OS linux
 end
 
 function dotf-is-mac
-    test "$OS" = mac
+    test "$DOTF_OS" = mac
 end
 
 function dotf-is-linux
-    test "$OS" = linux
+    test "$DOTF_OS" = linux
 end
