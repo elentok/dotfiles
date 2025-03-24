f8dafae52ddfb26406793962d7b0095cb1a4beea
David Elentok <3david@gmail.com>
2025-03-24 15:07:17 +0200

    nvim: remove mini.comment

--- core/nvim/lua/elentok/plugins/mini.lua
+++ core/nvim/lua/elentok/plugins/mini.lua
@@ -4,7 +4,7 @@ return {
   { "echasnovski/mini.bracketed", version = "*", opts = {} },
   { "echasnovski/mini.surround", version = "*", opts = {} },
   -- { "echasnovski/mini.jump", version = "*", opts = {} },
-  { "echasnovski/mini.comment", version = "*", opts = {} },
+  -- { "echasnovski/mini.comment", version = "*", opts = {} },
   { "echasnovski/mini.pairs", version = "*", opts = {} },
   {
     "echasnovski/mini.ai",
