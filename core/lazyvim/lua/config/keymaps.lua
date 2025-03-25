dd16539c8665afefa921255ef91a03d7325bb339
David Elentok <3david@gmail.com>
2025-03-25 09:56:39 +0200

    lazyvim: add tmux + keymaps

--- core/lazyvim/lua/config/keymaps.lua
+++ core/lazyvim/lua/config/keymaps.lua
@@ -3,3 +3,34 @@
 -- Add any additional keymaps here
 
 vim.keymap.set({ "i" }, "jk", "<esc>")
+
+-- Inspired by Helix
+vim.keymap.set({ "n", "v" }, "gh", "0", { desc = "Go to beginning of the line" })
+vim.keymap.set({ "n", "v" }, "ge", "G", { desc = "Go to end of the file" })
+vim.keymap.set({ "n", "v" }, "gl", "$", { desc = "Go to end of the line" })
+vim.keymap.set({ "n", "v" }, "gs", "_", { desc = "Go to first non-blank character" })
+
+-- Map <leader>n and <leader>p to [ and ] to make it easier to use unimpaired
+-- mappings without [ and ]
+-- vim.keymap.set({ "n", "v" }, "<leader>p", "[", { remap = true })
+-- vim.keymap.set({ "n", "v" }, "<leader>n", "]", { remap = true })
+
+vim.keymap.set({ "n", "v" }, "<leader>vv", "<c-v>", { desc = "Go into block visual mode" })
+vim.keymap.set("n", "vv", "V", { desc = "Go into visual line mode" })
+vim.keymap.set("n", "vb", "<c-v>", { desc = "Go into visual block mode" })
+
+vim.keymap.set("n", "<c-l>", function()
+  require("tmux").move_right()
+end)
+
+vim.keymap.set("n", "<c-h>", function()
+  require("tmux").move_left()
+end)
+
+vim.keymap.set("n", "<c-k>", function()
+  require("tmux").move_up()
+end)
+
+vim.keymap.set("n", "<c-j>", function()
+  require("tmux").move_down()
+end)
---
+++ core/lazyvim/lua/plugins/tmux.lua
@@ -0,0 +1,16 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  "aserowy/tmux.nvim",
+  opts = {
+    copy_sync = {
+      enable = false,
+    },
+    navigation = {
+      cycle_navigation = false,
+    },
+    resize = {
+      enable_default_keybindings = false,
+    },
+  },
+}
