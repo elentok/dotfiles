0d28129abda4e0a13e7e05cdcf1aba6fc8df100b
David Elentok <3david@gmail.com>
2025-03-26 22:26:24 +0200

    lazyvim: move more stuff to stuff.nvim

--- core/lazyvim/lua/plugins/elentok.lua
+++ core/lazyvim/lua/plugins/elentok.lua
@@ -16,25 +16,25 @@ return {
   },
 
   -- Open Link
-  {
-    "elentok/open-link.nvim",
-    lazy = false,
-    config = function()
-      local expanders = require("open-link.expanders")
-      require("open-link").setup({
-        expanders = {
-          expanders.homedir(),
-          expanders.github,
-          expanders.github_issue_or_pr("format-on-save", "elentok/format-on-save.nvim"),
-        },
-      })
-    end,
-    keys = {
-      {
-        "<Leader>ip",
-        "<cmd>PasteImage<cr>",
-        desc = "Paste image from clipboard",
-      },
-    },
-  },
+  -- {
+  --   "elentok/open-link.nvim",
+  --   lazy = false,
+  --   config = function()
+  --     local expanders = require("open-link.expanders")
+  --     require("open-link").setup({
+  --       expanders = {
+  --         expanders.homedir(),
+  --         expanders.github,
+  --         expanders.github_issue_or_pr("format-on-save", "elentok/format-on-save.nvim"),
+  --       },
+  --     })
+  --   end,
+  --   keys = {
+  --     {
+  --       "<Leader>ip",
+  --       "<cmd>PasteImage<cr>",
+  --       desc = "Paste image from clipboard",
+  --     },
+  --   },
+  -- },
 }
--- core/lazyvim/lua/plugins/stuff.lua
+++ core/lazyvim/lua/plugins/stuff.lua
@@ -4,11 +4,27 @@
 return {
   "elentok/stuff.nvim",
   dev = true,
-  ---@type StuffOptions
-  opts = {
-    toggleWord = {},
-  },
+  config = function()
+    local expanders = require("stuff.open-link.expanders")
+    require("stuff").setup({
+      toggle_word = {},
+      paste_image = true,
+      open_link = {
+        expanders = {
+          expanders.homedir(),
+          expanders.github,
+          expanders.github_issue_or_pr("format-on-save", "elentok/format-on-save.nvim"),
+        },
+      },
+    })
+  end,
   keys = {
     { "<leader>tw" },
+    { "gx" },
+    {
+      "<Leader>ip",
+      "<cmd>PasteImage<cr>",
+      desc = "Paste image from clipboard",
+    },
   },
 }
