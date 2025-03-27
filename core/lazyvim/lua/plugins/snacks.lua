2ec5cd5eaa4d259a6ffed2c87dbd0c7ff49e1087
David Elentok <3david@gmail.com>
2025-03-27 11:54:21 +0200

    lazyvim: add snacks mappings

---
+++ core/lazyvim/lua/plugins/snacks.lua
@@ -0,0 +1,40 @@
+---@module "snacks"
+
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    picker = {
+      win = {
+        input = {
+          keys = {
+            ["@"] = "qflist",
+          },
+        },
+      },
+    },
+
+    image = { enabled = true, markdown = { enabled = true } },
+  },
+  styles = {
+    notification = {
+      wo = { wrap = true }, -- Wrap notifications
+    },
+  },
+  keys = {
+    {
+      "<leader>w/",
+      function()
+        Snacks.picker.grep_word()
+      end,
+      desc = "Grep word",
+    },
+    {
+      "``",
+      function()
+        Snacks.picker.resume()
+      end,
+      desc = "Resume last picker",
+    },
+  },
+}
