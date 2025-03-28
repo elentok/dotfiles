bef125f9eff989f95b694dcce251b88b7ba2e089
David Elentok <3david@gmail.com>
2025-03-28 21:15:43 +0300

    lazyvim: add navic

---
+++ core/lazyvim/lua/plugins/lazyvim-overrides.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      -- dont show the winbar for some filetypes
+      opts.options.disabled_filetypes.winbar = { "dashboard", "lazy", "alpha" }
+      -- remove navic from the statusline
+      local navic = table.remove(opts.sections.lualine_c)
+      -- add it to the winbar instead
+      opts.winbar = { lualine_b = { "filename" }, lualine_c = { navic } }
+      opts.inactive_winbar = { lualine_b = { "filename" } }
+    end,
+  },
+}
