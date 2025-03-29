8dc98d8326a48d71c8a08f6d6a6f7acb7be7372f
David Elentok <3david@gmail.com>
2025-03-29 21:53:36 +0300

    lazyvim: lualine oil support

--- core/lazyvim/lua/plugins/lazyvim-overrides.lua
+++ core/lazyvim/lua/plugins/lazyvim-overrides.lua
@@ -21,12 +21,15 @@ return {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
       -- dont show the winbar for some filetypes
-      opts.options.disabled_filetypes.winbar = { "dashboard", "lazy", "alpha" }
+      opts.options.disabled_filetypes.winbar = { "dashboard", "lazy", "alpha", "snacks_dashboard" }
       -- remove navic from the statusline
       local navic = table.remove(opts.sections.lualine_c)
+
       -- add it to the winbar instead
       opts.winbar = { lualine_b = { "filename" }, lualine_c = { navic } }
       opts.inactive_winbar = { lualine_b = { "filename" } }
+
+      table.insert(opts.extensions, require("elentok.lualine-oil-extension"))
     end,
   },
 }
