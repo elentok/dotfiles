bf6be29341fbbe775a3941a46ec68eae27a690a5
David Elentok <3david@gmail.com>
2025-03-30 09:34:03 +0300

    lazyvim: emoji: add insert mode mapping

--- core/lazyvim/lua/plugins/emoji.lua
+++ core/lazyvim/lua/plugins/emoji.lua
@@ -2,12 +2,21 @@ return {
   "allaman/emoji.nvim",
   ft = "markdown",
   opts = {},
+  cmds = { "Emoji" },
   keys = {
     {
       "<leader>ie",
       function()
         require("emoji").insert()
       end,
+      desc = "Insert emoji",
+    },
+    {
+      "<c-x><c-i>",
+      function()
+        require("emoji").insert()
+      end,
+      mode = "i",
       desc = "Insert emoji",
     },
   },
