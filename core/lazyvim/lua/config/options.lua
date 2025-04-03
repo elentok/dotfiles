68d42e5300e933f1652d12915d631523634a645d
David Elentok <3david@gmail.com>
2025-04-03 14:48:08 +0300

    lazyvim: improve diagnostic setup

--- core/lazyvim/lua/config/options.lua
+++ core/lazyvim/lua/config/options.lua
@@ -7,3 +7,19 @@ vim.g.snacks_animate = false
 vim.g.lazyvim_eslint_auto_format = false
 
 vim.opt.splitkeep = "cursor"
+
+vim.diagnostic.config({
+  float = {
+    border = "rounded",
+  },
+  virtual_text = {
+    severity = {
+      max = vim.diagnostic.severity.WARN,
+    },
+  },
+  virtual_lines = {
+    severity = {
+      min = vim.diagnostic.severity.ERROR,
+    },
+  },
+})
