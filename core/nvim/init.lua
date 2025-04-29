623066a04f174fa7c8333a77fda56cf509948bbe
David Elentok <3david@gmail.com>
2025-04-29 22:56:21 +0300

    nvim: add lsp support

--- core/nvim/init.lua
+++ core/nvim/init.lua
@@ -1,3 +1,4 @@
 require("config.settings")
 require("config.lazy")
 require("config.keymaps")
+require("config.fish")
---
+++ core/nvim/lsp/fish.lua
@@ -0,0 +1,6 @@
+return {
+  cmd = { 'fish-lsp', 'start' },
+  cmd_env = { fish_lsp_show_client_popups = false },
+  filetypes = { 'fish' },
+  root_markers = { '.git' },
+} 
