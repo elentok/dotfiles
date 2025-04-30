021713ec79581d1ab996e9bfb4f0e0b5a74a2141
David Elentok <3david@gmail.com>
2025-04-30 21:47:58 +0300

    nvim: lazy: use catppuccin

--- core/nvim/lua/config/lazy.lua
+++ core/nvim/lua/config/lazy.lua
@@ -33,7 +33,7 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "catppuccin" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
   ui = {
