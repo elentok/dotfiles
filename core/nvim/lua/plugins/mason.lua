e59b4de62a3a88c527ad855962326e4ce61f38e0
David Elentok <3david@gmail.com>
2025-04-30 22:34:51 +0300

    nvim: add mason-tool-installer

--- core/nvim/lua/plugins/mason.lua
+++ core/nvim/lua/plugins/mason.lua
@@ -1,4 +1,23 @@
 return {
-  "williamboman/mason.nvim",
-  opts = {},
+  "WhoIsSethDaniel/mason-tool-installer.nvim",
+  dependencies = {
+    "williamboman/mason.nvim",
+    opts = {},
+  },
+
+  opts = {
+    ensure_installed = {
+      "bash-language-server",
+      "eslint_d",
+      "harper-ls",
+      "json-lsp",
+      "lua-language-server",
+      "marksman",
+      "openscad-lsp",
+      "prettierd",
+      "stylua",
+      "vtsls",
+      "yaml-language-server",
+    },
+  },
 }
