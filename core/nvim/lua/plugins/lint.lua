a25fae9e9ab839062749f7b90d558e2621b8934a
David Elentok <3david@gmail.com>
2025-04-30 21:19:37 +0300

    nvim: add lint

---
+++ core/nvim/lua/plugins/lint.lua
@@ -0,0 +1,25 @@
+return {
+  "mfussenegger/nvim-lint",
+  ft = { "typescript", "typescriptreact" },
+  config = function()
+    local lint = require("lint")
+
+    lint.linters_by_ft = {
+      typescript = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+    }
+
+    vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePost" }, {
+      group = vim.api.nvim_create_augroup("Elentok_Lint", {}),
+      callback = function() lint.try_lint() end,
+    })
+
+    vim.api.nvim_create_user_command("Lint", function() lint.try_lint() end, {})
+  end,
+
+  condition = function()
+    return vim.fn.findfile("eslint.config.js", ";.") ~= ""
+      or vim.fn.findfile(".eslintrc.js", ";.") ~= ""
+      or vim.fn.findfile("eslintrc.js", ";.") ~= ""
+  end,
+}
