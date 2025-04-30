d278f8c185ea9d531bce38c78d565416bead9bc1
David Elentok <3david@gmail.com>
2025-04-30 21:44:44 +0300

    nvim: add render-markdown

---
+++ core/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,42 @@
+---@module 'render-markdown'
+
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    ---@type render.md.UserConfig
+    bullet = {
+      icons = { " " },
+    },
+    checkbox = {
+      enabled = true,
+      unchecked = {
+        icon = "󱓼",
+      },
+      checked = {
+        icon = "󰄬",
+        highlight = "NonText",
+        scope_highlight = "NonText",
+      },
+      custom = {
+        in_progress = {
+          raw = "[/]",
+          rendered = "󰪠",
+          highlight = "NeogitGraphYellow",
+          scope_highlight = "NeogitGraphYellow",
+        },
+        waiting = {
+          raw = "[w]",
+          rendered = "󰏦",
+          highlight = "MiniIconsOrange",
+          scope_highlight = "MiniIconsOrange",
+        },
+        code_review = {
+          raw = "[r]",
+          rendered = "",
+          highlight = "MiniIconsPurple",
+          scope_highlight = "MiniIconsPurple",
+        },
+      },
+    },
+  },
+}
