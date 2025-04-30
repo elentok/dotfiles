46a3dfeca1373bbbceafeed5c57a2d6062aba2e8
David Elentok <3david@gmail.com>
2025-04-30 14:46:10 +0300

    nvim: update settings

--- core/nvim/lua/config/settings.lua
+++ core/nvim/lua/config/settings.lua
@@ -4,11 +4,17 @@ vim.g.maplocalleader = ","
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.laststatus = 3
+vim.o.wrap = false
 
 vim.o.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
+
+-- When splitting vertically move the focus to the right window.
+vim.o.splitright = true
+
+vim.o.clipboard = "unnamedplus"
 
 vim.diagnostic.config({
   virtual_text = {
