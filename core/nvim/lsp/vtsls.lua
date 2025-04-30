5c351c4109d680811dc6e945b9d82f7115da2ca7
David Elentok <3david@gmail.com>
2025-04-30 14:46:01 +0300

    nvim: add denols and vtsls

---
+++ core/nvim/lsp/vtsls.lua
@@ -0,0 +1,12 @@
+return {
+  cmd = { "vtsls", "--stdio" },
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx",
+  },
+  root_markers = { "tsconfig.json", "package.json", "jsconfig.json", ".git" },
+}
--- core/nvim/lua/config/lsp.lua
+++ core/nvim/lua/config/lsp.lua
@@ -1,1 +1,6 @@
-vim.lsp.enable({ "fish", "lua" })
+local typescript_env = require("elentok.typescript-env")
+
+local ts_server = "vtsls"
+if typescript_env.mode == "deno" then ts_server = "denols" end
+
+vim.lsp.enable({ "fish", "lua", ts_server })
