57bd4a75164555744be4b883633f7db26b505c63
David Elentok <3david@gmail.com>
2025-05-04 09:38:45 +0300

    nvim: don't show message when no .no-ai file found

--- core/nvim/lua/elentok/ai.lua
+++ core/nvim/lua/elentok/ai.lua
@@ -1,9 +1,5 @@
 local allow = vim.fn.findfile(".no-ai", ";.") == ""
 
-if not allow then
-  print("Found .no-ai, not initializing AI plugins")
-else
-  print("No .no-ai found, AI plugins allowed")
-end
+if not allow then print(" Found .no-ai, not initializing AI plugins") end
 
 return { allow = allow }
