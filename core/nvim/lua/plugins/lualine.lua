a75ec0b3a3179b0b2703e5f9cb5effdf766b22e5
David Elentok <3david@gmail.com>
2025-05-09 22:24:45 +0300

    nvim: lualine improvements

--- core/nvim/lua/plugins/lualine.lua
+++ core/nvim/lua/plugins/lualine.lua
@@ -1,5 +1,18 @@
 local spinner = { "", "", "", "", "", "" }
 
+local filename = {
+  "filename",
+  symbols = {
+    modified = " ",
+    readonly = " ",
+    unnamed = " No name",
+  },
+}
+
+local long_filename_block = vim.tbl_extend("force", filename, {
+  path = 1, -- relative path
+})
+
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "echasnovski/mini.icons" },
@@ -8,15 +21,17 @@ return {
       theme = "catppuccin",
       disabled_filetypes = {
         statusline = { "snacks_dashboard" },
+        winbar = { "snacks_dashboard" },
       },
       section_separators = { left = "", right = "" },
       component_separators = { left = "", right = "" },
     },
     sections = {
+      lualine_c = { long_filename_block },
       lualine_x = { { "lsp_status", symbols = { spinner = spinner } } },
     },
-    winbar = { lualine_b = { "filename" } },
-    inactive_winbar = { lualine_b = { "filename" } },
+    winbar = { lualine_b = { filename } },
+    inactive_winbar = { lualine_b = { filename } },
     extensions = { require("elentok.lualine-oil-extension") },
   },
 }
