ba55b14b1df79615d273f2bcf211a3e868784591
David Elentok <3david@gmail.com>
2025-05-11 12:10:13 +0300

    nvim: neovide: support changing font size

--- core/nvim/lua/elentok/neovide.lua
+++ core/nvim/lua/elentok/neovide.lua
@@ -1,6 +1,4 @@
-if not vim.g.neovide then
-  return
-end
+if not vim.g.neovide then return end
 
 -- Disable hinting as it's causing the font to look too bold
 -- (see https://neovide.dev/configuration.html)
@@ -16,7 +14,12 @@ local function map_cmd_to_ctrl()
   local keys = "abcdefghijklmnopqrstuwxyz[]\\^_0"
   for i = 1, #keys do
     local key = string.sub(keys, i, i)
-    vim.keymap.set({ "t", "n", "i", "v", "x", "c" }, "<D-" .. key .. ">", "<c-" .. key .. ">", { remap = true })
+    vim.keymap.set(
+      { "t", "n", "i", "v", "x", "c" },
+      "<D-" .. key .. ">",
+      "<c-" .. key .. ">",
+      { remap = true }
+    )
   end
 end
 
@@ -25,5 +28,17 @@ vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
 vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
 vim.keymap.set("i", "<D-v>", "<C-R>+") -- Paste insert mode
 vim.keymap.set("t", "<D-v>", "<C-\\><c-n>pi") -- Paste terminal mode
+
+vim.keymap.set(
+  { "n", "v" },
+  "<D-=>",
+  function() vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1 end
+)
+vim.keymap.set(
+  { "n", "v" },
+  "<D-->",
+  function() vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1 end
+)
+vim.keymap.set({ "n", "v" }, "<D-0>", function() vim.g.neovide_scale_factor = 1 end)
 
 map_cmd_to_ctrl()
