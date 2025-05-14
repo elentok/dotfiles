8668c27da6834ccb47124d1c12c78b07d026f280
David Elentok <3david@gmail.com>
2025-05-14 14:54:45 +0300

    nvim: add init-pager

---
+++ core/nvim/init-pager.lua
@@ -0,0 +1,7 @@
+vim.keymap.set("n", "q", "<Cmd>q<cr>")
+vim.keymap.set("n", "vv", "V")
+vim.keymap.set("n", "vc", "<c-v>")
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.wildignorecase = true
+vim.opt.clipboard = { "unnamed", "unnamedplus" }
