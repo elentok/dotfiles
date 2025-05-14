409c7b7f8f14b37029163acf33de31c23d061cd7
David Elentok <3david@gmail.com>
2025-05-14 11:23:25 +0300

    nvim: only load tmux plugin inside tmux

--- core/nvim/lua/plugins/tmux.lua
+++ core/nvim/lua/plugins/tmux.lua
@@ -13,30 +13,23 @@ return {
       enable_default_keybindings = false,
     },
   },
+  cond = vim.env.TMUX ~= nil,
   keys = {
     {
       "<c-h>",
-      function()
-        require("tmux").move_left()
-      end,
+      function() require("tmux").move_left() end,
     },
     {
       "<c-j>",
-      function()
-        require("tmux").move_bottom()
-      end,
+      function() require("tmux").move_bottom() end,
     },
     {
       "<c-k>",
-      function()
-        require("tmux").move_top()
-      end,
+      function() require("tmux").move_top() end,
     },
     {
       "<c-l>",
-      function()
-        require("tmux").move_right()
-      end,
+      function() require("tmux").move_right() end,
     },
   },
 }
