554ab291c8d3a2725c1599a172a0f192702ef551
David Elentok <3david@gmail.com>
2025-05-18 21:17:28 +0300

    transparent background

--- core/nvim/lua/plugins/catppuccin.lua
+++ core/nvim/lua/plugins/catppuccin.lua
@@ -1,13 +1,20 @@
+local transparent = true
+
+local dim_inactive = {
+  enabled = true,
+  shade = "dark",
+  percentage = 0.15,
+}
+
+if transparent then dim_inactive = nil end
+
 return {
   "catppuccin/nvim",
   name = "catppuccin",
   opts = {
+    transparent_background = transparent,
     flavour = "mocha",
-    dim_inactive = {
-      enabled = true,
-      shade = "dark",
-      percentage = 0.15,
-    },
+    dim_inactive = dim_inactive,
     custom_highlights = function(colors)
       return {
         VertSplit = { fg = colors.surface2 },
@@ -15,7 +22,5 @@ return {
       }
     end,
   },
-  init = function()
-    vim.cmd("colorscheme catppuccin")
-  end,
+  init = function() vim.cmd("colorscheme catppuccin") end,
 }
