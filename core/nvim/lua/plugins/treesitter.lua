12c68d521a9daf93b693f51a4f73869e9739b592
David Elentok <3david@gmail.com>
2025-05-25 10:13:26 +0300

    nvim: update deps and use latest treesitter

--- core/nvim/lua/plugins/treesitter.lua
+++ core/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 ---@type string[]
-local ensure_installed = {
+local parsers = {
   "bash",
   "c",
   "comment",
@@ -36,40 +36,15 @@ local ensure_installed = {
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    version = false, -- last release is way too old and doesn't work on Windows
+    branch = "main",
     build = ":TSUpdate",
-    event = { "LazyFile", "VeryLazy" },
-    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-    init = function(plugin)
-      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-      -- no longer trigger the **nvim-treesitter** module to be loaded in time.
-      -- Luckily, the only things that those plugins need are the custom queries, which we make available
-      -- during startup.
-      require("lazy.core.loader").add_to_rtp(plugin)
-      require("nvim-treesitter.query_predicates")
-    end,
-    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-    keys = {
-      { "<c-space>", desc = "Increment Selection" },
-      { "<bs>", desc = "Decrement Selection", mode = "x" },
-    },
-    opts_extend = { "ensure_installed" },
+    lazy = false,
+    init = function() require("nvim-treesitter").install(parsers) end,
     ---@type TSConfig
     ---@diagnostic disable-next-line: missing-fields
     opts = {
       highlight = { enable = true },
       indent = { enable = true },
-      ensure_installed = ensure_installed,
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
       textobjects = {
         move = {
           enable = true,
@@ -96,7 +71,5 @@ return {
         },
       },
     },
-    ---@param opts TSConfig
-    config = function(_, opts) require("nvim-treesitter.configs").setup(opts) end,
   },
 }
