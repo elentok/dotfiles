16a97a127c150fcbc64924f08d44e40737f49823
David Elentok <3david@gmail.com>
2025-06-11 14:36:20 +0300

    nvim: copilot: use global nodejs

--- core/nvim/lua/plugins/copilot.lua
+++ core/nvim/lua/plugins/copilot.lua
@@ -1,9 +1,13 @@
+vim.g.copilot_node = vim.fn.executable("/opt/homebrew/bin/node") == 1 and "/opt/homebrew/bin/node"
+  or nil
+
 return {
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
   event = "InsertEnter",
   cond = require("elentok.ai").allow,
   opts = {
+    copilot_node_command = vim.g.copilot_node,
     suggestion = { enabled = false },
     panel = { enabled = false },
     filetypes = {
