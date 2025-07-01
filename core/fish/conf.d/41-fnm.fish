ce6f2000ab34af9f459d1de016d989e065191c4a
David Elentok <3david@gmail.com>
2025-07-01 14:38:19 +0300

    fish: attempt to fix fnm

--- core/fish/conf.d/41-fnm.fish
+++ core/fish/conf.d/41-fnm.fish
@@ -2,7 +2,7 @@ fnm env --use-on-cd --shell fish | source
 
 # By default fnm adds it's path at the end so the Node that comes with homebrew
 # takes precedence. To avoid that I'm prepending it afterwards.
-fish_add_path --prepend "$FNM_MULTISHELL_PATH/bin"
+# fish_add_path --prepend "$FNM_MULTISHELL_PATH/bin"
 
 # fnm creates a new file on every session, this tries to clean it up
 function fnm_clean_up --on-event fish_exit
