23fe6f69b915c02c42f286081baf47b8833c034e
David Elentok <3david@gmail.com>
2025-07-09 10:05:01 +0300

    nvim: blink: use rounded corners on the menu

--- core/nvim/lua/plugins/blink.lua
+++ core/nvim/lua/plugins/blink.lua
@@ -56,6 +56,9 @@ return {
       },
     },
     completion = {
+      menu = {
+        border = "rounded",
+      },
       documentation = {
         auto_show = true,
         auto_show_delay_ms = 500,
