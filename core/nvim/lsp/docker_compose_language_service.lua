89332ea780278ab4b2b82108980fc011f051491e
David Elentok <3david@gmail.com>
2025-07-19 18:09:23 +0300

    nvim: add docker lsp support

---
+++ core/nvim/lsp/docker_compose_language_service.lua
@@ -0,0 +1,5 @@
+return {
+  cmd = { "docker-compose-langserver", "--stdio" },
+  filetypes = { "yaml.docker-compose" },
+  root_markers = { "docker-compose.yaml", "docker-compose.yml", "compose.yaml", "compose.yml" },
+}
--- core/nvim/lua/config/lsp.lua
+++ core/nvim/lua/config/lsp.lua
@@ -1,7 +1,15 @@
+-- Set the filetype of "*docker-compose.yml" files to "yaml.docker-compose"
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*docker-compose.yml",
+  callback = function() vim.bo.filetype = "yaml.docker-compose" end,
+})
+
 vim.lsp.enable({
   "bashls",
   "biome",
   "denols",
+  "docker_compose_language_service",
+  "dockerls",
   "dprint",
   "fish_lsp",
   "gopls",
