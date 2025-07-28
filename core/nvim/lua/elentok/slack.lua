b977b72b634d51ecd716c2954a862e77e5556f3b
David Elentok <3david@gmail.com>
2025-07-28 11:23:34 +0300

    md2slack: use dprint fmt without line breaks first

--- core/nvim/lua/elentok/slack.lua
+++ core/nvim/lua/elentok/slack.lua
@@ -1,5 +1,9 @@
-vim.api.nvim_create_user_command(
-  "Md2Slack",
-  ":silent '<,'>w !md2slack | dotf-clipboard copy",
-  { range = "%" }
-)
+vim.keymap.set("v", "<leader>ys", function()
+  local shell = require("stuff.util.shell")
+  local lines = require("stuff.util.visual").get_lines()
+  local result = shell("md2slack", { stdin = lines })
+
+  if result.code ~= 0 then return end
+
+  require("stuff.yank").yank(result.stdout)
+end, { desc = "Yank to slack" })
