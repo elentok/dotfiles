6d95dbbb9ebd95457f37d6c0b8d458fb46755dfb
David Elentok <3david@gmail.com>
2025-08-03 21:31:16 +0300

    nvim: set custom diagnostic icons

--- core/nvim/lua/config/settings.lua
+++ core/nvim/lua/config/settings.lua
@@ -31,6 +31,20 @@ vim.diagnostic.config({
   float = {
     border = "rounded",
   },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.INFO] = " ",
+      [vim.diagnostic.severity.HINT] = "󰠠 ",
+    },
+    -- linehl = {
+    --   [vim.diagnostic.severity.ERROR] = "Error",
+    --   [vim.diagnostic.severity.WARN] = "Warn",
+    --   [vim.diagnostic.severity.INFO] = "Info",
+    --   [vim.diagnostic.severity.HINT] = "Hint",
+    -- },
+  },
 })
 
 -- Backup dir
