4371126cc0421fe8a03ffb8f714b4c3139da3a78
David Elentok <3david@gmail.com>
2025-08-30 21:34:19 +0300

    jvp: use tv

--- core/fish/conf.d/jump.fish
+++ core/fish/conf.d/jump.fish
@@ -1,4 +1,4 @@
 function jvp --description 'jump to neovim plugin'
-    set plugin (cd ~/.local/share/nvim/lazy && command ls -1 | fzf --tmux)
+    set plugin (cd ~/.local/share/nvim/lazy && command ls -1 | tv)
     cd $plugin
 end
