0546a6476b55d6b18a5a453928f16f3aac12e45b
David Elentok <3david@gmail.com>
2025-09-01 18:29:02 +0300

    nvim: go back to snacks

--- core/nvim/lua/plugins/fzf.lua
+++ core/nvim/lua/plugins/fzf.lua
@@ -1,56 +1,57 @@
+return {}
 ---@module "fzf-lua"
 
-local function jump_to_config()
-  FzfLua.files({
-    cmd = "~/.dotfiles/core/nvim/list-scripts",
-  })
-end
-
-return {
-  "ibhagwan/fzf-lua",
-  dependencies = { "nvim-mini/mini.icons" },
-  ---@type fzf-lua.Config
-  opts = {
-    ---@diagnostic disable-next-line: missing-fields
-    grep = {
-      multiline = 1,
-    },
-    keymap = {
-      fzf = {
-        ["ctrl-q"] = "select-all+accept",
-      },
-    },
-  },
-  cmd = "FzfLua",
-  keys = {
-    { "<leader><space>", ":FzfLua files<cr>", desc = "FzfLua file picker" },
-    { "<leader>ff", ":FzfLua files<cr>", desc = "FzfLua file picker" },
-    { "``", ":FzfLua resume<cr>", desc = "Resume last picker" },
-    { "<leader>jr", ":FzfLua oldfiles cwd=$PWD<cr>", desc = "Find recent files" },
-    { "<leader>jR", ":FzfLua oldfiles<cr>", desc = "Find recent files (global)" },
-
-    { "<leader>jb", ":FzfLua buffers<cr>", desc = "Buffers" },
-    { "<leader>,", ":FzfLua buffers<cr>", desc = "Buffers" },
-    { "<leader>:", ":FzfLua command_history<cr>", desc = "Command History" },
-
-    { "grr", ":FzfLua lsp_references<cr>", desc = "LSP references" },
-
-    { "<leader>/", ":FzfLua live_grep<cr>", desc = "Live grep" },
-    { "<leader>w/", ":FzfLua grep_cword<cr>", desc = "Grep word" },
-    { "<leader>/", ":FzfLua grep_visual<cr>", mode = "v", desc = "Grep selection" },
-
-    { "<leader>js", ":FzfLua lsp_document_symbols<cr>", desc = "Jump to LSP symbol" },
-    { "<leader>jh", ":FzfLua helptags<cr>", desc = "Help Pages" },
-    { "<leader>jk", ":FzfLua keymaps<cr>", desc = "Keymaps" },
-
-    { "gre", ":FzfLua diagnostics_workspace<cr>", desc = "Diagnostics" },
-    { "grd", ":FzfLua diagnostics_document<cr>", desc = "Buffer Diags" },
-
-    { "<leader>gl", ":FzfLua git_bcommits<cr>", desc = "Show git log" },
-    { "<leader>gL", ":FzfLua git_blame<cr>", desc = "Show git history of current line" },
-    { "<leader>gD", ":FzfLua git_diff<cr>", desc = "Git Diff (hunks)" },
-    { "<leader>gs", ":FzfLua git_status<cr>", desc = "Git Status" },
-
-    { "<leader>jc", jump_to_config, desc = "Jump to config" },
-  },
-}
+-- local function jump_to_config()
+--   FzfLua.files({
+--     cmd = "~/.dotfiles/core/nvim/list-scripts",
+--   })
+-- end
+--
+-- return {
+--   "ibhagwan/fzf-lua",
+--   dependencies = { "nvim-mini/mini.icons" },
+--   ---@type fzf-lua.Config
+--   opts = {
+--     ---@diagnostic disable-next-line: missing-fields
+--     grep = {
+--       multiline = 1,
+--     },
+--     keymap = {
+--       fzf = {
+--         ["ctrl-q"] = "select-all+accept",
+--       },
+--     },
+--   },
+--   cmd = "FzfLua",
+--   keys = {
+--     { "<leader><space>", ":FzfLua files<cr>", desc = "FzfLua file picker" },
+--     { "<leader>ff", ":FzfLua files<cr>", desc = "FzfLua file picker" },
+--     { "``", ":FzfLua resume<cr>", desc = "Resume last picker" },
+--     { "<leader>jr", ":FzfLua oldfiles cwd=$PWD<cr>", desc = "Find recent files" },
+--     { "<leader>jR", ":FzfLua oldfiles<cr>", desc = "Find recent files (global)" },
+--
+--     { "<leader>jb", ":FzfLua buffers<cr>", desc = "Buffers" },
+--     { "<leader>,", ":FzfLua buffers<cr>", desc = "Buffers" },
+--     { "<leader>:", ":FzfLua command_history<cr>", desc = "Command History" },
+--
+--     { "grr", ":FzfLua lsp_references<cr>", desc = "LSP references" },
+--
+--     { "<leader>/", ":FzfLua live_grep<cr>", desc = "Live grep" },
+--     { "<leader>w/", ":FzfLua grep_cword<cr>", desc = "Grep word" },
+--     { "<leader>/", ":FzfLua grep_visual<cr>", mode = "v", desc = "Grep selection" },
+--
+--     { "<leader>js", ":FzfLua lsp_document_symbols<cr>", desc = "Jump to LSP symbol" },
+--     { "<leader>jh", ":FzfLua helptags<cr>", desc = "Help Pages" },
+--     { "<leader>jk", ":FzfLua keymaps<cr>", desc = "Keymaps" },
+--
+--     { "gre", ":FzfLua diagnostics_workspace<cr>", desc = "Diagnostics" },
+--     { "grd", ":FzfLua diagnostics_document<cr>", desc = "Buffer Diags" },
+--
+--     { "<leader>gl", ":FzfLua git_bcommits<cr>", desc = "Show git log" },
+--     { "<leader>gL", ":FzfLua git_blame<cr>", desc = "Show git history of current line" },
+--     { "<leader>gD", ":FzfLua git_diff<cr>", desc = "Git Diff (hunks)" },
+--     { "<leader>gs", ":FzfLua git_status<cr>", desc = "Git Status" },
+--
+--     { "<leader>jc", jump_to_config, desc = "Jump to config" },
+--   },
+-- }
