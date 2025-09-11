764a618484d7106e31ec8d6e494f72dda41f8c99
David Elentok <3david@gmail.com>
2025-09-11 09:25:20 +0300

    nvim: improve pager mappings

--- core/nvim/init-pager.lua
+++ core/nvim/init-pager.lua
@@ -1,6 +1,9 @@
 vim.keymap.set("n", "q", "<Cmd>q<cr>")
 vim.keymap.set("n", "vv", "V")
 vim.keymap.set("n", "vc", "<c-v>")
+vim.keymap.set("n", "gl", "$")
+vim.keymap.set("n", "ge", "G")
+vim.keymap.set("n", "yl", "0vg_y")
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.wildignorecase = true
