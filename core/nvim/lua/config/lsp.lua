ef5495f6bb0e385664784485666d50055078877f
David Elentok <3david@gmail.com>
2026-02-07 22:53:52 +0200

    nvim: fix lua_ls

--- core/nvim/lua/config/lsp.lua
+++ core/nvim/lua/config/lsp.lua
@@ -19,7 +19,7 @@ local server_names = {
   -- "harper_ls",
   "html",
   "jsonls",
-  "lua_ls",
+  -- "lua_ls",
   "marksman",
   "openscad_lsp",
   "pyright",
@@ -43,3 +43,52 @@ for _, server_name in ipairs(server_names) do
 end
 
 vim.lsp.enable(server_names)
+
+vim.lsp.config("lua_ls", {
+  on_attach = on_attach,
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+        path ~= vim.fn.stdpath("config")
+        and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+      then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most
+        -- likely LuaJIT in the case of Neovim)
+        version = "LuaJIT",
+        -- Tell the language server how to find Lua modules same way as Neovim
+        -- (see `:h lua-module-load`)
+        path = {
+          "lua/?.lua",
+          "lua/?/init.lua",
+        },
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+          -- Depending on the usage, you might want to add additional paths
+          -- here.
+          "${3rd}/luv/library",
+          -- '${3rd}/busted/library',
+        },
+        -- Or pull in all of 'runtimepath'.
+        -- NOTE: this is a lot slower and will cause issues when working on
+        -- your own configuration.
+        -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+        -- library = vim.api.nvim_get_runtime_file('', true),
+      },
+    })
+  end,
+  settings = {
+    Lua = {},
+  },
+})
+vim.lsp.enable("lua_ls")
