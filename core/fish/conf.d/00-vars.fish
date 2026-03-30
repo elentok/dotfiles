917b3365b4a20cea85e5324e8fc32a99ae36191c
David Elentok <3david@gmail.com>
2026-03-30 10:17:23 +0300

    fish: set GIT_EDITOR so nvim starts in insert mode

--- core/fish/conf.d/00-vars.fish
+++ core/fish/conf.d/00-vars.fish
@@ -4,6 +4,7 @@ set -e fish_user_paths
 set -x DOTF "$HOME/.dotfiles"
 set -x DOTP "$HOME/.dotplugins"
 set -x EDITOR nvim
+set -x GIT_EDITOR nvim -c startinsert
 
 set -x TMP $HOME/tmp
 if ! test -e "$TMP"
