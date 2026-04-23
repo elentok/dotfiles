b0d56c1c3f5e291d0ec04db4590fac8302e73026
David Elentok <3david@gmail.com>
2026-04-23 22:03:04 +0300

    nvim: mini.files: experiment with key mappings

--- core/nvim/plugin/20-mini.lua
+++ core/nvim/plugin/20-mini.lua
@@ -8,13 +8,26 @@ require("mini.bracketed").setup({})
 require("mini.pairs").setup({})
 require("mini.move").setup({})
 require("mini.files").setup({
+  -- windows = {
+  --   preview = true,
+  -- },
+  options = {
+    permanent_delete = false,
+  },
   mappings = {
-    synchronize = "s",
-    go_in_plus = "<CR>",
+    synchronize = "<space>w",
+    go_in = "",
+    go_in_plus = "l",
+    -- go_out = "-",
   },
 })
 
 vim.keymap.set("n", "-", function()
+  local filepath = vim.api.nvim_buf_get_name(0)
+  MiniFiles.open(filepath)
+end, { desc = "Open parent directory" })
+
+vim.keymap.set("n", "<leader>k", function()
   local filepath = vim.api.nvim_buf_get_name(0)
   MiniFiles.open(filepath)
 end, { desc = "Open parent directory" })
