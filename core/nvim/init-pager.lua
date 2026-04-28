d885c2d0592e1d52af0dc11f24023426392b30ff
David Elentok <3david@gmail.com>
2026-04-28 23:00:28 +0300

    nvim: update pager keymaps

--- core/nvim/init-pager.lua
+++ core/nvim/init-pager.lua
@@ -1,9 +1,9 @@
 vim.keymap.set("n", "q", "<Cmd>q<cr>")
 vim.keymap.set("n", "vv", "V")
 vim.keymap.set("n", "vc", "<c-v>")
-vim.keymap.set("n", "gl", "$")
-vim.keymap.set("n", "ge", "G")
-vim.keymap.set("n", "yl", "0vg_y")
+vim.keymap.set({ "n", "v" }, "gl", "$")
+vim.keymap.set({ "n", "v" }, "ge", "G")
+vim.keymap.set("n", "yl", "0vg_y:q<cr>")
 vim.keymap.set("v", "y", "y:q<cr>")
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
