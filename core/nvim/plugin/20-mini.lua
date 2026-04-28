e3449012d1d5e326e0f306587110e6fd5fdaf06a
David Elentok <3david@gmail.com>
2026-04-28 22:53:47 +0300

    nvim: move to canola (oil fork)

--- core/nvim/plugin/20-mini.lua
+++ core/nvim/plugin/20-mini.lua
@@ -7,67 +7,67 @@ end
 require("mini.bracketed").setup({})
 require("mini.pairs").setup({})
 require("mini.move").setup({})
-require("mini.files").setup({
-  windows = {
-    max_number = 2,
-  },
-  options = {
-    permanent_delete = false,
-  },
-  mappings = {
-    synchronize = "s",
-    reset = "r",
-    go_out = ",",
-    go_in = ".",
-    go_in_plus = "<cr>",
-  },
-})
-
-vim.api.nvim_create_autocmd("User", {
-  pattern = "MiniFilesBufferCreate",
-  callback = function(args)
-    vim.keymap.set(
-      "n",
-      "-",
-      MiniFiles.go_out,
-      { buffer = args.data.buf_id, desc = "Go out of directory" }
-    )
-    vim.keymap.set(
-      "n",
-      "<CR>",
-      function() MiniFiles.go_in({ close_on_file = true }) end,
-      { buffer = args.data.buf_id, desc = "Go in entry plus" }
-    )
-
-    vim.keymap.set(
-      "n",
-      "m",
-      function() MiniFiles.go_in({ close_on_file = true }) end,
-      { buffer = args.data.buf_id, desc = "Go in entry plus" }
-    )
-
-    vim.keymap.set(
-      "n",
-      "<space>w",
-      MiniFiles.synchronize,
-      { buffer = args.data.buf_id, desc = "Synchronize" }
-    )
-
-    vim.keymap.set(
-      "n",
-      "<c-s>",
-      MiniFiles.synchronize,
-      { buffer = args.data.buf_id, desc = "Synchronize" }
-    )
-  end,
-})
-
-vim.keymap.set("n", "-", function()
-  local filepath = vim.api.nvim_buf_get_name(0)
-  MiniFiles.open(filepath)
-end, { desc = "Open parent directory" })
-
-vim.keymap.set("n", "<leader>k", function()
-  local filepath = vim.api.nvim_buf_get_name(0)
-  MiniFiles.open(filepath)
-end, { desc = "Open parent directory" })
+-- require("mini.files").setup({
+--   windows = {
+--     max_number = 2,
+--   },
+--   options = {
+--     permanent_delete = false,
+--   },
+--   mappings = {
+--     synchronize = "s",
+--     reset = "r",
+--     go_out = ",",
+--     go_in = ".",
+--     go_in_plus = "<cr>",
+--   },
+-- })
+--
+-- vim.api.nvim_create_autocmd("User", {
+--   pattern = "MiniFilesBufferCreate",
+--   callback = function(args)
+--     vim.keymap.set(
+--       "n",
+--       "-",
+--       MiniFiles.go_out,
+--       { buffer = args.data.buf_id, desc = "Go out of directory" }
+--     )
+--     vim.keymap.set(
+--       "n",
+--       "<CR>",
+--       function() MiniFiles.go_in({ close_on_file = true }) end,
+--       { buffer = args.data.buf_id, desc = "Go in entry plus" }
+--     )
+--
+--     vim.keymap.set(
+--       "n",
+--       "m",
+--       function() MiniFiles.go_in({ close_on_file = true }) end,
+--       { buffer = args.data.buf_id, desc = "Go in entry plus" }
+--     )
+--
+--     vim.keymap.set(
+--       "n",
+--       "<space>w",
+--       MiniFiles.synchronize,
+--       { buffer = args.data.buf_id, desc = "Synchronize" }
+--     )
+--
+--     vim.keymap.set(
+--       "n",
+--       "<c-s>",
+--       MiniFiles.synchronize,
+--       { buffer = args.data.buf_id, desc = "Synchronize" }
+--     )
+--   end,
+-- })
+--
+-- vim.keymap.set("n", "-", function()
+--   local filepath = vim.api.nvim_buf_get_name(0)
+--   MiniFiles.open(filepath)
+-- end, { desc = "Open parent directory" })
+--
+-- vim.keymap.set("n", "<leader>k", function()
+--   local filepath = vim.api.nvim_buf_get_name(0)
+--   MiniFiles.open(filepath)
+-- end, { desc = "Open parent directory" })
--- core/nvim/plugin/40-oil.lua
+++ core/nvim/plugin/40-oil.lua
@@ -1,26 +1,28 @@
 ---@module "oil"
 
--- require("oil").setup({
---   skip_confirm_for_simple_edits = false,
---   delete_to_trash = true,
---   columns = {
---     "icon",
---     "size",
---   },
---   keymaps = {
---     ["<C-h>"] = false,
---     ["<C-l>"] = false,
---     ["<C-c>"] = false,
---     ["<C-p>"] = false,
---     ["<C-s>"] = ":w<cr>",
---     ["<leader>p"] = "actions.preview",
---     q = "actions.close",
---     L = "actions.select",
---     J = "actions.select",
---     H = "actions.parent",
---     K = "actions.parent",
---     R = "actions.refresh",
---   },
--- })
+require("oil").setup({
+  skip_confirm_for_simple_edits = false,
+  delete_to_trash = true,
+  columns = {
+    "icon",
+    "size",
+  },
+  keymaps = {
+    ["<C-h>"] = false,
+    ["<C-l>"] = false,
+    ["<C-c>"] = false,
+    ["<C-p>"] = false,
+    ["<C-s>"] = ":w<cr>",
+    ["<leader>p"] = "actions.preview",
+    q = "actions.close",
+    L = "actions.select",
+    J = "actions.select",
+    H = "actions.parent",
+    K = "actions.parent",
+    R = "actions.refresh",
+    [","] = "actions.parent",
+    ["."] = "actions.select",
+  },
+})
 
--- vim.keymap.set("n", "-", function() require("oil").open() end, { desc = "Open parent directory" })
+vim.keymap.set("n", "-", function() require("oil").open() end, { desc = "Open parent directory" })
