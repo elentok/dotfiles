aa81ec88de2c6f196590cefc6ab2c6bf63565c6d
David Elentok <3david@gmail.com>
2026-06-03 18:35:42 +0300

    fish: setup gx completion

--- core/fish/config.fish
+++ core/fish/config.fish
@@ -9,6 +9,7 @@ end
 
 if status is-interactive
     fzf --fish | source
+    gx completion fish | source
     # source "$DOTF/core/television/shell/integration.fish"
     # Commands to run in interactive sessions can go here
     # starship init fish | source
