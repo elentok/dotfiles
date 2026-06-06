7b99c9dd06553c506e183b0e2b9a1b33980c6a47
David Elentok <3david@gmail.com>
2026-06-06 23:03:57 +0300

    fzf: use catppuccin mocha theme

--- core/fish/conf.d/00-vars.fish
+++ core/fish/conf.d/00-vars.fish
@@ -11,7 +11,16 @@ if ! test -e "$TMP"
     mkdir -p "$TMP"
 end
 
-set -x FZF_DEFAULT_OPTS "--tiebreak=end --popup=85%,70% --wrap-sign='󰞘 ' --wrap=word"
+# Catppuccin theme
+# https://github.com/catppuccin/fzf/blob/main/themes/catppuccin-fzf-mocha.fish
+set -Ux FZF_DEFAULT_OPTS "\
+--tiebreak=end --popup=85%,70% --wrap-sign='󰞘 ' --wrap=word
+--color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
+--color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
+--color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
+--color=selected-bg:#45475A \
+--color=border:#6C7086,label:#CDD6F4"
+
 set -x BUN_INSTALL "$HOME/.bin"
 
 set -x MANPAGER "nvim +Man!"
