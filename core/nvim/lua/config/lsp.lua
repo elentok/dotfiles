a34bacad42405ee7e9b774a7d35bb317553a1775
David Elentok <3david@gmail.com>
2026-09-22 17:54:48 +0300

    nvim: improve lsp.lua structure

--- core/nvim/lua/config/lsp.lua
+++ core/nvim/lua/config/lsp.lua
@@ -1,67 +1,3 @@
-local servers = {
-  -- basedpyright = { "python" },
-  bashls = { "bash", "sh" },
-  biome = {
-    "astro",
-    "css",
-    "graphql",
-    "javascript",
-    "javascriptreact",
-    "json",
-    "jsonc",
-    "svelte",
-    "typescript",
-    "typescript.tsx",
-    "typescriptreact",
-    "vue",
-  },
-  cssls = { "css", "scss" },
-  css_variables = { "css", "scss" },
-  cssmodules_ls = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-  denols = {
-    "javascript",
-    "javascriptreact",
-    "javascript.jsx",
-    "typescript",
-    "typescriptreact",
-    "typescript.tsx",
-  },
-  docker_compose_language_service = { "yaml.docker-compose" },
-  dockerls = { "dockerfile" },
-  dprint = { "markdown" },
-  eslint = {
-    "astro",
-    "htmlangular",
-    "javascript",
-    "javascriptreact",
-    "svelte",
-    "typescript",
-    "typescriptreact",
-    "vue",
-  },
-  fish_lsp = { "fish" },
-  gopls = { "go", "gomod", "gowork", "gotmpl" },
-  graphql = { "graphql", "typescriptreact", "javascriptreact" },
-  html = { "html", "templ" },
-  jsonls = { "json", "jsonc" },
-  lua_ls = { "lua" },
-  ["markdown-oxide"] = { "markdown" },
-  openscad_lsp = { "openscad" },
-  spyglassmc_language_server = { "mcfunction" },
-  taplo = { "toml" },
-  pyrefly = { "python" },
-  -- ty = { "python" },
-  vtsls = {
-    "javascript",
-    "javascriptreact",
-    "javascript.jsx",
-    "typescript",
-    "typescriptreact",
-    "typescript.tsx",
-  },
-  yamlls = { "yaml", "yaml.docker-compose", "yaml.gitlab" },
-}
-
 local configured = {}
 local defaults_configured = false
 
@@ -74,26 +10,78 @@ local function configure_defaults()
   })
 end
 
-local function configure_server(server_name)
+---@param server_name string
+---@param config? vim.lsp.Config
+local function configure_server(server_name, config)
   if configured[server_name] then return end
   configured[server_name] = true
 
   configure_defaults()
+  if config then vim.lsp.config(server_name, config) end
   vim.lsp.enable(server_name)
 end
 
-local servers_by_filetype = {}
-for server_name, filetypes in pairs(servers) do
-  for _, filetype in ipairs(filetypes) do
-    servers_by_filetype[filetype] = servers_by_filetype[filetype] or {}
-    table.insert(servers_by_filetype[filetype], server_name)
-  end
+---@param name string
+---@param filetypes string[]
+---@param config? vim.lsp.Config
+local function add_server(name, filetypes, config)
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = filetypes,
+    callback = function() configure_server(name, config) end,
+  })
 end
 
-vim.api.nvim_create_autocmd("FileType", {
-  callback = function(ev)
-    for _, server_name in ipairs(servers_by_filetype[ev.match] or {}) do
-      configure_server(server_name)
-    end
-  end,
+local js_ts_filetypes = {
+  "javascript",
+  "javascriptreact",
+  "javascript.jsx",
+  "typescript",
+  "typescriptreact",
+  "typescript.tsx",
+}
+
+add_server("bashls", { "bash", "sh" })
+add_server("biome", {
+  "astro",
+  "css",
+  "graphql",
+  "javascript",
+  "javascriptreact",
+  "json",
+  "jsonc",
+  "svelte",
+  "typescript",
+  "typescript.tsx",
+  "typescriptreact",
+  "vue",
 })
+add_server("cssls", { "css", "scss" })
+add_server("css_variables", { "css", "scss" })
+add_server("cssmodules_ls", { "javascript", "javascriptreact", "typescript", "typescriptreact" })
+add_server("denols", js_ts_filetypes)
+add_server("docker_compose_language_service", { "yaml.docker-compose" })
+add_server("dockerls", { "dockerfile" })
+add_server("dprint", { "markdown" })
+add_server("eslint", {
+  "astro",
+  "htmlangular",
+  "javascript",
+  "javascriptreact",
+  "svelte",
+  "typescript",
+  "typescriptreact",
+  "vue",
+})
+add_server("fish_lsp", { "fish" })
+add_server("gopls", { "go", "gomod", "gowork", "gotmpl" })
+add_server("graphql", { "graphql", "typescriptreact", "javascriptreact" })
+add_server("html", { "html", "templ" })
+add_server("jsonls", { "json", "jsonc" })
+add_server("lua_ls", { "lua" })
+add_server("markdown-oxide", { "markdown" })
+add_server("openscad_lsp", { "openscad" })
+add_server("pyrefly", { "python" })
+add_server("spyglassmc_language_server", { "mcfunction" })
+add_server("taplo", { "toml" })
+add_server("vtsls", js_ts_filetypes)
+add_server("yamlls", { "yaml", "yaml.docker-compose", "yaml.gitlab" })
